579a5e81a6daeae6b8c84c002ea5e549f1f8a69b
Patrick Shaw <mail@patrickshaw.me>
2018-02-16 01:30:05 +1100

    Added isHexColor test

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ]
+    // testPathIgnorePatterns: ["/node_modules/", "/dist/"]
+}
