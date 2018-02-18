424d2dae3fa70c438055537a8ba030172d16323d
Patrick Shaw <mail@patrickshaw.me>
2018-02-18 12:24:40 +1100

    Added rgb(...) tests

--- test/unit.test.tsx
+++ test/unit.test.tsx
@@ -1,4 +1,4 @@
-import { isHexColor } from 'src/index';
+import { isHexColor, isRgbColor } from 'src/index';
 function testFunction(isColorFunction, trueCases: Set<any>, falseCases: Set<any>) {
   const invalidTypes = [
     ["Function", () => undefined],
@@ -71,12 +71,11 @@ const invalidHexValues = new Set([
   "#FFFFFF "
 ]);
 
-const validRgbValues = [
+const validRgbValues = new Set([
   "rgb(0,0,0)",
   "rgb(0.1,0.1,0.1)",
   "rgb(.2,.2,.2)",
   "rgb(255,255,255)",
-  "rgb(256,256,256)",
   "rgb(0,0,0)",
   "rgb  (0,0,0)",
   "rgb(  0,0,0)",
@@ -84,8 +83,9 @@ const validRgbValues = [
   "rgb(0,  0,0)",
   "rgb(0,0  ,0)",
   "rgb(0,0,  0)",
-  "rgb(0,0,0  )"
-];
+  "rgb(0,0,0  )",
+  "rgb(254.9999,254.9999,254.9999)"
+]);
 const invalidRgbValues = new Set([
   "gb(0,0,0)",
   "rb(0,0,0)",
@@ -96,14 +96,16 @@ const invalidRgbValues = new Set([
   "rgb(0,0,0",
   "rgb(0,0,0))",
   "rgb(0,0,0) ",
-  " rgb(0,0,0)"
+  " rgb(0,0,0)",
+  "rgb(255.0001,255.0001,255.0001)",
+  "rgb(256,256,256)",
 ]);
-const validRgbaValues = [
+const validRgbaValues = new Set([
   "rgba(0,0,0,0)",
   "rgba(255,255,255,1)",
   "rgba(.1,.1,.1,.1)",
-];
-const invalidRgbaValues = [
+]);
+const invalidRgbaValues = new Set([
   "rgba(256,256,256,1)",
   "rgba(255,255,255,1.1)",
   "rgba(256,256,256,1.1)",
@@ -113,7 +115,10 @@ const invalidRgbaValues = [
   "rgba(255,255,255,1) ",
   " rgba(255,255,255,1)",
   "rgba(-1,-1,-1,-1)"
-]
+]);
 describe('Hex', () => {
   testFunction(isHexColor, validHexValues, invalidHexValues);
+});
+describe('rgb(...)', () => {
+  testFunction(isRgbColor, validRgbValues, invalidRgbValues);
 });
