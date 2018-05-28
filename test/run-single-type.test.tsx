358c973234876a647127655f1c0b5fbe13f8b312
Patrick Shaw <mail@patrickshaw.me>
2018-05-29 00:55:51 +1000

    Refactored type test

--- test/run-single-type.test.tsx
+++ test/run-single-type.test.tsx
@@ -1,4 +1,15 @@
 import { runSingularTypeTests, typeTest } from 'src/index';
+function testResult(values, typeTests, expectedResult) {
+  it('correct result', () => {
+    const result = runSingularTypeTests(
+      values, typeTests
+    );
+    
+    expect(result.undefinedCount).toBe(expectedResult.undefinedCount);
+    expect(result.nullCount).toBe(expectedResult.nullCount);
+    expect(result.typeTest).toBe(expectedResult.typeTest);
+  })
+}
 describe('run-single-type', () => {
   it('type check called', () => {
     const check = jest.fn();
@@ -12,18 +23,15 @@ describe('run-single-type', () => {
     );
     expect(check.mock.calls.length).toBe(1);
   })
-  it('1 type', () => {
+  describe('1 type', () => {
     const int = typeTest(
       (value) => Number.isInteger(value),
       undefined
     );
-    const result = runSingularTypeTests(
-      [1, 2, 3], [
-        int
-      ]
-    );
-    expect(result.undefinedCount).toBe(0);
-    expect(result.nullCount).toBe(0);
-    expect(result.typeTest).toBe(int);
-  });
+    testResult([1, 2, 3], [int], {
+      undefinedCount: 0,
+      nullCount: 0,
+      typeTest: int
+    })
+  })
 });
