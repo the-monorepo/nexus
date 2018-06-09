eb540dce22995c8af4edd53e928a92c9d3b83140
Patrick Shaw <mail@patrickshaw.me>
2018-06-10 00:51:41 +1000

    Added type checking tests

---
+++ packages/types/test/types.test.tsx
@@ -0,0 +1,77 @@
+import {
+  isBoolean, 
+  isObject,
+  isArray,
+  isString,
+  isFunction,
+  isNumber
+} from '../src/type-checks';
+const typeValues = {
+  boolean: [
+    true, 
+    false
+  ],
+  string: [
+    'true',
+    'false',
+    ''
+  ],
+  object: [
+    {},
+    {
+      test: 'test'
+    }
+  ],
+  array: [
+    [],
+    ['test'],
+    [{}]
+  ],
+  number: [
+    1,
+    -1,
+    0,
+    2.32
+  ],
+  function: [
+    () => {},
+    function () { },
+    function namedFunction() {}
+  ],
+  class: [
+    class {
+
+    },
+    class NamedClass {
+
+    }
+  ]
+}
+function testTypeCheck(name, validValues, typeCheck) {
+  describe(name, () => {
+    validValues.forEach(key => describe(`${key} (valid)`, () => {
+      typeValues[key].forEach(value => {
+        it(`${value}`, () =>{
+          expect(typeCheck(value)).toBe(true);
+        })
+      })
+    }));
+    Object.keys(typeValues).filter(key => !validValues.includes(key)).forEach(key => {
+      describe(key, () => {
+        typeValues[key].forEach(value => {
+          it(`${value}`, () => {
+            expect(typeCheck(value)).toBe(false);
+          });
+        });
+      })
+    });
+  });
+}
+describe('type checks', () => {
+  testTypeCheck('isBoolean', ['boolean'], isBoolean);
+  testTypeCheck('isObject', ['object'], isObject);
+  testTypeCheck('isArray', ['array'], isArray);
+  testTypeCheck('isString', ['string'], isString);
+  testTypeCheck('isFunction', ['function', 'class'], isFunction);
+  testTypeCheck('isNumber', ['number'], isNumber);
+})
