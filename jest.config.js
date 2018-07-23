1344153b301101123770209669e29da38a3ac6a2
Patrick Shaw <psha67@student.monash.edu>
2018-07-24 05:15:18 +1000

    prettier now ignores coverage/

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   testMatch: ['<rootDir>/tests/?(**/)?(*.)test.(t|j)s?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: ['/node_modules/', '/lib/'],
-  "collectCoverage": true,
-  "coverageDirectory": "./coverage/",
+  collectCoverage: true,
+  coverageDirectory: './coverage/',
   transform: {
     '^.+\\.ts$': 'babel-jest',
   },
