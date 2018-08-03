5d2f0af5d696952b4f094dd916b44b98ca2221a9
Patrick Shaw <mail@patrickshaw.me>
2018-08-04 02:39:23 +1000

    More accurate coverage reporting
    Was including lib

--- packages/openapi/tests/integration/valid-schema.test.ts
+++ packages/openapi/tests/integration/valid-schema.test.ts
@@ -1,5 +1,5 @@
 import { createSchema } from '../../src/index';
-import { extractTypeInfo, DefaultTypeName, NumberFormat } from '@by-example/types';
+import { DefaultTypeName, NumberFormat } from '@by-example/types';
 import SwaggerParser from 'swagger-parser';
 function testWithValidation(name, callback) {
   it(name, async () => {
