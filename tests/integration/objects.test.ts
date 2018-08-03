fff4ce7d2c85ae0381608fb48eeb444e8d5150ca
Patrick Shaw <mail@patrickshaw.me>
2018-08-03 23:07:38 +1000

    More test refactoring

---
+++ tests/integration/objects.test.ts
@@ -0,0 +1,83 @@
+import { examples } from '../util/from-examples';
+import { DefaultTypeName, NumberFormat } from '@by-example/types';
+describe('objects', () => {
+  it('2 objects with all fields', () => {
+    examples([
+      {
+        int: 1,
+        string: '',
+      },
+      {
+        int: 1,
+        string: '',
+      },
+    ])
+      .typeInfo({
+        types: [
+          {
+            name: DefaultTypeName.object,
+            fields: {
+              int: {
+                types: [{ name: DefaultTypeName.number, format: NumberFormat.integer }],
+                nullCount: 0,
+                undefinedCount: 0,
+              },
+              string: {
+                types: [{ name: DefaultTypeName.string }],
+                nullCount: 0,
+                undefinedCount: 0,
+              },
+            },
+          },
+        ],
+        undefinedCount: 0,
+        nullCount: 0,
+      })
+      .openapi({
+        type: 'object',
+        required: ['int', 'string'],
+        properties: {
+          int: { type: 'integer' },
+          string: { type: 'string' },
+        },
+      });
+  });
+  it('2 objects with different fields', () => {
+    examples([
+      {
+        int: -1,
+      },
+      {
+        string: 'a',
+      },
+    ])
+      .typeInfo({
+        types: [
+          {
+            name: DefaultTypeName.object,
+            fields: {
+              int: {
+                types: [{ name: DefaultTypeName.number, format: NumberFormat.integer }],
+                nullCount: 0,
+                undefinedCount: 1,
+              },
+              string: {
+                types: [{ name: DefaultTypeName.string }],
+                nullCount: 0,
+                undefinedCount: 1,
+              },
+            },
+          },
+        ],
+        undefinedCount: 0,
+        nullCount: 0,
+      })
+      .openapi({
+        type: 'object',
+        properties: {
+          int: { type: 'integer' },
+          string: { type: 'string' },
+        },
+      });
+  });
+});
