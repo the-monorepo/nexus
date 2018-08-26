c152b6c685d9ecf9694fd78812a5f78c1a8f0c8c
Patrick Shaw <mail@patrickshaw.me>
2018-08-27 01:16:58 +1000

    Fixed infinite recursion bug

--- src/index.ts
+++ src/index.ts
@@ -9,25 +9,33 @@ type Input =
     }
   | Array<any>;
 
-export function mockFunctions(value: Input, options: Options = {}) {
+export function mockFunctions(
+  value: Input,
+  options: Options = {},
+  ogToMockedMap: Map<any, any> = new Map(),
+) {
   const { onMockedFunction = () => {} } = options;
   let mockedObject = undefined;
-
-  mockedObject = Object.assign(Object.create(Object.getPrototypeOf(value)), value);
-  Object.getOwnPropertyNames(value).forEach(key => {
-    const realValue = value[key];
-    if (typeof realValue === 'function') {
-      mockedObject[key] = jest.fn();
-      onMockedFunction(mockedObject[key], realValue);
-    } else if (typeof realValue === 'object') {
-      if (realValue !== null && options.recursive) {
-        mockedObject[key] = mockFunctions(realValue, options);
+  if (ogToMockedMap.has(value)) {
+    mockedObject = ogToMockedMap.get(value);
+  } else {
+    mockedObject = Object.assign(Object.create(Object.getPrototypeOf(value)), value);
+    ogToMockedMap.set(value, mockedObject);
+    Object.getOwnPropertyNames(value).forEach(key => {
+      const realValue = value[key];
+      if (typeof realValue === 'function') {
+        mockedObject[key] = jest.fn();
+        onMockedFunction(mockedObject[key], realValue);
+      } else if (typeof realValue === 'object') {
+        if (realValue !== null && options.recursive) {
+          mockedObject[key] = mockFunctions(realValue, options, ogToMockedMap);
+        } else {
+          mockedObject[key] = realValue;
+        }
       } else {
         mockedObject[key] = realValue;
       }
-    } else {
-      mockedObject[key] = realValue;
-    }
-  });
+    });
+  }
   return mockedObject;
 }
