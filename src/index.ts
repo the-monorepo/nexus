9f72155682ded406688f46a0ce5528ea63b7c6c8
Patrick Shaw <mail@patrickshaw.me>
2018-08-27 05:10:54 +1000

    More typescript fixes

--- src/index.ts
+++ src/index.ts
@@ -3,43 +3,40 @@ interface Options {
   onMockedFunction?: (mockedFunction, originalValue) => any;
 }
 
-type Input<T, V> =
-  | {
-      [key: string]: T;
-    }
-  | Array<V>;
-
-type InputObject<T> = { [P in keyof T]: T[P] };
-
 type MockedObject<T> = { [P in keyof T]: any };
 
-export function mockFunctions<T>(
-  value: InputObject<T> | any,
+function mockValue<T>(
+  value: T,
   options: Options = {},
   ogToMockedMap: Map<any, any> = new Map(),
-): MockedObject<T> | any {
+): MockedObject<T> {
   const { onMockedFunction = () => {} } = options;
-  let mockedObject = undefined;
-  if (ogToMockedMap.has(value)) {
-    mockedObject = ogToMockedMap.get(value);
-  } else {
-    mockedObject = Object.assign(Object.create(Object.getPrototypeOf(value)), value);
-    ogToMockedMap.set(value, mockedObject);
-    Object.getOwnPropertyNames(value).forEach(key => {
-      const realValue = value[key];
-      if (typeof realValue === 'function') {
-        mockedObject[key] = jest.fn();
-        onMockedFunction(mockedObject[key], realValue);
-      } else if (typeof realValue === 'object') {
-        if (realValue !== null && options.recursive) {
-          mockedObject[key] = mockFunctions(realValue, options, ogToMockedMap);
-        } else {
-          mockedObject[key] = realValue;
-        }
+  const mockedObject = Object.assign(Object.create(Object.getPrototypeOf(value)), value);
+  ogToMockedMap.set(value, mockedObject);
+  Object.getOwnPropertyNames(value).forEach(key => {
+    const realValue = value[key];
+    if (typeof realValue === 'function') {
+      mockedObject[key] = jest.fn();
+      onMockedFunction(mockedObject[key], realValue);
+    } else if (typeof realValue === 'object') {
+      if (realValue !== null && options.recursive) {
+        mockedObject[key] = mockFunctions(realValue, options, ogToMockedMap);
       } else {
         mockedObject[key] = realValue;
       }
-    });
-  }
+    } else {
+      mockedObject[key] = realValue;
+    }
+  });
   return mockedObject;
+}
+
+export function mockFunctions<T>(
+  value: T,
+  options: Options = {},
+  ogToMockedMap: Map<any, any> = new Map(),
+): MockedObject<T> {
+  return ogToMockedMap.has(value)
+    ? ogToMockedMap.get(value)
+    : mockValue(value, options, ogToMockedMap);
 }
