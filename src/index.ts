f7a791bd052da7903f44d4fff71921b3c7730aff
Patrick Shaw <mail@patrickshaw.me>
2018-08-28 01:34:55 +1000

    Now works with Maps and Sets

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { create } from 'domain';
+
 export interface Options {
   recursive?: boolean;
   onMockedFunction?: (mockedFunction, originalValue) => any;
@@ -5,18 +7,36 @@ export interface Options {
 
 export type Mocked<T> = { [P in keyof T]: any };
 
+function constructBasedOff(obj) {
+  return new obj.constructor();
+}
+
 function mockValue(realValue, options: Options, ogToMockedMap: Map<any, any>) {
+  function handleContainer(createMockStubCallback, mockCallback) {
+    if (ogToMockedMap.has(realValue)) {
+      return ogToMockedMap.get(realValue);
+    } else if (options.recursive || ogToMockedMap.size == 0) {
+      const mocked = createMockStubCallback(realValue);
+      ogToMockedMap.set(realValue, mocked);
+      return mockCallback(realValue, mocked, options, ogToMockedMap);
+    } else {
+      return realValue;
+    }
+  }
   const { onMockedFunction = () => {} } = options;
   if (typeof realValue === 'function') {
     const mockedFn = jest.fn();
     onMockedFunction(mockedFn, realValue);
     return mockedFn;
-  } else if (typeof realValue === 'object') {
-    if (realValue !== null && options.recursive) {
-      return mockFunctions(realValue, options, ogToMockedMap);
-    } else {
-      return realValue;
-    }
+  } else if (realValue === null || realValue === undefined) {
+    return realValue;
+  } else if (realValue instanceof Map) {
+    return handleContainer(constructBasedOff, mockMapValues);
+  } else if (realValue instanceof Set) {
+    return handleContainer(constructBasedOff, mockSetValues);
+  }
+  if (typeof realValue === 'object') {
+    return handleContainer(constructBasedOff, mockProperties);
   } else {
     return realValue;
   }
@@ -24,12 +44,12 @@ function mockValue(realValue, options: Options, ogToMockedMap: Map<any, any>) {
 
 function mockProperties<T>(
   value: T,
+  mockedObject,
   options: Options = {},
   ogToMockedMap: Map<any, any> = new Map(),
 ): Mocked<T> {
-  const mockedObject = Object.assign(Object.create(Object.getPrototypeOf(value)), value);
-  ogToMockedMap.set(value, mockedObject);
   const propertyDescriptors = Object.getOwnPropertyDescriptors(value);
+
   Object.keys(propertyDescriptors).forEach(key => {
     const propertyDescriptor = propertyDescriptors[key];
     const mockedValue = mockValue(propertyDescriptor.value, options, ogToMockedMap);
@@ -39,12 +59,34 @@ function mockProperties<T>(
   return mockedObject;
 }
 
+function mockMapValues<T, V>(
+  actualMap: Map<T, V>,
+  options: Options = {},
+  ogToMockedMap: Map<any, any> = new Map(),
+): Map<T, V | Mocked<V>> {
+  const mockedMap = new Map();
+  for (const [key, value] of actualMap.entries()) {
+    mockedMap.set(key, mockValue(value, options, ogToMockedMap));
+  }
+  return mockedMap;
+}
+
+function mockSetValues<T>(
+  actualSet: Set<T>,
+  options: Options = {},
+  ogToMockedMap: Map<any, any> = new Map(),
+): Set<T | Mocked<T>> {
+  const mockedSet = new Set();
+  for (const value of actualSet) {
+    mockedSet.add(mockValue(value, options, ogToMockedMap));
+  }
+  return mockedSet;
+}
+
 export function mockFunctions<T>(
   value: T,
   options: Options = {},
   ogToMockedMap: Map<any, any> = new Map(),
 ): Mocked<T> {
-  return ogToMockedMap.has(value)
-    ? ogToMockedMap.get(value)
-    : mockProperties(value, options, ogToMockedMap);
+  return mockValue(value, options, ogToMockedMap);
 }
