32b5abc89b880ea171b5bba42384903a083d4ce7
Patrick Shaw <psha67@student.monash.edu>
2018-09-15 04:44:07 +1000

    Added module field to pacakge.json and transpiling to both commonjs and ES modules

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,18 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: '6',
+module.exports = api => {
+  const env = api.env();
+  const esm = env === 'ES6';
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: esm ? false : undefined,
+          targets: {
+            esmodules: esm,
+          },
         },
-      },
+      ],
+      '@babel/preset-typescript',
     ],
-    '@babel/preset-typescript',
-  ],
+  };
 };
