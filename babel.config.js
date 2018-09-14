268a7918cb30ab7adbe60f604e4e3b2c47a59c19
Patrick Shaw <mail@patrickshaw.me>
2018-09-15 05:14:11 +1000

    Node 6 support, ESM support and source maps

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,20 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: '8.11.3',
+module.exports = api => {
+  const env = api.env();
+  const esm = env === 'ESM';
+  return {
+    sourceMaps: true,
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: esm ? false : undefined,
+          targets: {
+            node: '6',
+            esmodules: esm,
+          },
         },
-      },
+      ],
+      '@babel/preset-typescript',
     ],
-    '@babel/preset-typescript',
-  ],
+  };
 };
