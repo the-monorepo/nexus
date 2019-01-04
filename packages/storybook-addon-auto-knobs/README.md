e5175e1d7349872e5971438be8588044614e4748
Patrick Shaw <mail@patrickshaw.me>
2019-01-04 22:28:35 +1100

    Separated the knobified and storybook addon logic into different packages

---
+++ packages/storybook-addon-auto-knobs/README.md
@@ -0,0 +1,10 @@
+# Storybook Addon-Auto-Knobs
+
+Automatically created knobbed versions of your stories based on value and prop types
+
+## Installation
+
+`npm install --save @by-example/storybook-addon-auto-knobs@1.0.0-alpha.5`
+or
+`yarn add @by-example/storybook-addon-auto-knobs@1.0.0-alpha.5`
+
---
+++ packages/storybook-addon-auto-knobs/src/index.ts
@@ -0,0 +1,17 @@
+import { makeDecorator } from '@storybook/addons';
+import { fromExamples } from '@by-example/storybook-knobified';
+export const withAutoKnobs = makeDecorator({
+  name: 'withAutoKnobs',
+  wrapper: (getStory, context) => {
+    /*const bucket = getStorybook().filter((bucket) => bucket.kind === context.kind)[0];
+    const storiesProps = bucket.stories.filter(story => story.name !== context.story).map(story => {
+      return getStory()
+    });*/
+    const story = getStory(context);
+    const knobbedStory = {
+      ...story,
+      props: fromExamples([story.props], context.type, {}).knobified(story.props),
+    };
+    return knobbedStory;
+  },
+});
