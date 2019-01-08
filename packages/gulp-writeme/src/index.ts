978f00d4fed6eb4ad397c89e9a7f39b890b8293e
Patrick Shaw <mail@patrickshaw.me>
2019-01-08 22:40:11 +1100

    Added gulp-writeme

---
+++ packages/gulp-writeme/src/index.ts
@@ -0,0 +1,12 @@
+import through from 'through2';
+import { join } from 'path';
+import { genReadmeFromPackageDir } from '@pshaw/writeme';
+export function writeme(missingFileCallback) {
+  return through.obj(async (file, enc, callback) => {
+    const readmeText = await genReadmeFromPackageDir(file.path, missingFileCallback);
+    file.contents = new Buffer(readmeText);
+    file.path = join(file.path, 'README.md');
+    callback(null, file);
+  });
+}
+export default writeme;
