db72ae8f2b4ea0fb1dc05961b5aadbc9e0783c33
Patrick Shaw <psha67@student.monash.edu>
2019-01-10 23:50:52 +1100

    writeme now Promise compatible

--- packages/writeme/writeme.config.js
+++ packages/writeme/writeme.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-  isDevPackage: true,
+const { readFile } = require('mz/fs');
+const { join } = require('path');
+module.exports = async () => {
+  let examples =
+    '```js\n' +
+    (await readFile(join(__dirname, 'examples', 'from-package-directory'))) +
+    '\n' +
+    '```';
+  return {
+    isDevPackage: true,
+    examples,
+  };
 };
