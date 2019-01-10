73b2a221c4572614dea0d301bdc9b51fa8c7e6ac
Patrick Shaw <mail@patrickshaw.me>
2019-01-10 23:50:52 +1100

    writeme now Promise compatible

--- packages/gulp-writeme/src/index.ts
+++ packages/gulp-writeme/src/index.ts
@@ -1,10 +1,10 @@
-import { genReadmeFromPackageDir } from '@shawp/writeme';
+import ogWriteme from '@shawp/writeme';
 import { join } from 'path';
 import through from 'through2';
 export function writeme(missingFileCallback) {
   return through.obj(async (file, enc, callback) => {
-    const readmeText = await genReadmeFromPackageDir(file.path, missingFileCallback);
-    file.contents = new Buffer(readmeText);
+    const readmeText = await ogWriteme(file.path, missingFileCallback);
+    file.contents = new Buffer(readmeText, enc);
     file.path = join(file.path, 'README.md');
     callback(null, file);
   });
