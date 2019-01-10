3acfda6ccbe8d5daed19b1e9c1bc0395aabe0bb2
Patrick Shaw <mail@patrickshaw.me>
2019-01-10 23:55:23 +1100

    Generalised writeme title package json name replacement

--- packages/writeme/writeme.config.js
+++ packages/writeme/writeme.config.js
@@ -1,11 +1,9 @@
 const { readFile } = require('mz/fs');
 const { join } = require('path');
 module.exports = async () => {
-  let examples =
-    '```js\n' +
-    (await readFile(join(__dirname, 'examples', 'from-package-directory'))) +
-    '\n' +
-    '```';
+  const example = await readFile(join(__dirname, 'examples', 'from-package-directory'));
+  const examples = '```js\n' + example + '\n' + '```';
+  console.log(examples);
   return {
     isDevPackage: true,
     examples,
