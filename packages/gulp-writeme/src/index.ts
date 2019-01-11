fd5399a911b63d10d74cd932f011cde51650cdc6
Patrick Shaw <psha67@student.monash.edu>
2019-01-12 03:05:58 +1100

    Better writeme error handling

--- packages/gulp-writeme/src/index.ts
+++ packages/gulp-writeme/src/index.ts
@@ -1,11 +1,17 @@
 import ogWriteme from '@shawp/writeme';
 import { join } from 'path';
 import through from 'through2';
+import PluginError from 'plugin-error';
 export function writeme(missingFileCallback) {
   return through.obj(async (file, enc, callback) => {
-    const readmeText = await ogWriteme(file.path, missingFileCallback);
-    file.contents = new Buffer(readmeText, enc);
-    file.path = join(file.path, 'README.md');
+    try {
+      const readmeText = await ogWriteme(file.path, missingFileCallback);
+      file.contents = new Buffer(readmeText, enc);
+      file.path = join(file.path, 'README.md');  
+    } catch(err) {
+      callback(new PluginError('writeme', err, { showStack: true }));
+      return;
+    }
     callback(null, file);
   });
 }
