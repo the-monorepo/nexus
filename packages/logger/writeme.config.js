312b7314cca62d13aed5308fed0e73cc17b861ad
Patrick Shaw <psha67@student.monash.edu>
2019-01-12 03:06:24 +1100

    Fixed logger writeme config

--- packages/logger/writeme.config.js
+++ packages/logger/writeme.config.js
@@ -1,10 +1,12 @@
-const { readCodeBlock } = require('markdown-util');
+const { readCodeBlock } = require('@shawp/markdown-util');
 const { join } = require('path');
 module.exports = async () => {
-  const examples = await readCodeBlock(join(__dirname, 'examples/file-and-console/index.js'));
+  const examples = await readCodeBlock(
+    join(__dirname, 'examples/file-and-console/index.js'),
+  );
   return {
     sections: {
-      examples
-    }
-  }
-}
+      examples,
+    },
+  };
+};
