e36ba3141275211829916df005ef3b378d381092
Patrick Shaw <mail@patrickshaw.me>
2019-01-12 03:05:58 +1100

    Better writeme error handling

---
+++ packages/logger/writeme.config.js
@@ -0,0 +1,10 @@
+const { readCodeBlock } = require('markdown-util');
+const { join } = require('path');
+module.exports = async () => {
+  const examples = await readCodeBlock(join(__dirname, 'examples/file-and-console/index.js'));
+  return {
+    sections: {
+      examples
+    }
+  }
+}
