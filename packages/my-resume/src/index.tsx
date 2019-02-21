b214030dd09c276adf592773ac492191258a2a7e
Patrick Shaw <mail@patrickshaw.me>
2019-02-21 14:38:54 +1100

    WIP combined lint and prettier streams
    Probably going to need to drop TSLint so I can do proper streaming...

--- packages/my-resume/src/index.tsx
+++ packages/my-resume/src/index.tsx
@@ -1,7 +1,7 @@
 import { createResume } from '@pshaw/resume-template';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import * as data from './data';
+import * as ReactDOM from 'react-dom';
 
 const Resume = createResume();
 ReactDOM.render(<Resume data={data} />, document.getElementById('root'));
