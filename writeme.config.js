a99c477097ae6aa5a9587bc0cba6786615ea912f
Patrick Shaw <mail@patrickshaw.me>
2019-02-24 10:48:35 +1100

    Added writeme files into cleanup

--- writeme.config.js
+++ writeme.config.js
@@ -2,16 +2,16 @@ module.exports = {
   projects: {
     overrides: [
       {
+        category: 'Logging',
+        test: './packages/{logger,winston-formats}',
+      },
+      {
         category: 'ESLint',
         test: './packages/tslint*',
       },
       {
         category: 'TSLint',
         test: './packages/eslint*',
-      },
-      {
-        category: 'Logging',
-        test: './packages/{logger,winston-formats}',
       },
     ],
   },
