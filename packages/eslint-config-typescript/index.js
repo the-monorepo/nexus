f2a15116ebdeaf2ee9c099d9905393dc16f1db91
Patrick Shaw <mail@patrickshaw.me>
2019-02-25 01:49:41 +1100

    Fixed sourcemaps being totally wrong

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -3,5 +3,6 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 };
