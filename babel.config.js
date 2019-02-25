299ef547840635965ce4e15d6013eb6bc7cf0f2e
Patrick Shaw <psha67@student.monash.edu>
2019-02-25 13:18:27 +1100

    Resume updates

--- babel.config.js
+++ babel.config.js
@@ -2,35 +2,34 @@ module.exports = api => {
   const env = api.env();
   const esm = env === 'ESM';
 
-  function baseConfig(targets) {
-    return {
-      presets: [
-        '@babel/preset-typescript',
-        [
-          '@babel/preset-env',
-          {
-            modules: esm ? false : undefined,
-            targets: {
-              ...targets,
-              esmodules: esm,
-            },
-          },
-        ],
-      ],
-      plugins: env === 'test' ? ['babel-plugin-istanbul'] : [],
-    };
-  }
-
-  const config = baseConfig({ node: env === 'development' ? 'current' : '6' });
-  const tsxConfig = baseConfig();
-  tsxConfig.plugins.shift('@babel/preset-react');
-
   return {
-    ...config,
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: esm ? false : undefined,
+          targets: {
+            node: env === 'development' ? 'current' : '6',
+            esmodules: esm,
+          },
+        },
+      ],
+      '@babel/preset-typescript',
+    ],
+    plugins: env === 'test' ? ['babel-plugin-istanbul'] : [],
     overrides: [
       {
         test: ['./packages/my-resume', './packages/resume-template'],
-        ...tsxConfig,
+        presets: [
+          '@babel/preset-react',
+          [
+            '@babel/preset-env',
+            {
+              modules: esm ? false : undefined,
+              targets: { esmodules: esm },
+            },
+          ],
+        ],
       },
     ],
   };
