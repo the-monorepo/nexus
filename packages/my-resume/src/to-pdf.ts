c95819dd1b98be87af8a4e34204f316b836bc399
Patrick Shaw <mail@patrickshaw.me>
2019-03-16 01:30:32 +1100

    Now using puppeteer to render my resume
    Why does it seem more reliable than a typical Chrome browser? o.O

---
+++ packages/my-resume/src/to-pdf.ts
@@ -0,0 +1,18 @@
+import puppeteer from 'puppeteer';
+import express from 'express';
+import { logger, consoleTransport } from '@pshaw/logger';
+const l = logger().add(consoleTransport());
+l.info('Serving bundle...');
+const app = express();
+app.use('/', express.static('dist'));
+const PORT = 3000;
+app.listen(PORT, async () => {
+  l.info(`Listing on port ${PORT}`);
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000', { waitUntil: 'networkidle2' });
+  const filename = 'resume.pdf';
+  await page.pdf({ path: filename, format: 'A4', printBackground: true });
+  l.info(`Saved PDF as ${filename}`);
+  process.exit(0);
+});
