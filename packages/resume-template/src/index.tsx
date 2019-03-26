eeb02fa692a5fa17f3247e3b1354877984fb9851
Patrick Shaw <mail@patrickshaw.me>
2019-03-27 03:10:04 +1100

    Managed to use JSX without actually using React
    This is the coolest thing i've learnt in a few weeks

--- packages/resume-template/src/index.tsx
+++ packages/resume-template/src/index.tsx
@@ -1,44 +1,9 @@
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
-import jss from 'jss';
-import * as jssGlobal from 'jss-global';
-import * as jssNested from 'jss-nested';
-import * as React from 'react';
-
-import { Page } from './components';
+import './jsx';
 
 export function createResume() {
-  const theme = createMuiTheme({
-    typography: {
-      useNextVariants: true,
-      fontWeightMedium: 600,
-      subtitle1: {
-        fontWeight: 400,
-      },
-      subtitle2: {
-        fontWeight: 400,
-      },
-      fontFamily: ['Open Sans', 'sans-serif'].join(','),
-    },
-  });
-
-  jss
-    .use(jssGlobal.default())
-    .use(jssNested.default())
-    .createStyleSheet({
-      '@global': {
-        // TODO: * seems to break types
-        '*': {
-          padding: 0,
-          margin: 0,
-        },
-      } as any,
-    })
-    .attach();
-
-  const Resume = ({ data }) => (
-    <MuiThemeProvider theme={theme}>
-      <Page data={data} />
-    </MuiThemeProvider>
+  return (
+    <div>
+      Test
+    </div>
   );
-  return Resume;
 }
