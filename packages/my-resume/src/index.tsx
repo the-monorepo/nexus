9b620640baea967c46095d29691ea8efa978d629
Patrick Shaw <psha67@student.monash.edu>
2019-03-27 03:10:04 +1100

    Managed to use JSX without actually using React
    This is the coolest thing i've learnt in a few weeks

--- packages/my-resume/src/index.tsx
+++ packages/my-resume/src/index.tsx
@@ -1,8 +1,7 @@
 import { createResume } from '@pshaw/resume-template';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 
 import * as data from './data';
 
-const Resume = createResume();
-ReactDOM.render(<Resume data={data} />, document.getElementById('root'));
+const resumeElement = createResume(data);
+const rootElement = document.getElementById('root');
+rootElement!.appendChild(resumeElement);
