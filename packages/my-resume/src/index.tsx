3776cb3ea8e00da394aa83dfa37d1cb3134c62d8
Patrick Shaw <mail@patrickshaw.me>
2019-03-28 16:25:33 +1100

    No longer referring to elements by their HTML name, but rather their class constructor

--- packages/my-resume/src/index.tsx
+++ packages/my-resume/src/index.tsx
@@ -4,4 +4,6 @@ import * as data from './data';
 
 const Resume = createResume();
 const rootElement = document.getElementById('root');
-rootElement!.appendChild(<Resume data={data}/>);
+const resumeElement = <Resume data={data}/>;
+console.log(resumeElement);
+rootElement!.appendChild(resumeElement);
