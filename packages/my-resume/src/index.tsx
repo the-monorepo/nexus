f71f1e450710538ce5a415e72bc4ddd3cb4f0093
Patrick Shaw <mail@patrickshaw.me>
2019-04-02 20:38:37 +1100

    Observable array rendering is actually working

--- packages/my-resume/src/index.tsx
+++ packages/my-resume/src/index.tsx
@@ -9,20 +9,65 @@ import * as mbx from 'mobx-dom';
 import { map, MobxElement } from 'mobx-dom';
 import { observable, action, autorun, isObservableArray } from 'mobx';
 
+/*
 const store = observable({
-  arr: observable.array()
+  arr: observable.array([])
 });
-map(store.arr, () => {});
-
 setInterval(action(() => {
-  store.arr.push('1');
-  console.log('update');
+  const add = Math.round(Math.random()) === 0;
+  if (add) {
+    store.arr.push(observable.array());
+  } else {
+    if(store.arr.length > 0) {
+      store.arr.pop();
+    }
+  }
+  store.arr.forEach((inner) => {
+    const append = Math.round(Math.random()) === 0;
+    if (append) {
+      inner.push(0);
+    } else {
+      if(inner.length > 0) {
+        inner.pop();
+      }
+    }
+    for(let i = 0; i< inner.length; i++) {
+      inner[i] = Math.round(Math.random());
+    }
+  });
+}));
+class Test extends MobxElement {  
+  render() {
+    return <>
+      <div>This should be at the top</div>
+      {map(store.arr, (inner) => <div>{map(inner, (v) => <span style={{ width: '10px', height: '10px', color: v === 0 ? 'red' : 'green' }}></span>)}</div>)}
+      <div>This should be at the bottom</div>
+    </>;
+
+  }
+}
+*/
+let i = 0;
+const store = observable({
+  arr: observable.array([])
+});
+setInterval(action(() => {
+  if(i % 2 === 0) {
+    store.arr.push(i);
+  } else {
+    store.arr.pop();
+  }
+  console.warn('update');
+  i++;
 }), 1000);
 class Test extends MobxElement {  
   render() {
-    return <> 
-      {store.arr ? map(store.arr, (v) => <span>{v}</span>) : null}
-    </>
+    return <>
+      <div>This should be at the top</div>
+      {map(store.arr, (inner) => <div>{inner}</div>)}
+      <div>This should be at the bottom</div>
+    </>;
+
   }
 }
 window.customElements.define('x-test', Test);
