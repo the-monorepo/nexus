6648343be319dc0fd11d6a9d6b3b19af69f9d589
Patrick Shaw <mail@patrickshaw.me>
2019-04-16 19:04:51 +1000

    More efficient means of mapping to components

--- packages/mobx-dom/src/index.ts
+++ packages/mobx-dom/src/index.ts
@@ -8,6 +8,55 @@ import {
   toJS,
 } from 'mobx';
 
+type ChildHookNode = {
+  next: ChildHookNode | null;
+  value: (clonedNode: Node, thisArg: any) => (DynamicCallbacks | null | undefined);
+}
+/**
+ * Basically a linked list that contains only the methods required for holding and adding 
+ * to the child hooks container
+ */
+function childHooksContainer() {
+  let head: ChildHookNode | null = null;
+  let tail: ChildHookNode | null = null;
+  function node(value): ChildHookNode {
+    return {
+      next: null,
+      value
+    }
+  }
+  function handleNoHead(value, callback) {
+    const newNode = node(value);
+    if (head) {
+      callback(newNode);
+    } else {
+      head = newNode;
+      tail = newNode;
+    }
+  }
+  return {
+    unshift(value) {
+      handleNoHead(value, (newNode) => {
+        newNode.next = head;
+        head = newNode;
+      })
+    },
+    push(value) {
+      handleNoHead(value, (newNode) => {
+        tail!.next = newNode;
+        tail = newNode;
+      })
+    },
+    [Symbol.iterator]: function * () {
+      let current = head;
+      while(current !== null) {
+        yield current.value;
+        current = current.next;
+      }
+    }
+  }
+}
+
 export abstract class MobxElement extends HTMLElement {
   private readonly shadow;
   private unmountObj;
@@ -47,6 +96,9 @@ export function render(
   before?,
   thisArg?,
 ): DynamicCallbacks | undefined {
+  if (!parent) {
+    throw new Error('Parent was not defined');
+  }
   if (renderInfo === undefined || renderInfo === null) {
     return undefined;
   } else if (textNodeTypes.has(typeof renderInfo)) {
@@ -54,54 +106,6 @@ export function render(
     parent.insertBefore(child, before);
     return {
       firstElement: child,
-    };
-  } else if (isObservableArray(renderInfo)) {
-    const firstMarker = document.createComment('');
-    parent.insertBefore(firstMarker, before);
-    const childElements: DynamicCallbacks[] = [];
-    const dispose = renderInfo.observe(changeData => {
-      if (changeData.type === 'splice') {
-        if (changeData.removedCount > 0) {
-          const stopIndex = changeData.index + changeData.removedCount;
-          const stopElement = stopIndex < childElements.length ? childElements[stopIndex].firstElement : before;
-          let currentElement = childElements[changeData.index].firstElement;
-          while(currentElement !== stopElement) {
-            const nextSibling = currentElement.nextSibling;
-            currentElement.parentNode.removeChild(currentElement);
-            currentElement = nextSibling;
-          }
-        }
-
-        let addedChildElements = [];
-        if (changeData.addedCount > 0) {
-          const fragment = document.createDocumentFragment();
-
-          addedChildElements = changeData.added.map((childInfo) => {
-            const data = render(fragment, childInfo, undefined, thisArg);
-            const firstElement = data ? data.firstElement : fragment.appendChild(document.createComment(''));
-            return { disposeReactions: data.disposeReactions, firstElement }
-          }).filter(data => !!data);
-          const elementToAddBefore = changeData.index < childElements.length ? childElements[changeData.index].firstElement : before;
-          const parentToAddTo = elementToAddBefore ? elementToAddBefore.parentNode : parent;
-          parentToAddTo.insertBefore(fragment, elementToAddBefore);
-        }
-        
-        const removed = childElements.splice(changeData.index, changeData.removedCount, ...addedChildElements);
-        for (const removedChildInfo of removed) {
-          if (removedChildInfo && removedChildInfo.disposeReactions) {
-            removedChildInfo.disposeReactions();
-          }
-        }
-      }
-    }, true);
-    return { 
-      disposeReactions: () => {
-        dispose();
-        for(const childInfo of childElements) {
-          childInfo.disposeReactions();
-        }
-      },
-      firstElement: firstMarker,
     };
   } else if (Array.isArray(renderInfo) && renderInfo.length > 0) {
     const fragment = document.createDocumentFragment();
@@ -128,48 +132,47 @@ export function render(
   }
 }
 
-function addStaticElements(parent, children, dynamic: ((...args) => DynamicCallbacks | undefined)[]) {
+function initFunctionalChild(parent: Node, child) {
+  const markerIndex = parent.childNodes.length;
+  parent.appendChild(document.createComment(''));
+  return (clonedParent, thisArg) => {
+    const beforeMarker = clonedParent.childNodes[markerIndex];
+    const afterMarker = beforeMarker.nextSibling;
+    let unmountObj;
+    const boundChild = child.bind(thisArg);
+    const reactionDisposer = reaction(
+      boundChild,
+      nextRenderInfo => {
+        removeUntilBefore(beforeMarker.nextSibling, afterMarker);
+        if (unmountObj && unmountObj.disposeReactions) {
+            unmountObj.disposeReactions();
+        }
+        unmountObj = render(
+          clonedParent,
+          nextRenderInfo,
+          afterMarker,
+          thisArg,
+        );
+      },
+      { fireImmediately: true }
+    );
+    return {
+      disposeReactions: () => {
+        reactionDisposer();
+        if (unmountObj && unmountObj.disposeReactions) {
+          unmountObj.disposeReactions();
+        }
+      },
+      firstElement: beforeMarker,
+    }
+  };
+}
+  
+function addStaticElements(parent, children, dynamic) {
   for (const child of children) {
     if (typeof child === 'function') {
-      const markerIndex = parent.childNodes.length;
-      parent.appendChild(document.createComment(''));
-      parent.appendChild(document.createComment(''));
-      dynamic.push((clonedParent, thisArg) => {
-        const beforeMarker = clonedParent.childNodes[markerIndex];
-        const afterMarker = beforeMarker.nextSibling;
-        let unmountObj;
-        const boundChild = child.bind(thisArg);
-        const reactionDisposer = reaction(
-          boundChild,
-          next => {
-            let currentElement = beforeMarker.nextSibling;
-            while(!currentElement.isSameNode(afterMarker)) {
-              const nextElement = currentElement.nextSibling;
-              clonedParent.removeChild(currentElement);
-              currentElement = nextElement;
-            }
-            if (unmountObj && unmountObj.disposeReactions) {
-                unmountObj.disposeReactions();
-            }
-            unmountObj = render(
-              clonedParent,
-              next,
-              afterMarker,
-              thisArg,
-            );
-          },
-          { fireImmediately: true },
-        );
-        return {
-          disposeReactions: () => {
-            reactionDisposer();
-            if (unmountObj && unmountObj.disposeReactions) {
-              unmountObj.disposeReactions();
-            }
-          },
-          firstElement: beforeMarker,
-        }
-      });
+      const dynamicFn = initFunctionalChild(parent, child);
+      dynamic.unshift(dynamicFn);
     } else if (textNodeTypes.has(typeof child)) {
       const element = document.createTextNode(child.toString());
       parent.appendChild(element);
@@ -180,7 +183,7 @@ function addStaticElements(parent, children, dynamic: ((...args) => DynamicCallb
       const childDynamic = (clonedParent, thisArg) => {
         return renderInfo.dynamic(clonedParent.childNodes[index], thisArg);
       }
-      dynamic.push(childDynamic);
+      dynamic.unshift(childDynamic);
     }
   }
 }
@@ -200,18 +203,21 @@ function lazyTemplateFactory(element, dynamic, getCallbackElement) {
 
 export function Fragment({ children }) {
   const element = document.createDocumentFragment();
-  const childObserveFns: any[] = [];
+  const childObserveFns = childHooksContainer();
   addStaticElements(element, children, childObserveFns);
   const dynamic = (clonedElement, thisArg) => {
-      const itemsCallbacks = childObserveFns
-        .map(item => item(clonedElement, thisArg))
-        .filter(callback => !!callback);
-      return itemsCallbacks.length <= 0 ? undefined : {
-        disposeReactions: itemsCallbacks.filter(callback => !!callback.disposeReactions)
-          .forEach(itemCallbacks => itemCallbacks.disposeReactions()),
-        firstElement: itemsCallbacks[0].firstElement
-      };
+    const itemsCallbacks: any[] = [];
+    for(const itemCallbacks of childObserveFns) {
+      if(itemCallbacks) {
+        itemsCallbacks.push(itemCallbacks(clonedElement, thisArg));
+      }
+    }
+    return itemsCallbacks.length <= 0 ? undefined : {
+      disposeReactions: itemsCallbacks.filter(callback => !!callback.disposeReactions)
+        .forEach(itemCallbacks => itemCallbacks.disposeReactions()),
+      firstElement: itemsCallbacks[0].firstElement
     };
+  };
   const getElementFromTemplate = (templateContent) => templateContent;
   return {
     renderInfo: {
@@ -412,13 +418,13 @@ export function createElement(component, jsxAttributeObj = {}, ...children) {
       }
     };
   } else if (typeof component === 'string' || component.prototype instanceof Node) {
-    const childObserveFns: ((...args) => DynamicCallbacks)[] = [];
+    const childObserveFns = childHooksContainer();
     const element =
       component.prototype instanceof Node
         ? new component()
         : document.createElement(component);
-    addStaticAttributes(element, staticAttrs);
     addStaticElements(element, children, childObserveFns);
+    addStaticAttributes(element, staticAttrs);
     const dynamic = (clonedElement, thisArg) => {
       for(const name in staticProps) {
         clonedElement[name] = staticProps[name];
@@ -428,11 +434,12 @@ export function createElement(component, jsxAttributeObj = {}, ...children) {
         thisArg,
         dynamicFields,
       );
-      const itemsCallbacks = childObserveFns
-        .map(item => {
-          return item(clonedElement, thisArg);
-        })
-        .filter(callback => !!callback);
+      const itemsCallbacks: any[] = [];
+      for(const itemCallbacks of childObserveFns) {
+        if(itemCallbacks) {
+          itemsCallbacks.push(itemCallbacks(clonedElement, thisArg));
+        }
+      }  
       return {
         firstElement: clonedElement, 
         disposeReactions: () => {
@@ -464,9 +471,86 @@ export function createElement(component, jsxAttributeObj = {}, ...children) {
         }
         return obj;
       }, {}) : jsxAttributeObj;
+    return component({ children, ...unwrappedAttributes });
+  }
+}
+
+function removeUntilBefore(startElement, stopElement) {
+  let currentElement = startElement;
+  while(currentElement !== stopElement) {
+    const nextSibling = currentElement.nextSibling;
+    currentElement.parentNode.removeChild(currentElement);
+    currentElement = nextSibling;
+  }
+}
+
+function removePart(changeData, childElements) {
+  const stopIndex = changeData.index + changeData.removedCount; 
+  const stopElement = stopIndex < childElements.length ? childElements[stopIndex].firstElement : before;
+  let currentElement = childElements[changeData.index].firstElement;
+  while(currentElement !== stopElement) {
+    const nextSibling = currentElement.nextSibling;
+    currentElement.parentNode.removeChild(currentElement);
+    currentElement = nextSibling;
+  }
+}
+
+export function repeat(arr, mapFn) {
+  const marker = document.createComment('');
+  const dynamic = (firstMarker, thisArg) => {
+    const before = firstMarker.nextSibling;
+    const childElements = [];
+    const dispose = arr.observe((changeData) => {
+      switch(changeData.type) {
+        case 'splice': 
+          if (changeData.removedCount > 0) {
+            removePart(changeData, childElements);
+          }
+          const removed = childElements.splice(changeData.index, changeData.removedCount);
+          for (const removedChildInfo of removed) {
+            if (removedChildInfo && removedChildInfo.disposeReactions) {
+              removedChildInfo.disposeReactions();
+            }
+          }
     
-    const unwrappedChildren = children.map(child => typeof child === 'function' ? child : child);
-    const result = component({ children: unwrappedChildren, ...unwrappedAttributes });
-    return result;
+          let addedChildElements = [];
+          if (changeData.addedCount > 0) {
+            const fragment = document.createDocumentFragment();
+    
+            addedChildElements = changeData.added.map((data) => {
+              const dyanmicFn = render(fragment, mapFn(data), undefined, thisArg);
+              return dyanmicFn;
+            });
+            const elementToAddBefore = changeData.index < childElements.length ? childElements[changeData.index].firstElement : before;
+            firstMarker.parentNode.insertBefore(fragment, elementToAddBefore);
+          }
+          
+          childElements.splice(changeData.index, 0, ...addedChildElements);
+          break;
+        case 'update':
+          const oldValue = changeData.oldValue;
+          if (oldValue && oldValue.disposeReactions) {
+            oldValue.disposeReactions();
+          }
+          childElements.splice(changeData.index, 1, changeData.newValue);
+      }
+    });
+    return {
+      disposeReactions: () => {
+        dispose();
+        for(const childInfo of childElements) {
+          if(childInfo && childInfo.disposeReactions) {
+            childInfo.disposeReactions();
+          }
+        }
+      }
+    };
+  }
+  return {
+    renderInfo: {
+      template: lazyTemplateFactory(marker, dynamic, (templateContent) => templateContent.childNodes[0]),
+      element: marker,
+      dynamic
+    }
   }
 }
