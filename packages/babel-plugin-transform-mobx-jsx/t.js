c98fd7ea4dbdda3479db7e176bda2528061b5636
Patrick Shaw <mail@patrickshaw.me>
2019-04-23 05:33:25 +1000

    HTML and templates are now generated inside clients

--- packages/babel-plugin-transform-mobx-jsx/t.js
+++ packages/babel-plugin-transform-mobx-jsx/t.js
@@ -1,20 +1,11 @@
-const mbx = require('mobx-dom');
-const _component$2 = mbx.elementComponent("<div></div>");
-
-const _component$ = mbx.elementComponent("<div>Rawr! <!----></div>");
-
-const Test = ({
-  awesome,
+const Something = ({
   children
 }) => function () {
-  const _root$ = _component$.create();
+  const _template$ = mbx.elementTemplate("<div class=\"yay\">html() {\n    return HTMLDynamicChildrenMarker;\n  }</div>");
+
+  const _template$ = _root$.create();
 
   const _div$ = _root$.children[0];
-  const _marker$ = _div$.children[1];
-  return mbx.node(mbx.fields(_div$, mbx.attribute(_div$, "class", () => awesome)), mbx.children(_marker$, () => children.map(test => function () {
-    const _root$2 = _component$2.create();
-
-    const _div$2 = _root$2.children[0];
-    return mbx.node(mbx.fields(_div$2, mbx.attribute(_div$2, "rawr", () => test)));
-  }())));
+  const _marker$ = _div$.children[0];
+  return mbx.dynamicNode(_root$, mbx.children(_marker$, () => children));
 }();
