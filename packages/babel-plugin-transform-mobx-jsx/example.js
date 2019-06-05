5dcf3f3ce91a2a6e00c596bedaeef0396f97ae72
Patrick Shaw <psha67@student.monash.edu>
2019-06-06 06:57:53 +1000

    Reworked mobx-dom babel package and removed markers
    Should see memory benefits + faster build times (less hacky code)

--- packages/babel-plugin-transform-mobx-jsx/example.js
+++ packages/babel-plugin-transform-mobx-jsx/example.js
@@ -1,5 +1,5 @@
 import * as mbx from './mobx-dom';
-import { render } from './mobx-dom';
+import { render, repeat } from './mobx-dom';
 
 const adjectives = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean", "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable", "important", "inexpensive", "cheap", "expensive", "fancy"];
 const colours = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"];
@@ -9,91 +9,117 @@ function random(max) {
     return Math.round(Math.random() * 1000) % max;
 }
 
-class RowData {
-    constructor(id, label) {
-        this.label = label;
-        this.id = id;
-        this.isSelected = false;
-    }
-}
-
 let rowId = 1;
+const data = [];
 const buildData = (count = 1000) => {
-    const data = [];
-    while(data.length < count) {
-        const row = new RowData(
-            rowId++, 
-            adjectives[random(adjectives.length)] +
+    const newData = [];
+    while(newData.length < count) {
+        const row = {
+            id: rowId++, 
+            label: adjectives[random(adjectives.length)] +
                 " " +
                 colours[random(colours.length)] +
                 " " +
-                nouns[random(nouns.length)]
-        );
-        data.push(row);
+                nouns[random(nouns.length)],
+        };
+        newData.push(row);
     }
-    return data;
-};
-const store = {
-    data: [],
-    selected: null,
+    return newData;
 };
 
-const updateData = () => {
-    const data = store.data;
-    for (let i = 0; i < data.length; i += 10) {
-        data[i].label = data[i].label + ' !!!';
-    }
-};
 const deleteRow = (id) => {
-    const data = store.data;
     const idx = data.findIndex((d) => d.id === id);
 
     data.splice(idx, 1);
+    rerender();
 };
 
-const run = () => {
-    store.data.splice(0, store.data.length, buildData(1000));
-    store.selected = null;
+const run = (e) => {
+    e.preventDefault();
+    data.splice(0, data.length, ...buildData(1000));
     rerender();
 };
-const add = () => {
-    store.data.splice(store.data.length, 0, ...buildData(1000))
+const add = (e) => {
+    e.preventDefault();
+    data.splice(data.length, 0, ...buildData(1000))
     rerender();
 };
-const update = () => {
-  updateData()
+const update = (e) => {
+  e.preventDefault();
+  for (let i = 0; i < data.length; i += 10) {
+    data[i].label += ' !!!';
+  }
   rerender();
 };
-const select = (row) => {
-    if(store.selected) {
-        store.selected.isSelected = false;
+
+let selectedRow = null;
+const select = (id) => {
+    if(selectedRow !== null) {
+        selectedRow.selected = false;
     }
-    store.selected = row;
-    row.isSelected = true;
+    selectedRow = data.find(row => row.id === id);
+    selectedRow.selected = true;
     rerender();
 };
-const runLots = () =>{
+
+const runLots = (e) => {
+    e.preventDefault();
     const newData = buildData(10000);
-    store.data.replace(newData);
-    store.selected = null;        
+    data.splice(0, data.length, ...newData);
     rerender();
 };
-const clear = () => {
-    store.data.clear();
-    store.selected = null;
+const clear = (e) => {
+    e.preventDefault();
+    data.splice(0, data.length);
     rerender();
 };
-const swapRows = () => {
-    const data = store.data;
+const swapRows = (e) => {
+    e.preventDefault();
     if(data.length > 998) {
-        const a = store.data[1];
+        const a = data[1];
         data[1] = data[998];
         data[998] = a;
     }
     rerender();
 };
 
-const Main = ({ store }) => (
+const idKeyFn = ({ id }) => id;
+
+const getAction = (node, stopNode) => {
+    while(node !== stopNode) {
+        const action = node.dataset.action;
+        if (action !== undefined) {
+            return action;
+        }
+        node = node.parentNode;
+    }
+};
+const rowClick = e => {
+  e.preventDefault();
+  const tr = e.target.closest('tr');
+  const action = getAction(e.target, tr);
+  if (action === 'delete') {
+    const id = parseInt(tr.id);
+    deleteRow(id);        
+  } else if (action === 'select') {
+    const id = parseInt(tr.id);
+    select(id);
+  }
+};
+
+const Row = ({ id, label, selected }) => (
+    <tr class={selected ? 'danger' : null} id={id}>
+        <td class="col-md-1" $textContent={id}/>
+        <td class="col-md-4">
+            <a data-action='select' $textContent={label}/>
+        </td>
+        <td class="col-md-1"><a data-action='delete'><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a></td>
+        <td class="col-md-6"/>
+    </tr>
+);
+
+
+const Main = () => (
   <div class="container">
     <div class="jumbotron">
         <div class="row">
@@ -125,25 +151,13 @@ const Main = ({ store }) => (
         </div>
     </div>
     <table class="table table-hover table-striped test-data">
-      <tbody>
-        {store.data.map((data) => {
-            const id = data.id;
-            return (
-              <tr class={data.isSelected ? 'danger' : null}>
-                  <td class="col-md-1" $textContent={id}/>
-                  <td class="col-md-4">
-                      <a $$click={() => { select(data) }} $textContent={data.label}/>
-                  </td>
-                  <td class="col-md-1"><a $$click={() => deleteRow(id)}><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a></td>
-                  <td class="col-md-6"/>
-              </tr>
-            );          
-        })}
+      <tbody $$click={rowClick}>
+        {repeat(data, idKeyFn, Row)}
       </tbody>
     </table>
     <span class="preloadicon glyphicon glyphicon-remove" aria-hidden="true"></span>
   </div>
 );
 
-const rerender = () => render(<Main store={store}/>, document.getElementById('main'));
+const rerender = () => render(<Main />, document.getElementById('main'));
 rerender();
--- packages/babel-plugin-transform-mobx-jsx/t.js
+++ packages/babel-plugin-transform-mobx-jsx/t.js
@@ -1,7 +1,7 @@
 "use strict";
 
 import * as mbx from './mobx-dom';
-import { render } from './mobx-dom';
+import { render, repeat } from './mobx-dom';
 const adjectives = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean", "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable", "important", "inexpensive", "cheap", "expensive", "fancy"];
 const colours = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"];
 const nouns = ["table", "chair", "house", "bbq", "desk", "car", "pony", "cookie", "sandwich", "burger", "pizza", "mouse", "keyboard"];
@@ -10,91 +10,81 @@ function random(max) {
   return Math.round(Math.random() * 1000) % max;
 }
 
-class RowData {
-  constructor(id, label) {
-    this.label = label;
-    this.id = id;
-    this.isSelected = false;
-  }
-
-}
-
 let rowId = 1;
+const data = [];
 
 const buildData = (count = 1000) => {
-  const data = [];
+  const newData = [];
 
-  while (data.length < count) {
-    const row = new RowData(rowId++, adjectives[random(adjectives.length)] + " " + colours[random(colours.length)] + " " + nouns[random(nouns.length)]);
-    data.push(row);
+  while (newData.length < count) {
+    const row = {
+      id: rowId++,
+      label: adjectives[random(adjectives.length)] + " " + colours[random(colours.length)] + " " + nouns[random(nouns.length)]
+    };
+    newData.push(row);
   }
 
-  return data;
-};
-
-const store = {
-  data: [],
-  selected: null
-};
-
-const updateData = () => {
-  const data = store.data;
-
-  for (let i = 0; i < data.length; i += 10) {
-    data[i].label = data[i].label + ' !!!';
-  }
+  return newData;
 };
 
 const deleteRow = id => {
-  const data = store.data;
   const idx = data.findIndex(d => d.id === id);
   data.splice(idx, 1);
-};
-
-const run = () => {
-  store.data.splice(0, store.data.length, buildData(1000));
-  store.selected = null;
   rerender();
 };
 
-const add = () => {
-  store.data.splice(store.data.length, 0, ...buildData(1000));
+const run = e => {
+  e.preventDefault();
+  data.splice(0, data.length, ...buildData(1000));
   rerender();
 };
 
-const update = () => {
-  updateData();
+const add = e => {
+  e.preventDefault();
+  data.splice(data.length, 0, ...buildData(1000));
   rerender();
 };
 
-const select = row => {
-  if (store.selected) {
-    store.selected.isSelected = false;
+const update = e => {
+  e.preventDefault();
+
+  for (let i = 0; i < data.length; i += 10) {
+    data[i].label += ' !!!';
   }
 
-  store.selected = row;
-  row.isSelected = true;
   rerender();
 };
 
-const runLots = () => {
+let selectedRow = null;
+
+const select = id => {
+  if (selectedRow !== null) {
+    selectedRow.selected = false;
+  }
+
+  selectedRow = data.find(row => row.id === id);
+  selectedRow.selected = true;
+  rerender();
+};
+
+const runLots = e => {
+  e.preventDefault();
   const newData = buildData(10000);
-  store.data.replace(newData);
-  store.selected = null;
+  data.splice(0, data.length, ...newData);
   rerender();
 };
 
-const clear = () => {
-  store.data.clear();
-  store.selected = null;
+const clear = e => {
+  e.preventDefault();
+  data.splice(0, data.length);
   rerender();
 };
 
-const swapRows = () => {
-  const data = store.data;
+const swapRows = e => {
+  e.preventDefault();
 
   if (data.length > 998) {
-    const a = store.data[1];
+    const a = data[1];
     data[1] = data[998];
     data[998] = a;
   }
@@ -102,59 +92,77 @@ const swapRows = () => {
   rerender();
 };
 
-const _td$propertySetter = (_element, _value) => _element.textContent = _value;
+const idKeyFn = ({
+  id
+}) => id;
 
-const _a$propertySetter = (_element2, _value2) => _element2.textContent = _value2;
+const getAction = (node, stopNode) => {
+  while (node !== stopNode) {
+    const action = node.dataset.action;
 
-const _template$ = mbx.elementTemplate("<tr><td class=\"col-md-1\"></td><td class=\"col-md-4\"><a></a></td><td class=\"col-md-1\"><a><span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span></a></td><td class=\"col-md-6\"></td></tr>", _root => {
-  const _td$ = _root.firstChild;
-  const _td$2 = _root.childNodes[1];
+    if (action !== undefined) {
+      return action;
+    }
+
+    node = node.parentNode;
+  }
+};
+
+const rowClick = e => {
+  e.preventDefault();
+  const tr = e.target.closest('tr');
+  const action = getAction(e.target, tr);
+
+  if (action === 'delete') {
+    const id = parseInt(tr.id);
+    deleteRow(id);
+  } else if (action === 'select') {
+    const id = parseInt(tr.id);
+    select(id);
+  }
+};
+
+const _textContent$setter = (_element, _value) => _element.textContent = _value;
+
+const _template = mbx.elementTemplate("<tr><td class=\"col-md-1\"></td><td class=\"col-md-4\"><a data-action=\"select\"></a></td><td class=\"col-md-1\"><a data-action=\"delete\"><span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span></a></td><td class=\"col-md-6\"></td></tr>", _rootNode => {
+  const _tr$ = _rootNode;
+  const _td$ = _tr$.firstChild;
+  const _td$2 = _td$.nextSibling;
   const _a$ = _td$2.firstChild;
-  const _td$3 = _root.childNodes[2];
-  const _a$2 = _td$3.firstChild;
-  return [mbx.attribute(_root, "class"), mbx.property(_td$, _td$propertySetter), mbx.event(_a$, "click"), mbx.property(_a$, _a$propertySetter), mbx.event(_a$2, "click")];
+  return [mbx.attribute(_tr$, "class"), mbx.attribute(_tr$, "id"), mbx.property(_td$, _textContent$setter), mbx.property(_a$, _textContent$setter)];
 });
 
-const _template$2 = mbx.elementTemplate("<div class=\"container\"><div class=\"jumbotron\"><div class=\"row\"><div class=\"col-md-6\"><h1>React + Mobx</h1></div><div class=\"col-md-6\"><div class=\"row\"><div class=\"col-sm-6 smallpad\"><button type=\"button\" class=\"btn btn-primary btn-block\" id=\"run\">Create 1,000 rows</button></div><div class=\"col-sm-6 smallpad\"><button type=\"button\" class=\"btn btn-primary btn-block\" id=\"runlots\">Create 10,000 rows</button></div><div class=\"col-sm-6 smallpad\"><button type=\"button\" class=\"btn btn-primary btn-block\" id=\"add\">Append 1,000 rows</button></div><div class=\"col-sm-6 smallpad\"><button type=\"button\" class=\"btn btn-primary btn-block\" id=\"update\">Update every 10th row</button></div><div class=\"col-sm-6 smallpad\"><button type=\"button\" class=\"btn btn-primary btn-block\" id=\"clear\">Clear</button></div><div class=\"col-sm-6 smallpad\"><button type=\"button\" class=\"btn btn-primary btn-block\" id=\"swaprows\">Swap Rows</button></div></div></div></div></div><table class=\"table table-hover table-striped test-data\"><tbody><!----></tbody></table><span class=\"preloadicon glyphicon glyphicon-remove\" aria-hidden=\"true\"></span></div>", _root2 => {
-  const _div$ = _root2.firstChild;
+const Row = ({
+  id,
+  label,
+  selected
+}) => mbx.componentResult(_template, [selected ? 'danger' : null, id, id, label]);
+
+const _template2 = mbx.elementTemplate("<div class=\"container\"><div class=\"jumbotron\"><div class=\"row\"><div class=\"col-md-6\"><h1>React + Mobx</h1></div><div class=\"col-md-6\"><div class=\"row\"><div class=\"col-sm-6 smallpad\"><button type=\"button\" class=\"btn btn-primary btn-block\" id=\"run\">Create 1,000 rows</button></div><div class=\"col-sm-6 smallpad\"><button type=\"button\" class=\"btn btn-primary btn-block\" id=\"runlots\">Create 10,000 rows</button></div><div class=\"col-sm-6 smallpad\"><button type=\"button\" class=\"btn btn-primary btn-block\" id=\"add\">Append 1,000 rows</button></div><div class=\"col-sm-6 smallpad\"><button type=\"button\" class=\"btn btn-primary btn-block\" id=\"update\">Update every 10th row</button></div><div class=\"col-sm-6 smallpad\"><button type=\"button\" class=\"btn btn-primary btn-block\" id=\"clear\">Clear</button></div><div class=\"col-sm-6 smallpad\"><button type=\"button\" class=\"btn btn-primary btn-block\" id=\"swaprows\">Swap Rows</button></div></div></div></div></div><table class=\"table table-hover table-striped test-data\"><tbody></tbody></table><span class=\"preloadicon glyphicon glyphicon-remove\" aria-hidden=\"true\"></span></div>", _rootNode2 => {
+  const _div$ = _rootNode2;
   const _div$2 = _div$.firstChild;
   const _div$3 = _div$2.firstChild;
-  const _h1$ = _div$3.firstChild;
-  const _div$4 = _div$2.childNodes[1];
-  const _div$5 = _div$4.firstChild;
+  const _div$5 = _div$3.lastChild;
   const _div$6 = _div$5.firstChild;
-  const _button$ = _div$6.firstChild;
-  const _div$7 = _div$5.childNodes[1];
-  const _button$2 = _div$7.firstChild;
-  const _div$8 = _div$5.childNodes[2];
-  const _button$3 = _div$8.firstChild;
-  const _div$9 = _div$5.childNodes[3];
-  const _button$4 = _div$9.firstChild;
-  const _div$10 = _div$5.childNodes[4];
-  const _button$5 = _div$10.firstChild;
-  const _div$11 = _div$5.childNodes[5];
-  const _button$6 = _div$11.firstChild;
-  const _table$ = _root2.childNodes[1];
+  const _div$7 = _div$6.firstChild;
+  const _button$ = _div$7.firstChild;
+  const _div$8 = _div$7.nextSibling;
+  const _button$2 = _div$8.firstChild;
+  const _div$9 = _div$8.nextSibling;
+  const _button$3 = _div$9.firstChild;
+  const _div$10 = _div$9.nextSibling;
+  const _button$4 = _div$10.firstChild;
+  const _div$11 = _div$10.nextSibling;
+  const _button$5 = _div$11.firstChild;
+  const _div$12 = _div$6.lastChild;
+  const _button$6 = _div$12.firstChild;
+  const _table$ = _div$2.nextSibling;
   const _tbody$ = _table$.firstChild;
-  const _marker$ = _tbody$.firstChild;
-  return [mbx.event(_button$, "click"), mbx.event(_button$2, "click"), mbx.event(_button$3, "click"), mbx.event(_button$4, "click"), mbx.event(_button$5, "click"), mbx.event(_button$6, "click"), mbx.children(_marker$)];
+  return [mbx.event(_button$, "click"), mbx.event(_button$2, "click"), mbx.event(_button$3, "click"), mbx.event(_button$4, "click"), mbx.event(_button$5, "click"), mbx.event(_button$6, "click"), mbx.event(_tbody$, "click"), mbx.children(_tbody$, null)];
 });
 
-const Main = ({
-  store
-}) => mbx.componentResult(_template$2, [run, runLots, add, update, clear, swapRows, store.data.map(data => {
-  const id = data.id;
-  return mbx.componentResult(_template$, [data.isSelected ? 'danger' : null, id, () => {
-    select(data);
-  }, data.label, () => deleteRow(id)]);
-})]);
+const Main = () => mbx.componentResult(_template2, [run, runLots, add, update, clear, swapRows, rowClick, repeat(data, idKeyFn, Row)]);
 
-const _template$3 = mbx.elementTemplate("<!---->", _root3 => {
-  return [mbx.children(_root3)];
-});
-
-const rerender = () => render(mbx.componentResult(_template$3, [Main({
-  store: store
-})]), document.getElementById('main'));
+const rerender = () => render(Main({}), document.getElementById('main'));
 
 rerender();
