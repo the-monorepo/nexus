44a813169b3e43ecad076faa1e6e9675b24e3f5b
Patrick Shaw <psha67@student.monash.edu>
2019-07-02 00:53:54 +1000

    Added importPath functionality to fl-core

--- packages/fl-addon-core/src/index.ts
+++ packages/fl-addon-core/src/index.ts
@@ -1,6 +1,17 @@
 export const run = async () => {
   try {
-    const [nodePath, filePath, modulePath, testPath] = process.argv;
+    const [nodePath, filePath, modulePath, testPath, importPathJson] = process.argv;
+    const importPaths = JSON.parse(importPathJson);
+    for(const importPath of importPaths) {
+      const requiredModule = require(importPath);
+      if (requiredModule !== undefined) {
+        if (typeof requiredModule === 'function') {
+          await Promise.resolve(requiredModule());
+        } else if(typeof requiredModule.default === 'function') {
+          await Promise.resolve(requiredModule.default());
+        }
+      }
+    }
     const runner = require(modulePath).default;
     try {
       await Promise.resolve(runner(testPath));
