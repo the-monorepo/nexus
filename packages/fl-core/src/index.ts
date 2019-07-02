550e7041411b7c055ada4a3713038dea89ae5286
Patrick Shaw <mail@patrickshaw.me>
2019-07-03 08:18:52 +1000

    Seperated passing and failing test data types

--- packages/fl-core/src/index.ts
+++ packages/fl-core/src/index.ts
@@ -5,17 +5,18 @@ import * as types from 'fl-addon-message-types';
 import { AssertionResult, ExecutionResult, TestResult } from 'fl-addon-core';
 
 const addonEntryPath = require.resolve('./addon-entry');
-const runInSeperateProcesses = async (tester, directories, processCount, absoluteImportPaths) => {
+const runInSeperateProcesses = async (
+  tester,
+  directories,
+  processCount,
+  absoluteImportPaths,
+) => {
   await new Promise((resolve, reject) => {
     let processesStillRunning = processCount;
     const forkForTest = testPath => {
       return fork(
         addonEntryPath,
-        [
-          tester,
-          JSON.stringify([testPath]),
-          JSON.stringify(absoluteImportPaths),
-        ],
+        [tester, JSON.stringify([testPath]), JSON.stringify(absoluteImportPaths)],
         {
           env: {
             ...process.env,
@@ -47,7 +48,12 @@ const runInSeperateProcesses = async (tester, directories, processCount, absolut
   });
 };
 
-const runAndRecycleProcesses = async (tester, directories, processCount, absoluteImportPaths) => {
+const runAndRecycleProcesses = async (
+  tester,
+  directories,
+  processCount,
+  absoluteImportPaths,
+) => {
   const testsPerWorkerWithoutRemainder = Math.floor(directories.length / processCount);
   const remainders = directories.length % processCount;
   let i = 0;
@@ -64,18 +70,21 @@ const runAndRecycleProcesses = async (tester, directories, processCount, absolut
       },
     );
     return new Promise((resolve, reject) => {
-      forkTest.on('message', (message: ExecutionResult | AssertionResult | TestResult) => {
-        console.log(message);
-        switch (message.type) {
-          case types.EXECUTION:
-            console.log(testResults);
-            resolve(message.passed);
-            break;
-          case types.TEST:
-            testResults.push(message);
-            break;
-        }
-      });
+      forkTest.on(
+        'message',
+        (message: ExecutionResult | AssertionResult | TestResult) => {
+          console.log(message);
+          switch (message.type) {
+            case types.EXECUTION:
+              console.log(testResults);
+              resolve(message.passed);
+              break;
+            case types.TEST:
+              testResults.push(message);
+              break;
+          }
+        },
+      );
       forkTest.on('exit', code => {
         if (code !== 0) {
           reject(new Error('An error ocurred while running tests'));
@@ -101,22 +110,17 @@ const runAndRecycleProcesses = async (tester, directories, processCount, absolut
   await Promise.all(forkPromises);
 };
 
-export const run = async ({
-  tester,
-  testMatch,
-  setupFiles,
-}) => {
-  const directories = await globby(
-    testMatch,
-    { onlyFiles: true },
-  );
+export const run = async ({ tester, testMatch, setupFiles }) => {
+  const directories = await globby(testMatch, { onlyFiles: true });
   // We pop the paths off the end of the list so the first path thing needs to be at the end
   directories.reverse();
 
   const processIsolation = false;
-  const absoluteImportPaths = setupFiles.map(path => require.resolve(path, {
-    paths: [process.cwd()]
-  }));
+  const absoluteImportPaths = setupFiles.map(path =>
+    require.resolve(path, {
+      paths: [process.cwd()],
+    }),
+  );
 
   const processCount = cpus().length;
 
