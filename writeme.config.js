85de5105d0126a74a4902d12b860574d1e646254
Patrick Shaw <mail@patrickshaw.me>
2019-07-03 23:06:31 +1000

    Added fl-istanbul-reporter

---
+++ writeme.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  projects: {
+    overrides: [
+      {
+        category: 'Build tooling',
+        test: './{build-packages,packages}/{build-util,gulp-*}',
+      },
+      {
+        category: 'Documentation',
+        test: './{build-packages,packages}/{writeme-*,markdown-util}',
+      },
+      {
+        category: 'Logging',
+        test: './{build-packages,packages}/{winston-formats,logger}',
+      },
+      {
+        category: 'ESLint',
+        test: './{build-packages,packages}/tslint*',
+      },
+      {
+        category: 'TSLint',
+        test: './{build-packages,packages}/eslint*',
+      },
+    ],
+  },
+};
