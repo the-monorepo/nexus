d6c6b03508a7feb725f59f1687aee5100c3dbeae
Patrick Shaw <mail@patrickshaw.me>
2019-07-09 00:33:41 +1000

    Added overrideConsoleLogger.test.ts code back in
    Really need to figure out why gulp format removes file contents...

---
+++ packages/logger/test/unit/overrideConsoleLogger.test.ts
@@ -0,0 +1,46 @@
+/*
+import { stubFunctions } from 'sinon-stub-functions';
+import { logger, overrideConsoleLogger } from '../../src';
+import { testCases } from '../util/testCases';
+const mockLogger: any = stubFunctions(logger(), true);
+// Logging methods that console and logger share in common
+const loggingMethods = ['error', 'debug', 'info', 'warn'];
+**
+ * Checks which logging methods in the custom logger were actually called
+ *
+function testLoggerCallCounts(callCounts) {
+  Object.keys(mockLogger.levels).forEach(level => {
+    const callCount = callCounts[level] ? callCounts[level] : 0;
+    expect(mockLogger[level]).to.have.callCount(callCount);
+  });
+}
+**
+ * Will test if logging something through console.xxx will, in turn,
+ * call the custom logger logging methods if you use overrideConsoleLogger
+ *
+function testConsoleInput(...input) {
+  const stringifiedInput = input.map(i => JSON.stringify(i)).join();
+  describe(`input = [${stringifiedInput}]`, () => {
+    it(`logging to console.log`, () => {
+      overrideConsoleLogger(mockLogger);
+      // eslint-disable-next-line no-console
+      console.log(...input);
+      testLoggerCallCounts({ verbose: 1 });
+    });
+    loggingMethods.forEach(loggingMethodName => {
+      it(`logging to console.${loggingMethodName}}`, () => {
+        overrideConsoleLogger(mockLogger);
+        // eslint-disable-next-line no-console
+        console[loggingMethodName](...input);
+        testLoggerCallCounts({ [loggingMethodName]: 1 });
+      });
+    });
+  });
+}
+// TODO: Might be nice if we test the actual stuff that ends up being logged too
+describe('console logs to custom logger', () => {
+  for (const testCase of testCases) {
+    testConsoleInput(testCase.input);
+  }
+});
+*/
