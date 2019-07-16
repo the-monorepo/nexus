fa8dae63b83caa917137b8a695ef846325e9d8d3
Patrick Shaw <mail@patrickshaw.me>
2019-07-16 18:28:53 +1000

    Added exam benchmark method that reads from files

--- packages/fault-benchmark-exam/src/index.ts
+++ packages/fault-benchmark-exam/src/index.ts
@@ -1,10 +1,13 @@
-import { ScorelessFault } from '@fault/addon-sbfl';
+import { ScorelessFault, Fault } from '@fault/addon-sbfl';
+import { readFaultFile, convertFileFaultDataToFaults } from '@fault/record-faults';
+import { mkdirSync } from 'fs';
+import { writeFile } from 'mz/fs';
 
 export const faultToKey = (fault: ScorelessFault): string => {
   return `${fault.sourcePath}:${fault.location.start.line}:${fault.location.start.column}`;
 }
 
-export const measure = (actualFaults: ScorelessFault[], expectedFaults: ScorelessFault[], totalExecutableStatements: number) => {
+export const calculateExamScore = (actualFaults: ScorelessFault[], expectedFaults: ScorelessFault[], totalExecutableStatements: number) => {
   const expectedFaultMap: Map<string, ScorelessFault> = new Map();
   for(const fault of expectedFaults) {
     expectedFaultMap.set(faultToKey(fault), fault);
@@ -27,4 +30,28 @@ export const measure = (actualFaults: ScorelessFault[], expectedFaults: Scoreles
   return (sum / expectedFaults.length) / totalExecutableStatements;
 }
 
-export default measure;
+type BenchmarkData = {
+  [algorithmName: string]: number;
+}
+export const measureFromFiles = async (
+  actualFaultFiles: { name: string, path: string }[],
+  expectedFaultFile: string,
+  fileOutputPath: string,
+  totalExecutableStatements: number,
+) => {
+  const expectedFaultFileData = await readFaultFile(expectedFaultFile);
+
+  const expectedFaults: ScorelessFault[] = convertFileFaultDataToFaults(expectedFaultFileData);
+
+  const output: BenchmarkData = {};
+
+  for(const faultFilePath of actualFaultFiles) {
+    const faultFileData = await readFaultFile(faultFilePath.path);
+    const faults = convertFileFaultDataToFaults(faultFileData);
+    const examScore = calculateExamScore(faults, expectedFaults, totalExecutableStatements);
+    output[faultFilePath.name] = examScore;
+  }
+ 
+  mkdirSync(fileOutputPath, { recursive: true });
+  await writeFile(fileOutputPath, JSON.stringify(output), { encoding: 'utf8', flag: 'w+' });
+}
