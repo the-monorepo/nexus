1d885e347812c558fecd808c3c82291d3a753489
Patrick Shaw <mail@patrickshaw.me>
2019-07-16 16:18:56 +1000

    Completed exam benchmark algorithm

--- packages/fault-benchmark-exam/src/index.ts
+++ packages/fault-benchmark-exam/src/index.ts
@@ -1,27 +1,30 @@
-import { Fault } from '@fault/addon-sbfl';
+import { ScorelessFault } from '@fault/addon-sbfl';
 
-export const sortFaults = (faults: Fault[]) => {
-  faults.sort((a, b) => {
-    if (a.score === b.score) {
-      return 0;
-    } else if (a.score === null) {
-      return -1;
-    } else if (b.score === null) {
-      return 1;
-    } else if (a.score > b.score) {
-      return 1;
-    } else {
-      return -1;
-    }
-  });
+export const faultToKey = (fault: ScorelessFault): string => {
+  return `${fault.sourcePath}:${fault.location.start.line}:${fault.location.start.column}`;
 }
 
-export const faultToKey = (fault: Fault): string => {
-  return `${fault.location.start.line}:${fault.location.start.column}`;
-}
-
-export const measure = (actualFaults: Fault[], expectedFaults: Set<number>) => {
-  for(const fault of actualFaults) {
-    
+export const measure = (actualFaults: ScorelessFault[], expectedFaults: ScorelessFault[], totalExecutableStatements: number) => {
+  const expectedFaultMap: Map<string, ScorelessFault> = new Map();
+  for(const fault of expectedFaults) {
+    expectedFaultMap.set(faultToKey(fault), fault);
   }
+
+  let sum = 0;
+  let linesInspected = 1; // The first fault will still need to be counted as 1 line so start with 1
+
+  for(const actualFault of actualFaults) {
+    const key = faultToKey(actualFault);
+    const expectedFault = expectedFaultMap.get(key);
+    if (expectedFault !== undefined) {
+      sum += linesInspected;
+      expectedFaultMap.delete(key);
+    } else {
+      linesInspected++;
+    }
+  }
+
+  return (sum / expectedFaults.length) / totalExecutableStatements;
 }
+
+export default measure;
