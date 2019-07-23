8e72df20b562371d6704f4b6522f5f96376c2c70
Patrick Shaw <mail@patrickshaw.me>
2019-07-23 16:01:28 +1000

    Corrected expected-faults.json for bucketsort

--- packages/fault-benchmarker/src/index.ts
+++ packages/fault-benchmarker/src/index.ts
@@ -7,28 +7,29 @@ import { writeFile } from 'mz/fs';
 import { existsSync } from 'fs';
 import { readCoverageFile, getTotalExecutedStatements } from '@fault/istanbul-util';
 import * as flRunner from '@fault/runner';
-import { basename, join } from 'path';
+import { resolve } from 'path';
 import globby from 'globby';
 
-export const faultToKey = (fault: ScorelessFault): string => {
-  return `${fault.sourcePath}:${fault.location.start.line}:${fault.location.start.column}`;
+export const faultToKey = (projectDir: string, fault: ScorelessFault): string => {
+  return `${resolve(projectDir, fault.sourcePath)}:${fault.location.start.line}:${fault.location.start.column}`;
 };
 
 export const calculateExamScore = (
+  projectDir: string, 
   actualFaults: ScorelessFault[],
   expectedFaults: ScorelessFault[],
   totalExecutableStatements: number,
 ) => {
   const expectedFaultMap: Map<string, ScorelessFault> = new Map();
   for (const fault of expectedFaults) {
-    expectedFaultMap.set(faultToKey(fault), fault);
+    expectedFaultMap.set(faultToKey(projectDir, fault), fault);
   }
 
   let sum = 0;
   let linesInspected = 1; // The first fault will still need to be counted as 1 line so start with 1
 
   for (const actualFault of actualFaults) {
-    const key = faultToKey(actualFault);
+    const key = faultToKey(projectDir, actualFault);
     const expectedFault = expectedFaultMap.get(key);
     if (expectedFault !== undefined) {
       sum += linesInspected;
@@ -46,7 +47,7 @@ export type BenchmarkData = {
 };
 
 export const getProjectPaths = async () => {
-  return await globby('./projects', { onlyDirectories: true });
+  return await globby('./projects/*', { onlyDirectories: true, expandDirectories: false });
 };
 
 const sbflAlgorithmModuleNames = [
@@ -71,14 +72,14 @@ const log = logger().add(
 
 export const runOnProject = async (projectDir: string) => {
   log.verbose(`Starting ${projectDir}...`);
-  const benchmarkConfigPath = join(projectDir, 'benchmark.config.js');
+  const benchmarkConfigPath = resolve(projectDir, 'benchmark.config.js');
   const benchmarkConfigExists = existsSync(benchmarkConfigPath);
   const {
-    setupFiles = [join(__dirname, 'babel')],
-    testMatch = join(projectDir, '**/*.test.{js,jsx,ts,tsx}'),
+    setupFiles = [resolve(__dirname, 'babel')],
+    testMatch = resolve(projectDir, '**/*.test.{js,jsx,ts,tsx}'),
   }: BenchmarkConfig = benchmarkConfigExists ? require(benchmarkConfigPath) : {};
   const expectedFaults = convertFileFaultDataToFaults(
-    require(join(projectDir, 'expected-faults.json')),
+    require(resolve(projectDir, 'expected-faults.json')),
   );
 
   const projectOutput = {};
@@ -87,7 +88,7 @@ export const runOnProject = async (projectDir: string) => {
     log.verbose(`Running ${sbflModuleName} on ${projectDir}`);
     const sbflModuleFolderName = sbflModuleName.replace(/@/g, '').replace(/\/|\\/g, '-');
 
-    const sbflFaultFilePath = join(
+    const sbflFaultFilePath = resolve(
       projectDir,
       'faults',
       sbflModuleFolderName,
@@ -109,12 +110,13 @@ export const runOnProject = async (projectDir: string) => {
 
     const actualFaults = convertFileFaultDataToFaults(require(sbflFaultFilePath));
     const coverage = await readCoverageFile(
-      join(projectDir, 'coverage/coverage-final.json'),
+      resolve(projectDir, 'coverage/coverage-final.json'),
     );
 
     const totalExecutableStatements = getTotalExecutedStatements(coverage);
 
     const examScore = calculateExamScore(
+      projectDir, 
       actualFaults,
       expectedFaults,
       totalExecutableStatements,
@@ -122,9 +124,9 @@ export const runOnProject = async (projectDir: string) => {
 
     projectOutput[sbflModuleName] = examScore;
   }
-  const faultResultsPath = join(projectDir, 'fault-results.json');
-  console.log(faultResultsPath);
-  console.log(projectDir);
+  const faultResultsPath = resolve(projectDir, 'fault-results.json');
+
+  console.log(projectOutput);
   await writeFile(faultResultsPath, JSON.stringify(projectOutput, undefined, 2));
 };
 
@@ -132,7 +134,7 @@ export const run = async () => {
   const projectDirs = await getProjectPaths();
 
   for (const projectDir of projectDirs) {
-    await runOnProject(join(__dirname, '..', projectDir));
+    await runOnProject(resolve(__dirname, '..', projectDir));
   }
 };
 run().catch(console.error);
