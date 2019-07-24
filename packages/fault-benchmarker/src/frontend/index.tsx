91845b4fd3ff5eb5365fa20ca0aa9d94287db1e9
Patrick Shaw <mail@patrickshaw.me>
2019-07-25 06:44:17 +1000

    Fixed dynamic section rendering things in reverse

--- packages/fault-benchmarker/src/frontend/index.tsx
+++ packages/fault-benchmarker/src/frontend/index.tsx
@@ -3,18 +3,43 @@
 import * as mbx from 'mobx-dom';
 import benchmarkResults from '../../benchmark-results.json';
 const algorithmNames = Object.keys(benchmarkResults.average);
+
+type ProjectResult = {
+  name: string;
+  results: (number | undefined | null)[];
+}
+const projectResults: ProjectResult[] = benchmarkResults.projects.map(project => {
+  const results: (number | undefined | null)[] = [];
+  for (const algorithmName of algorithmNames) {
+    results.push(project.results[algorithmName]);
+  }
+  return {
+    name,
+    results
+  };
+});
+
 const TableHeader = () => (
   <div>
-    <div>rawr?</div>
     {
       algorithmNames.map((algorithmName) => <div>{algorithmName}</div>)
     }
-    <div>Rawr?</div>
+  </div>
+);
+
+const ProjectResult = (projectResult: ProjectResult) => (
+  <div>
+    <div>{projectResult.name}</div>
+    {projectResult.results.map((result) => <div>{result}</div>)}
   </div>
 )
+
 const Main = () => (
   <div>
     <TableHeader/>
+    {
+      projectResults.map(ProjectResult)
+    }
   </div>
 );
 
