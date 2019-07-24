0d542c4c44159a91c7169a77bc0b0dfaf54ba788
Patrick Shaw <mail@patrickshaw.me>
2019-07-24 15:19:05 +1000

    Added correct versions of each algorithm in the quixbugs suite

---
+++ packages/fault-benchmarker/projects/quixbugs-powerset/index.correct.js
@@ -0,0 +1,25 @@
+export function powerset(arr) {
+  if (arr.length > 0) {
+    const [first, ...rest] = arr;
+    const restSubsets = powerset(rest);
+    return restSubsets.concat(restSubsets.map(subset => [first].concat(subset)));
+  } else {
+      return [[]];
+  }
+}
+/*
+Power Set
+
+Input:
+arr: A list
+
+Precondition:
+arr has no duplicate elements
+
+Output:
+A list of lists, each representing a different subset of arr. The empty set is always a subset of arr, and arr is always a subset of arr.
+
+Example:
+>>> powerset(['a', 'b', 'c'])
+[[], ['c'], ['b'], ['b', 'c'], ['a'], ['a', 'c'], ['a', 'b'], ['a', 'b', 'c']]*/
+
