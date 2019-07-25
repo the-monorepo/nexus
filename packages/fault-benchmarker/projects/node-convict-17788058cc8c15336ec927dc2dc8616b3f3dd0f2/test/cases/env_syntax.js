d2fef2ceeb3dc16d1814557d1c4f3141f9edf738
Patrick Shaw <mail@patrickshaw.me>
2019-07-25 17:37:43 +1000

    Added node-convict defect to benchmarker

---
+++ packages/fault-benchmarker/projects/node-convict-17788058cc8c15336ec927dc2dc8616b3f3dd0f2/test/cases/env_syntax.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.conf = {
+  foo: {
+    bar: {
+      default: 'a',
+      format: ['a', 'b'],
+      env: 'BAR'
+    }
+  }
+};
+
+exports.env = {
+  BAR: 'c' // hey!  that's not an allowable value!
+};
---
+++ packages/fault-benchmarker/projects/node-convict-17788058cc8c15336ec927dc2dc8616b3f3dd0f2/test/cases/nesting.js
@@ -0,0 +1,14 @@
+'use strict';
+
+exports.conf = {
+  top: {
+    middle: {
+      leaf: 'foo',
+      leaf2: {
+        default: 'bar',
+        doc: 'a second leaf'
+      }
+    },
+    internal_leaf: 'baz'
+  }
+};
