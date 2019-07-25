f018d1c48cb246334590823a8e4c2a243777dfae
Patrick Shaw <mail@patrickshaw.me>
2019-07-26 05:02:22 +1000

    Fixed coverage collection race condition in mocha tester

--- packages/fault-tester-mocha/src/index.ts
+++ packages/fault-tester-mocha/src/index.ts
@@ -1,9 +1,7 @@
 import Mocha from 'mocha';
 import { IPCReporter } from './recordTests';
 import { submitFileResult } from '@fault/messages';
-import { cloneCoverage } from '@fault/istanbul-util';
-import { ParentResult, IPC } from '@fault/types';
-const COVERAGE_KEY = '__coverage__';
+import { ParentResult, IPC, RunTestPayload } from '@fault/types';
 
 export const initialize = async () => {
   const originalCacheKeys = new Set(Object.keys(require.cache));
@@ -18,35 +16,48 @@ export const initialize = async () => {
     }
   };
 
-  process.on('message', async (data: ParentResult) => {
+  const queue: RunTestPayload[] = [];
+  let running = false;
+  const runQueue = async () => {
+    if (running) {
+      return;
+    }
+    running = true;
+    while(queue.length > 0) {
+      const data = queue.pop()!;
+      const mocha = new Mocha({
+        color: true,
+        reporter: IPCReporter,
+        fullStackTrace: true,
+      } as any);
+
+      mocha.addFile(require.resolve('./recordTests'));
+      mocha.addFile(data.filePath);
+
+      try {
+        await new Promise(resolve => {
+          mocha.run(async failures => {
+            await submitFileResult(data);
+            clearCache();
+            resolve(failures);
+          });
+        });
+      } catch (err) {
+        console.error(err);
+        process.exit(1);
+      }
+    }
+    running = false;
+  }
+  process.on('message', (data: ParentResult) => {
     switch (data.type) {
       case IPC.STOP_WORKER: {
         process.exit(0);
         break;
       }
       case IPC.RUN_TEST: {
-        const mocha = new Mocha({
-          color: true,
-          reporter: IPCReporter,
-          fullStackTrace: true,
-        } as any);
-
-        mocha.addFile(require.resolve('./recordTests'));
-        mocha.addFile(data.filePath);
-
-        try {
-          await new Promise(resolve => {
-            global.beforeTestCoverage = cloneCoverage(global[COVERAGE_KEY]);
-            mocha.run(async failures => {
-              await submitFileResult(data);
-              clearCache();
-              resolve(failures);
-            });
-          });
-        } catch (err) {
-          console.error(err);
-          process.exit(1);
-        }
+        queue.push(data);
+        runQueue();
         break;
       }
     }
