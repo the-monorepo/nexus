c11e33a84d63eb4c6d344fce76dfd55fd9ea0557
Patrick Shaw <psha67@student.monash.edu>
2019-07-25 15:08:23 +1000

    Added some UI to the fault benchmarker

--- packages/fault-benchmarker/src/frontend/index.tsx
+++ packages/fault-benchmarker/src/frontend/index.tsx
@@ -1,45 +1,82 @@
 
 /// <reference path="global.d.ts" />
+import './styles.css';
 import * as mbx from 'mobx-dom';
 import benchmarkResults from '../../benchmark-results.json';
 const algorithmNames = Object.keys(benchmarkResults.average);
 
+type Result = number | undefined | null;
 type ProjectResult = {
   name: string;
-  results: (number | undefined | null)[];
+  min: number;
+  max: number;
+  results: Result[];
 }
 const projectResults: ProjectResult[] = benchmarkResults.projects.map(project => {
-  const results: (number | undefined | null)[] = [];
+  const results: Result[] = [];
   for (const algorithmName of algorithmNames) {
     results.push(project.results[algorithmName]);
   }
+  const max = Math.max(...results.filter(a => !!a) as number[]);
+  const min = Math.min(...results.filter(a => !!a) as number[]);
   return {
-    name,
+    name: project.name,
+    max: Number.isNaN(max) ? 1 : max,
+    min: Number.isNaN(min) ? 1 : min,
     results
   };
 });
 
 const TableHeader = () => (
-  <div>
+  <tr>
+    <td></td>
     {
-      algorithmNames.map((algorithmName) => <div>{algorithmName}</div>)
+      algorithmNames.map((algorithmName) => <td><p>{algorithmName}</p></td>)
     }
-  </div>
+  </tr>
 );
 
-const ProjectResult = (projectResult: ProjectResult) => (
-  <div>
-    <div>{projectResult.name}</div>
-    {projectResult.results.map((result) => <div>{result}</div>)}
-  </div>
-)
+const ProjectResult = (project: ProjectResult) => {
+  const range = project.max - project.min;
+  return (
+    <tr>
+      <td><p>{project.name}</p></td>
+      {project.results.map((result) => {
+        const colorWeight = range === 0 ? 0 : (((result ? result : 0) - project.min) / range);
+        const offset = 120;
+        const factor = (255 - offset) - 10;
+        const red = offset + factor * Math.min(1, colorWeight);
+        const green = offset + factor * Math.max(0, 1 - colorWeight);
+        const blue = offset * 0.8;
+        console.log(project.min, project.max);
+        return (
+          <td style={`background-color: rgb(${red}, ${green}, ${blue});`}>
+            <p>{result ? Math.round(result * 1000) / 1000 : 'unknown'}</p>
+          </td>
+        );
+      })}
+    </tr>
+  );
+}
+
+const averageResults: number[] = Object.values(benchmarkResults.average).map(result => result ? result : 0) as number[];
+const averageMin = Math.min(...averageResults);
+const averageMax = Math.max(...averageResults);
 
 const Main = () => (
-  <div>
-    <TableHeader/>
-    {
-      projectResults.map(ProjectResult)
-    }
+  <div class="page">
+    <header>
+      <h1>Fault.js Benchmarker</h1>
+    </header>
+    <table class='table'>
+      <tbody>
+        <TableHeader/>
+        {
+          projectResults.map(ProjectResult)
+        }
+        <ProjectResult name='Average' min={averageMin} max={averageMax} results={averageResults}/>
+      </tbody>  
+    </table>
   </div>
 );
 
