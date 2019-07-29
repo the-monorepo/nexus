72f092fb7aa22984997a109a234130840604da4e
Patrick Shaw <mail@patrickshaw.me>
2019-07-29 17:23:54 +1000

    Removed fault-results.json in projects

--- gulpfile.js
+++ gulpfile.js
@@ -128,7 +128,8 @@ async function clean() {
   await del(globBuildOutputFromPackagesDirName(packagesDirName));
   await del(globBuildOutputFromPackagesDirName(buildPackagesDirName));
   await del(['./README.md', './{build-packages,packages}/*/README.md']);
-  await del(['./packages/fault-benchmarker/projects/*/{faults,coverage}']);
+  await del(['./packages/fault-benchmarker/projects/*/{faults,coverage,fault-results.json}']);
+  await del(['./packages/fault-benchmarker/benchmark-results.json']);
 }
 gulp.task('clean', clean);
 
