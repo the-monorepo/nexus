504a73a7209225951472a0928a54067aeb50b226
Patrick Shaw <mail@patrickshaw.me>
2019-08-01 16:31:09 +1000

    Added original files to chai

--- packages/fault-benchmarker/projects/quixbugs-quicksort/babel.config.js
+++ packages/fault-benchmarker/projects/quixbugs-quicksort/babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
   plugins: ['babel-plugin-istanbul'],
 };
