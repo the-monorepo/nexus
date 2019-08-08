8ffdf62e7c218c8e525b32ac687ae882e3393ddc
Patrick Shaw <mail@patrickshaw.me>
2019-08-08 20:45:10 +1000

    Added yargs-750 defects and expectd faults

--- packages/fault-benchmarker/projects/yargs-786/yargs.js
+++ packages/fault-benchmarker/projects/yargs-786/yargs.js
@@ -935,7 +935,7 @@ function Yargs (processArgs, cwd, parentRequire) {
     options.configuration = pkgUp()['yargs'] || {}
     const parsed = Parser.detailed(args, options)
     var argv = parsed.argv
-    if (parseContext) argv = assign(argv, parseContext)
+    if (parseContext) argv = assign(parseContext, argv)
     var aliases = parsed.aliases
 
     argv.$0 = self.$0
