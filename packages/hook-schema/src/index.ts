91cf3eb3dc30893e1931023f6ed70b9a7d15877f
Patrick Shaw <psha67@student.monash.edu>
2019-09-05 04:51:14 +1000

    Fixed hook-schema

--- packages/hook-schema/src/index.ts
+++ packages/hook-schema/src/index.ts
@@ -1,54 +1,54 @@
 export type HookCallback<P extends any[] = any[], T = any> = (...params: P) => T;
 export type HookCallbackFactory<P extends any[] = any[], T = any> = () => HookCallback<
-P,
-T>;
-
+  P,
+  T
+>;
 export interface Hook<
-B extends any[] = any[],
-BR = any,
-A extends any[] = any[],
-AR = any>
-{
+  B extends any[] = any[],
+  BR = any,
+  A extends any[] = any[],
+  AR = any
+> {
   before: HookCallback<B, BR>;
-  after: HookCallback<A, AR>;}
-
+  after: HookCallback<A, AR>;
+}
 
 type NonArrayHookSchemaValue = null | HookSchema | HookCallbackFactory;
 export type HookSchemaValue =
-NonArrayHookSchemaValue |
-(NonArrayHookSchemaValue | MergeOptions)[]; // TODO: Could use a tuple but array literals don't get interpreted as tuples which causes type errors when there aren't any
+  | NonArrayHookSchemaValue
+  | (NonArrayHookSchemaValue | MergeOptions)[]; // TODO: Could use a tuple but array literals don't get interpreted as tuples which causes type errors when there aren't any
 
 export interface HookSchema {
-  [k: string]: HookSchemaValue;}
+  [k: string]: HookSchemaValue;
+}
 
+export type Hooks<H extends HookSchema> = {
+  [K in keyof H]: H[K] extends HookSchema
+    ? Hooks<H[K]>
+    : H[K] extends HookCallbackFactory
+    ? ReturnType<H[K]>
+    : HookCallback;
+};
 
-export type Hooks<H extends HookSchema> = { [K in
-keyof H]: H[K] extends HookSchema ?
-Hooks<H[K]> :
-H[K] extends HookCallbackFactory ?
-ReturnType<H[K]> :
-HookCallback };
-
-
-type RecursivePartial<T> = { [P in
-keyof T]?: T[P] extends (infer U)[] ?
-RecursivePartial<U>[] :
-T[P] extends object ?
-RecursivePartial<T[P]> :
-T[P] };
-
+type RecursivePartial<T> = {
+  [P in keyof T]?: T[P] extends (infer U)[]
+    ? RecursivePartial<U>[]
+    : T[P] extends object
+    ? RecursivePartial<T[P]>
+    : T[P];
+};
 
 export interface CompleteHooksOptions<K extends HookSchema, O extends HookSchema> {
   before: Hooks<K>;
   after: Hooks<K>;
-  on: Hooks<O>;}
-
+  on: Hooks<O>;
+}
 
 export interface HookOptions<K extends HookSchema, O extends HookSchema> {
   before?: RecursivePartial<Hooks<K>>;
   after?: RecursivePartial<Hooks<K>>;
-  on?: RecursivePartial<Hooks<O>>;}
-
+  on?: RecursivePartial<Hooks<O>>;
+}
 
 export type HookOptionsOf<T extends any> = Parameters<T['withNoops']>[0];
 export type CompleteHookOptionsOf<T extends any> = ReturnType<T['withNoops']>;
@@ -58,119 +58,119 @@ export function defaultHook(): HookCallback {
 }
 
 export function defaultHooksFromSchema<H extends HookSchema>(
-schemaObj: H,
-hooksObj: RecursivePartial<Hooks<H>> = {})
-: Hooks<H> {
-  return (Object.keys(schemaObj).reduce((hooks, key: keyof H) => {
+  schemaObj: H,
+  hooksObj: RecursivePartial<Hooks<H>> = {},
+): Hooks<H> {
+  return Object.keys(schemaObj).reduce((hooks, key: keyof H) => {
     if (!hooks[key]) {
       // TODO: There's some type errors that occur for whatever reason with as any. Need to remove them
       if (schemaObj[key] === null) {
-        hooks[key] = ((async () => {}) as any);
+        hooks[key] = (async () => {}) as any;
       } else if (typeof schemaObj[key] === 'function') {
-        hooks[key] += ((schemaObj[key] as HookCallbackFactory)() as any);
+        hooks[key] += (schemaObj[key] as HookCallbackFactory)() as any;
       } else {
-        hooks[key] = (defaultHooksFromSchema((schemaObj[key] as HookSchema), {}) as any);
+        hooks[key] = defaultHooksFromSchema(schemaObj[key] as HookSchema, {}) as any;
       }
     }
     return hooks;
-  }, hooksObj) as Hooks<H>);
+  }, hooksObj) as Hooks<H>;
 }
 
 const mergeHookCallback = (filteredHooksList: HookCallback[]) => {
   const callHooks = async (...params) => {
-    for (const callback of (filteredHooksList as HookCallback[])) {
+    for (const callback of filteredHooksList as HookCallback[]) {
       await Promise.resolve(callback(...params));
     }
   };
   return callHooks;
 };
 /**
-    * Wraps the calls of a set of hooks into a single call
-    * @param hooksList The list of hooks
-    * @param hookSchema The schema that the hooks are based off
-    */
+ * Wraps the calls of a set of hooks into a single call
+ * @param hooksList The list of hooks
+ * @param hookSchema The schema that the hooks are based off
+ */
 export function mergeHooks<H extends HookSchema>(
-hooksList: (RecursivePartial<Hooks<H>> | HookCallback | undefined)[],
-value: H | null | HookCallbackFactory | [H | null | HookCallbackFactory, MergeOptions])
-: Hooks<H> {
-  const filteredHooksList = (hooksList.filter(hooks => !!hooks) as (
-  RecursivePartial<Hooks<H>> |
-  HookCallback)[]);
+  hooksList: (RecursivePartial<Hooks<H>> | HookCallback | undefined)[],
+  value: H | null | HookCallbackFactory | [H | null | HookCallbackFactory, MergeOptions],
+): Hooks<H> {
+  const filteredHooksList = hooksList.filter(hooks => !!hooks) as (
+    | RecursivePartial<Hooks<H>>
+    | HookCallback)[];
   if (Array.isArray(value)) {
-    const arr = ((value as any) as [H | null | HookCallbackFactory, MergeOptions]);
+    const arr = (value as any) as [H | null | HookCallbackFactory, MergeOptions];
     const options: MergeOptions = arr[1];
-    const callHooks = options.yield ?
-    function* callHooks(...params) {
-      for (const callback of (filteredHooksList as HookCallback[])) {
-
-      }
-    } :
-    mergeHookCallback((filteredHooksList as HookCallback[]));
-    return (callHooks as any);
+    const callHooks = options.yield
+      ? function* callHooks(...params) {
+          for (const callback of filteredHooksList as HookCallback[]) {
+            yield Promise.resolve(callback(...params));
+          }
+        }
+      : mergeHookCallback(filteredHooksList as HookCallback[]);
+    return callHooks as any;
   } else if (typeof value === 'function' || value === null) {
-    const merged = mergeHookCallback((filteredHooksList as HookCallback[]));
+    const merged = mergeHookCallback(filteredHooksList as HookCallback[]);
     /*
-                                                                               TODO: This breaks the returned type contract, although only when the hooksList
-                                                                               is a list of hook callbacks (as opposed to a hook object)
-                                                                             */
-    return (merged as any);
+      TODO: This breaks the returned type contract, although only when the hooksList
+      is a list of hook callbacks (as opposed to a hook object)
+    */
+    return merged as any;
   } else {
-    return (Object.keys(value).reduce((merged, key) => {
-      merged[key] = mergeHooks(filteredHooksList.map(hooks => hooks[key]), (value[
-      key] as
-      any));
+    return Object.keys(value).reduce((merged, key) => {
+      merged[key] = mergeHooks(filteredHooksList.map(hooks => hooks[key]), value[
+        key
+      ] as any);
       return merged;
-    }, {}) as Hooks<H>);
+    }, {}) as Hooks<H>;
   }
 }
 
 export type MergeOptions = {
-  yield?: boolean;};
-
+  yield?: boolean;
+};
 export function mergeHookOptions<H extends HookSchema, O extends HookSchema>(
-hookOptionsList: (HookOptions<H, O> | undefined)[],
-beforeAfterSchemaObj: H,
-onSchemaObj: O = ({} as any))
-: CompleteHooksOptions<H, O> {
-  const definedHookOptionsList = (hookOptionsList.filter(hookOptions => !!hookOptions) as (
-  HookOptions<H, O> |
-  RecursivePartial<Hooks<H>>)[]);
+  hookOptionsList: (HookOptions<H, O> | undefined)[],
+  beforeAfterSchemaObj: H,
+  onSchemaObj: O = {} as any,
+): CompleteHooksOptions<H, O> {
+  const definedHookOptionsList = hookOptionsList.filter(hookOptions => !!hookOptions) as (
+    | HookOptions<H, O>
+    | RecursivePartial<Hooks<H>>)[];
   return {
     before: mergeHooks(
-    definedHookOptionsList.
-    filter(hookOptions => !!hookOptions.before).
-    map(hookOptions => (hookOptions.before as RecursivePartial<Hooks<H>>)),
-    beforeAfterSchemaObj),
-
+      definedHookOptionsList
+        .filter(hookOptions => !!hookOptions.before)
+        .map(hookOptions => hookOptions.before as RecursivePartial<Hooks<H>>),
+      beforeAfterSchemaObj,
+    ),
     after: mergeHooks(
-    definedHookOptionsList.
-    filter(hookOptions => !!hookOptions.after).
-    map(hookOptions => (hookOptions.after as RecursivePartial<Hooks<H>>)),
-    beforeAfterSchemaObj),
-
+      definedHookOptionsList
+        .filter(hookOptions => !!hookOptions.after)
+        .map(hookOptions => hookOptions.after as RecursivePartial<Hooks<H>>),
+      beforeAfterSchemaObj,
+    ),
     on: mergeHooks(
-    definedHookOptionsList.
-    filter(hookOptions => !!hookOptions.on).
-    map(hookOptions => (hookOptions.on as RecursivePartial<Hooks<O>>)),
-    onSchemaObj) };
-
-
+      definedHookOptionsList
+        .filter(hookOptions => !!hookOptions.on)
+        .map(hookOptions => hookOptions.on as RecursivePartial<Hooks<O>>),
+      onSchemaObj,
+    ),
+  };
 }
 
 export function fromSchema<H extends HookSchema, O extends HookSchema>(
-beforeAfterSchemaObj: H,
-onSchemaObj: O = ({} as any))
-{
+  beforeAfterSchemaObj: H,
+  onSchemaObj: O = {} as any,
+) {
   return {
     withNoops: (partialHooks: HookOptions<H, O> = {}): CompleteHooksOptions<H, O> => {
       return {
         on: defaultHooksFromSchema(onSchemaObj, partialHooks.on),
         before: defaultHooksFromSchema(beforeAfterSchemaObj, partialHooks.before),
-        after: defaultHooksFromSchema(beforeAfterSchemaObj, partialHooks.after) };
-
+        after: defaultHooksFromSchema(beforeAfterSchemaObj, partialHooks.after),
+      };
     },
     merge: (hookOptions: (HookOptions<H, O> | undefined)[]): CompleteHooksOptions<H, O> =>
-    mergeHookOptions(hookOptions, beforeAfterSchemaObj, onSchemaObj) };
-
+      mergeHookOptions(hookOptions, beforeAfterSchemaObj, onSchemaObj),
+  };
 }
 export default fromSchema;
