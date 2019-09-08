5f0f0f97404253f00ea34cded882d34805c0fc20
Patrick Shaw <psha67@student.monash.edu>
2019-09-09 03:35:18 +1000

    More heap tests and fixed hook schema bug

--- packages/fault-addon-mutation-localization/test/heap.test.ts
+++ packages/fault-addon-mutation-localization/test/heap.test.ts
@@ -1,7 +1,42 @@
+import { leftIndex, parentIndex, rightIndex, swap } from '../src/heap';
 import Heap from '../src/heap';
 
 describe('heap', () => {
+  it('swap', () => {
+    const arr = [1,2,3];
+    swap(arr, 0, 2);
+    expect(arr).to.be.deep.equal([3, 2, 1]);
+  });
+  it('leftIndex', () => {
+    expect(leftIndex(0)).to.be.equal(1);
+    expect(leftIndex(1)).to.be.equal(3);
+    expect(leftIndex(2)).to.be.equal(5);
+    expect(leftIndex(3)).to.be.equal(7);
+    expect(leftIndex(4)).to.be.equal(9);
+    expect(leftIndex(5)).to.be.equal(11);
+    expect(leftIndex(6)).to.be.equal(13);
+  });
+  it('rightIndex', () => {
+    expect(rightIndex(0)).to.be.equal(2);
+    expect(rightIndex(1)).to.be.equal(4);
+    expect(rightIndex(2)).to.be.equal(6);
+    expect(rightIndex(3)).to.be.equal(8);
+    expect(rightIndex(4)).to.be.equal(10);
+    expect(rightIndex(5)).to.be.equal(12);
+    expect(rightIndex(6)).to.be.equal(14);
+  })
+  it('parentIndex', () => {
+    expect(parentIndex(1)).to.be.equal(0);
+    expect(parentIndex(2)).to.be.equal(0);
+    expect(parentIndex(3)).to.be.equal(1);
+    expect(parentIndex(4)).to.be.equal(1);
+    expect(parentIndex(5)).to.be.equal(2);
+    expect(parentIndex(6)).to.be.equal(2);
+  });
+
   const testCases: [string, number[]][] = [
+    ['empty', []],
+    ['single element', [1]],
     ['already sorted', [1,2,3,4,5]],
     ['already sorted, repeating elements', [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]],
     ['unsorted', [3,2,1,5,6]],
@@ -15,7 +50,7 @@ describe('heap', () => {
   for(const [name, compareFn] of compareFns) {
     describe(name, () => {
       it('length', () => {
-        expect(new Heap(compareFn, [1,2,3,4]).length).to.be(4);
+        expect(new Heap(compareFn, [1,2,3,4]).length).to.be.equal(4);
       });
 
       it('constructed', () => {
@@ -39,7 +74,7 @@ describe('heap', () => {
 
       describe('pop', () => {
         const testSplice = (i: number | undefined) => {
-          it(i.toString(), () => {
+          it(`${i}`, () => {
             const removalArr = [-1,2,3,1,0];
             const sortedArr = [...removalArr].sort(compareFn);
             const heap = new Heap(compareFn, removalArr);
