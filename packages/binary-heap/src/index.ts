646a50e0f9059ae508a2912e0879cb1a6a3f28aa
Patrick Shaw <mail@patrickshaw.me>
2019-09-11 01:51:23 +1000

    Added location tracker to binary-heap
    Kind of more than a typical binary heap now...

--- packages/binary-heap/src/index.ts
+++ packages/binary-heap/src/index.ts
@@ -8,25 +8,26 @@ export const swap = <T>(arr: T[], i1: number, i2: number) => {
   arr[i2] = temp;
 }
 
-export const push = <T>(arr: T[], compareFn: CompareFn<T>, ...items: T[]) => {
+export const push = <T>(arr: T[], locations: Map<T, number>, compareFn: CompareFn<T>, ...items: T[]) => {
   for(const item of items) {
     const updateIndex = arr.length;
     arr[updateIndex] = item;
-    checkSwapWithParent(arr, compareFn, updateIndex);
+    checkSwapWithParent(arr, locations, compareFn, updateIndex);
   }
 }
 
-export const pop = <T>(arr: T[], compareFn: CompareFn<T>, index: number = 0) => {
+export const pop = <T>(arr: T[], locations: Map<T, number>, compareFn: CompareFn<T>, index: number = 0) => {
   swap(arr, index, arr.length - 1);
-  const poppedItem = arr.pop();
-  checkSwapWithChildren(arr, compareFn, index);
+  const poppedItem = arr.pop()!;
+  locations.delete(poppedItem);
+  checkSwapWithChildren(arr, locations, compareFn, index);
   return poppedItem;
 }
 
 /**
  * @returns true if swapped, false if not
  */
-export const checkSwapWithParent = <T>(arr: T[], compareFn: CompareFn<T>, index: number): boolean => {
+export const checkSwapWithParent = <T>(arr: T[], locations: Map<T, number>, compareFn: CompareFn<T>, index: number): boolean => {
   if (index <= 0) {
     return false;
   }
@@ -34,43 +35,53 @@ export const checkSwapWithParent = <T>(arr: T[], compareFn: CompareFn<T>, index:
   const parentI = parentIndex(index);
   const parentComparison = compareFn(arr[index], arr[parentI]);
   if (parentComparison < 0) {
+    locations.set(arr[parentI], index);
     swap(arr, index, parentI);
-    checkSwapWithParent(arr, compareFn, parentI);
+    checkSwapWithParent(arr, locations, compareFn, parentI);
     return true;
+  } else {
+    locations.set(arr[index], index);
   }
 
   return false;
 }
 
-export const checkSwapWithChildren = <T>(arr: T[], compareFn: CompareFn<T>, index: number) => {
-  const item = arr[index];
-
+export const checkSwapWithChildren = <T>(arr: T[], locations: Map<T, number>, compareFn: CompareFn<T>, index: number) => {
   const rightI = rightIndex(index);
   const leftI = leftIndex(index);
 
-  const rightIsCandidate = rightI < arr.length && compareFn(item, arr[rightI]) > 0;
-  const leftIsCandidate = leftI < arr.length && compareFn(item, arr[leftI]) > 0;
+  const rightIsCandidate = rightI < arr.length && compareFn(arr[index], arr[rightI]) > 0;
+  const leftIsCandidate = leftI < arr.length && compareFn(arr[index], arr[leftI]) > 0;
 
   if (leftIsCandidate && rightIsCandidate) {
     const swappedIndex = compareFn(arr[leftI], arr[rightI]) <= 0 ? leftI : rightI
+    locations.set(arr[swappedIndex], index);
     swap(arr, index,  swappedIndex);
-    checkSwapWithChildren(arr, compareFn, swappedIndex);
+    checkSwapWithChildren(arr, locations, compareFn, swappedIndex);
   } else if (leftIsCandidate) {
+    locations.set(arr[leftI], index);
     swap(arr, index, leftI);
-    checkSwapWithChildren(arr, compareFn, leftI);
+    checkSwapWithChildren(arr, locations, compareFn, leftI);
   } else if (rightIsCandidate) {
+    locations.set(arr[rightI], index);
     swap(arr, index, rightI);
-    checkSwapWithChildren(arr, compareFn, rightI);
+    checkSwapWithChildren(arr, locations, compareFn, rightI);
+  } else {
+    locations.set(arr[index], index);
   }
 }
 
-export const update = <T>(arr: T[], compareFn: CompareFn<T>, index: number) => {
-  const swappedWithParent = checkSwapWithParent(arr, compareFn, index);
+export const updateIndex = <T>(arr: T[], locations: Map<T, number>, compareFn: CompareFn<T>, index: number) => {
+  const swappedWithParent = checkSwapWithParent(arr, locations, compareFn, index);
   if (swappedWithParent) {
     return;
   }
-  checkSwapWithChildren(arr, compareFn, index);
+  checkSwapWithChildren(arr, locations, compareFn, index);
 }
+
+export const update = <T>(arr: T[], locations: Map<T, number>, compareFn: CompareFn<T>, item: T) => {
+  return updateIndex(arr, locations, compareFn, locations.get(item)!);
+};
 
 /**
  * Attempts to mimic the functionality of Array.prototype.sort
@@ -94,20 +105,33 @@ export const defaultCompareFn = (a: any, b: any) => {
 };
 export class Heap<T> implements Iterable<T> {
   private readonly arr: T[] = [];
+  private readonly locations: Map<T, number> = new Map();
   constructor(private readonly compareFn: CompareFn<T> = defaultCompareFn, initial: Iterable<T> = []) {
     this.push(...initial);
   }
 
   push(...item: T[]) {
-    return push(this.arr, this.compareFn, ...item);
+    return push(this.arr, this.locations, this.compareFn, ...item);
   }
 
-  update(index: number) {
-    return update(this.arr, this.compareFn, index);
+  updateIndex(index: number) {
+    return updateIndex(this.arr, this.locations, this.compareFn, index);
+  }
+
+  update(item: T) {
+    return update(this.arr, this.locations, this.compareFn, item)
   }
 
   pop(i?: number) {
-    return pop(this.arr, this.compareFn, i);
+    return pop(this.arr, this.locations, this.compareFn, i);
+  }
+
+  has(item: T) {
+    return this.locations.has(item);
+  }
+
+  position(item: T) {
+    return this.locations.get(item);
   }
 
   peek() {
