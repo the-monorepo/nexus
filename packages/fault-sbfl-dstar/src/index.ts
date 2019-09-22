57686d5f4f7ee4362c8365d2304c31034b8e9918
Patrick Shaw <mail@patrickshaw.me>
2019-09-23 04:26:40 +1000

    Fixed dstar algorithm

--- packages/fault-sbfl-dstar/src/index.ts
+++ packages/fault-sbfl-dstar/src/index.ts
@@ -8,7 +8,6 @@ export const dStar = (
   if (codeElementTestStateCounts.failed === 0) {
     return null;
   }
-  let a = {}.a.a + 6;
   return (
     Math.pow(codeElementTestStateCounts.failed, e) /
     (codeElementTestStateCounts.passed +
