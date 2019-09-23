1e8d3e69a4ca54eb1c6bd9acbbb84850c1ea767a
Patrick Shaw <mail@patrickshaw.me>
2019-09-23 16:21:36 +1000

    Readded node-convict-111 (not in a submodule)

---
+++ packages/fault-benchmarker/projects/node-convict-111/test/runner.js
@@ -0,0 +1,19 @@
+  const convict = require('../lib/convict.js'),
+  path = require('path');
+
+/*eslint no-process-exit: 0*/
+
+process.on('message', function(spec) {
+  try {
+    var s = require(path.join(__dirname, 'cases', spec.spec));
+    if (s.formats)
+      convict.addFormats(s.formats);
+    var conf = convict(s.conf).loadFile(spec.config_files).validate();
+    process.send({result: conf.get()});
+    process.exit(0);
+  } catch(e) {
+    console.error(e); // eslint-disable-line no-console
+    process.send({error: e.message });
+    process.exit(1);
+  }
+});
