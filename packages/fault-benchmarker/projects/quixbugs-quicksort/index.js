59010813fb161617b53b61eff5c3030251cfc8c4
Patrick Shaw <mail@patrickshaw.me>
2019-09-30 03:29:52 +1000

    Reverted quixbugs-quicksort defect

--- packages/fault-benchmarker/projects/quixbugs-quicksort/index.js
+++ packages/fault-benchmarker/projects/quixbugs-quicksort/index.js
@@ -3,17 +3,17 @@ export const quicksort = arr => {
     return [];
   }
   const pivot = arr[0];
-
+  const lesser = quicksort(arr.slice(1).filter(x => x < pivot));
   const greater = quicksort(arr.slice(1).filter(x => x > pivot));
   return lesser.concat(pivot).concat(greater);
 };
 
 /*
-   QuickSort
-   
-   
-   Input:
-   arr: A list of ints
-   
-   Output:
-   The elements of arr in sorted order*/
+QuickSort
+
+
+Input:
+arr: A list of ints
+
+Output:
+The elements of arr in sorted order*/
