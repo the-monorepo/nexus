f41194fee31b64c3f8b731ba0aefd76712c378b3
Patrick Shaw <mail@patrickshaw.me>
2019-09-30 13:36:52 +1000

    Added benchmarker install script

---
+++ packages/fault-benchmarker/src/install.ts
@@ -0,0 +1,36 @@
+import { exec } from 'child_process';
+import globby from 'globby';
+import { resolve } from 'path';
+
+const main = async () => {
+  const projectsDir = './projects';
+  const path = ['*'];
+  const resolved =
+    typeof path === 'string'
+      ? resolve(projectsDir, path)
+      : path.map(glob => resolve(projectsDir, glob));
+  const projectDirs = await globby(resolved, { onlyDirectories: true, expandDirectories: false });
+  for(const projectDir of projectDirs) {
+    const resolvedProjectDir = resolve(__dirname, '..', projectDir);
+    console.log(`Installing for ${resolvedProjectDir}`);
+    const packagePaths = await globby(resolve(resolvedProjectDir, 'yarn.lock'), { onlyFiles: true });
+    const command = `${packagePaths.length > 0 ? 'yarn' : 'npm'} install`;
+    console.log(command);
+    const p = exec(command, { cwd: resolvedProjectDir });
+    await new Promise((resolve, reject) => {
+        p.addListener('error', reject);
+        p.addListener('exit', (code, signal) => {
+            if (code !== 0) {
+                reject()
+            } else {
+                resolve();
+            }
+        });
+        
+        p.stdout.on('data', function (data) {
+            console.log(data.toString());
+        });
+    })
+  }
+}
+main();
