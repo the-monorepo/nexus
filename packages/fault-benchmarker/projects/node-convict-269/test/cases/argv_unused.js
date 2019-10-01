d608043a15c6aadd733981962cdddde999892788
Patrick Shaw <mail@patrickshaw.me>
2019-10-02 05:17:10 +1000

    Removed node-convict-269
    Realised its a cli-test failure whose coverage can't be picked up

--- packages/fault-benchmarker/projects/node-convict-269/test/cases/argv_unused.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-exports.conf = {
-  ip: {
-    default: '127.0.0.1',
-    format: 'ipaddress',
-    arg: 'ip-address'
-  }
-};
--- packages/fault-benchmarker/projects/node-convict-269/test/cases/formats/schema.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  data: {
-    format: '*',
-    default: undefined
-  },
-};
