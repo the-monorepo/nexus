46db5ade04a737fb84d2d48132b7f2de2632461c
Patrick Shaw <mail@patrickshaw.me>
2019-10-29 01:24:41 +1100

    Tweaked SBFL algos to return null

--- packages/fault-sbfl-tarantula/src/index.ts
+++ packages/fault-sbfl-tarantula/src/index.ts
@@ -4,6 +4,12 @@ export const tarantula = (
   codeElementTestStateCounts: Stats,
   totalTestStateCounts: Stats,
 ) => {
+  if (totalTestStateCounts.failed === 0) {
+    return null;
+  }
+  if ((codeElementTestStateCounts.failed + codeElementTestStateCounts.passed) === 0) {
+    return 0;
+  }
   return (
     (codeElementTestStateCounts.failed /
     totalTestStateCounts.failed) /
