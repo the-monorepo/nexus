bf21e6f3c722c424543fe36ba5c986d700a66ac8
Patrick Shaw <mail@patrickshaw.me>
2019-10-29 01:24:41 +1100

    Tweaked SBFL algos to return null

--- packages/fault-sbfl-barinel/src/index.ts
+++ packages/fault-sbfl-barinel/src/index.ts
@@ -1,5 +1,8 @@
 import { Stats } from '@fault/addon-sbfl';
 export const barinel = (codeElementStats: Stats) => {
+  if ((codeElementStats.passed + codeElementStats.failed === 0)) {
+    return null;
+  }
   return (
     1 - codeElementStats.passed / (codeElementStats.passed + codeElementStats.failed)
   );
