0c57b1b98125493fd1cd8789a031d05496cac457
Patrick Shaw <mail@patrickshaw.me>
2019-11-15 23:16:23 +1100

    Added quixbugs-get-factors

---
+++ faultjs/fault-benchmarker/projects/quixbugs-get-factors/index.js
@@ -0,0 +1,29 @@
+export const getFactors = (n) => {
+  if (n == 1) {
+    return [];
+  }
+  
+  for(let i = 2; i < Math.trunc(n ** 0.5) + 1; i++) {
+    if (n % i === 0) {
+      return [i].concat(getFactors(Math.trunc(n / i)));
+    }
+  }
+
+  return [n];
+}
+
+/*
+export const getFactors = (n) => {
+  if (n == 1) {
+    return [];
+  }
+  
+  for(let i = 2; i < Math.trunc(n ** 0.5) + 1; i++) {
+    if (n % i === 0) {
+      return [i].concat(get_factors(Math.trunc(n / i)));
+    }
+  }
+
+  return [n];
+}
+*/
