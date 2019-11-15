d71b15baf976501f0d24ed78d81d72b4e25d7deb
Patrick Shaw <psha67@student.monash.edu>
2019-11-16 02:41:53 +1100

    Added quixbugs-knapsack to benchmarker

---
+++ faultjs/fault-benchmarker/projects/quixbugs-knapsack/index.correct.js
@@ -0,0 +1,59 @@
+export const knapsack = (capacity, items) => {
+  const memo = [];
+
+  for(let i = 0; i <= items.length; i++) {
+    const arr = [];
+    for(let j = 0; j <= capacity; j++) {
+      arr[j] = 0;
+    }
+    memo[i] = arr;
+  }
+
+  for(let i = 1; i <= items.length; i++) {
+    const [weight, value] = items[i - 1];
+    
+    for (let j = 1; j <= capacity; j++) {
+      memo[i][j] = memo[i - 1][j];
+
+      if (weight <= j) {
+        memo[i][j] = Math.max(
+          memo[i][j],
+          value + memo[i - 1][j - weight]
+        );
+      }
+    }
+  }
+
+  return memo[items.length][capacity];
+}
+
+/*
+export const knapsack = (capacity, items) => {
+  const memo = [];
+
+  for(let i = 0; i <= items.length; i++) {
+    const arr = [];
+    for(let j = 0; j <= capacity; j++) {
+      arr[j] = 0;
+    }
+    memo[i] = arr;
+  }
+
+  for(let i = 1; i <= items.length; i++) {
+    const [weight, value] = items[i - 1];
+    
+    for (let j = 1; j <= capacity; j++) {
+      memo[i][j] = memo[i - 1][j];
+
+      if (weight <= j) {
+        memo[i][j] = Math.max(
+          memo[i][j],
+          value + memo[i - 1][j - weight]
+        );
+      }
+    }
+  }
+
+  return memo[items.length][capacity];
+}
+*/
