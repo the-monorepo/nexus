996a1798355fc5fc1c5a7500860fd17c3e86379e
Patrick Shaw <mail@patrickshaw.me>
2019-11-15 23:44:33 +1100

    Added quixbugs-is-valid-parens to benchmarker

--- faultjs/fault-benchmarker/projects/quixbugs-get-factors/index.correct.js
+++ faultjs/fault-benchmarker/projects/quixbugs-get-factors/index.correct.js
@@ -9,7 +9,7 @@ export const getFactors = (n) => {
     }
   }
 
-  return [];
+  return [n];
 }
 
 /*
---
+++ faultjs/fault-benchmarker/projects/quixbugs-is-valid-parenthesization/index.correct.js
@@ -0,0 +1,31 @@
+export const isValidParaenthesization = (parens) => {
+  let depth = 0;
+  for(const paren of parens) {
+    if(parent === '(') {
+      depth++;
+    } else {
+      depth--;
+      if(depth < 0) {
+        return false;
+      }
+    }
+  }
+  return depth === 0;
+};
+
+/*
+export const isValidParaenthesization = (parens) => {
+  let depth = 0;
+  for(const paren of parens) {
+    if(parent === '(') {
+      depth++;
+    } else {
+      depth--;
+      if(depth < 0) {
+        return false;
+      }
+    }
+  }
+  return depth === 0;
+};
+*/
