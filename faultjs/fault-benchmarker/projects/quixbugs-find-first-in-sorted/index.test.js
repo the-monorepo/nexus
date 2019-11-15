36d51953c466e8a73f5d5e43c418940085a87b12
Patrick Shaw <psha67@student.monash.edu>
2019-11-16 01:53:03 +1100

    Added quixbugs-find-first-in-sorted to benchmarker

---
+++ faultjs/fault-benchmarker/projects/quixbugs-find-first-in-sorted/index.test.js
@@ -0,0 +1,12 @@
+import { findFirstInSorted } from './index';
+import { expect } from 'chai';
+
+describe('flatten', () => {
+  it('1', () => expect(findFirstInSorted([3, 4, 5, 5, 5, 5, 6], 5)).to.equal(2));
+  it('1', () => expect(findFirstInSorted([3, 4, 5, 5, 5, 5, 6], 7)).to.equal(-1));
+  it('1', () => expect(findFirstInSorted([3, 4, 5, 5, 5, 5, 6], 2)).to.equal(-1));
+  it('1', () => expect(findFirstInSorted([3, 6, 7, 9, 9, 10, 14, 27], 14)).to.equal(6));
+  it('1', () => expect(findFirstInSorted([0, 1, 6, 8, 13, 14, 67, 128], 80)).to.equal(-1));
+  it('1', () => expect(findFirstInSorted([0, 1, 6, 8, 13, 14, 67, 128], 67)).to.equal(6));
+  it('1', () => expect(findFirstInSorted([0, 1, 6, 8, 13, 14, 67, 128], 128)).to.equal(7));
+});
