f1257fe0c01f7ae5c5ace77c1f85b62e4a1aa69b
Patrick Shaw <mail@patrickshaw.me>
2019-11-15 23:01:17 +1100

    Ran format on entire project

--- misc/html-template/src/index.tsx
+++ misc/html-template/src/index.tsx
@@ -5,8 +5,8 @@ export const Template = () => (
     &lt;!DOCTYPE html&gt;
     <html>
       <body>
-        <div id="root"/>
-        <script type="text/javascript" src="/index.js"/>
+        <div id="root" />
+        <script type="text/javascript" src="/index.js" />
       </body>
     </html>
   </>
--- monorepo.config.js
+++ monorepo.config.js
@@ -1,12 +1,14 @@
-
 module.exports = {
   buildProjects: ['build-packages'],
   projectsDirs: ['faultjs', 'misc'],
   serve: {
-    servers: [{
-      input: './faultjs/fault-benchmarker/src/frontend/index.tsx',
-    }, {
-      input: './misc/my-resume/src/index.tsx',
-    }],
+    servers: [
+      {
+        input: './faultjs/fault-benchmarker/src/frontend/index.tsx',
+      },
+      {
+        input: './misc/my-resume/src/index.tsx',
+      },
+    ],
   },
 };
