4c59d2cbd08fe3f6924558a7c0b1152fb71df146
Patrick Shaw <mail@patrickshaw.me>
2019-11-16 12:21:20 +1100

    Added quixbugs-pascal to benchmarker

---
+++ faultjs/fault-benchmarker/projects/quixbugs-pascal/index.js
@@ -0,0 +1,35 @@
+export const pascal = (n) => {
+  const rows = [[1]];
+  for(let r = 1; r < n; r++) {
+    const row = [];
+
+    for (let c = 0; c < r; c++) {
+      const upleft = c > 0 ? rows[r - 1][c - 1] : 0;
+      const upright = c < r ? rows[r - 1][c] : 0;
+      row.push(upleft + upright);
+    }
+
+    rows.push(row);
+  }
+
+  return rows;
+}
+
+/*
+export const pascal = (n) => {
+  const rows = [[1]];
+  for(let r = 1; r < n; r++) {
+    const row = [];
+
+    for (let c = 0; c < r + 1; c++) {
+      const upleft = c > 0 ? rows[r - 1][c - 1] : 0;
+      const upright = c < r ? rows[r - 1][c] : 0;
+      row.push(upleft + upright);
+    }
+
+    rows.push(row);
+  }
+
+  return rows;
+}
+*/
