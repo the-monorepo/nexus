231b0de3485f49f76b76fac3fa60dcdb94e11660
Patrick Shaw <mail@patrickshaw.me>
2019-11-17 00:09:35 +1100

    Added quixbugs-lis to benchmarker

---
+++ faultjs/fault-benchmarker/projects/quixbugs-lis/index.correct.js
@@ -0,0 +1,53 @@
+export const lis = (arr) => {
+  const ends = new Map();
+  let longest = 0;
+  
+  let i = 0;
+  for(const val of arr) {
+    const prefixLengths = [];
+    for(let j = 1; j < longest + 1; j++) {
+      if(arr[ends.get(j)] < val) {
+        prefixLengths.push(j);
+      }
+    }
+
+    const length = prefixLengths.length > 0 ? Math.max(...prefixLengths) : 0;
+
+    if((length === longest) || (val < arr[ends.get(length + 1)])) {
+      ends.set(length + 1, i);
+      longest = Math.max(longest, length + 1);
+    }
+
+    i++;
+  }
+
+  return longest;
+}
+
+/*
+export const lis = (arr) => {
+  const ends = new Map();
+  let longest = 0;
+  
+  let i = 0;
+  for(const val of arr) {
+    const prefixLengths = [];
+    for(let j = 1; j < longest + 1; j++) {
+      if(arr[ends.get(j)] < val) {
+        prefixLengths.push(j);
+      }
+    }
+
+    const length = prefixLengths.length > 0 ? Math.max(...prefixLengths) : 0;
+
+    if((length === longest) || (val < arr[ends.get(length + 1)])) {
+      ends.set(length + 1, i);
+      longest = Math.max(longest, length + 1);
+    }
+
+    i++;
+  }
+
+  return longest;
+}
+*/
