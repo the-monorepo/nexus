ef5f427eae673b5f6a30490b1b93ad8f1b236d15
Patrick Shaw <mail@patrickshaw.me>
2019-11-16 11:53:00 +1100

    Added quixbugs-bitcount

---
+++ faultjs/fault-benchmarker/projects/quixbugs-bitcount/index.js
@@ -0,0 +1,22 @@
+export const bitcount = (n) => {
+  let count = 0;
+
+  while (n !== 0) {
+    n ^= n - 1;
+    count++;
+  }
+
+  return count;
+}
+/*
+export const bitcount = (n) => {
+  let count = 0;
+
+  while (n !== 0) {
+    n &= n - 1;
+    count++;
+  }
+
+  return count;
+}
+*/
