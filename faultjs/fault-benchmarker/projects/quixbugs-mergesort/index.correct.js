bb8fec3097006b87a7446f85aa815a190cdedd7f
Patrick Shaw <psha67@student.monash.edu>
2019-11-16 23:39:44 +1100

    Added quixbugs-mergesort to benchmarker

---
+++ faultjs/fault-benchmarker/projects/quixbugs-mergesort/index.correct.js
@@ -0,0 +1,25 @@
+export const longestCommonSubsequence = (a, b) => {
+  if (a.length <= 0 || b.length <= 0) {
+    return '';
+  } else if (a[0] === b[0]) {
+    return a[0] + longestCommonSubsequence(a.slice(1), b.slice(1));
+  } else {
+    const seq1 = longestCommonSubsequence(a, b.slice(1));
+    const seq2 = longestCommonSubsequence(a.slice(1), b);
+    return seq1.length >= seq2.length ? seq1 : seq2;
+  }
+}
+
+/*
+export const longestCommonSubsequence = (a, b) => {
+  if (a.length <= 0 || b.length <= 0) {
+    return '';
+  } else if (a[0] === b[0]) {
+    return a[0] + longestCommonSubsequence(a.slice(1), b.slice(1));
+  } else {
+    const seq1 = longestCommonSubsequence(a, b.slice(1));
+    const seq2 = longestCommonSubsequence(a.slice(1), b);
+    return seq1.length >= seq2.length ? seq1 : seq2;
+  }
+}
+*/
