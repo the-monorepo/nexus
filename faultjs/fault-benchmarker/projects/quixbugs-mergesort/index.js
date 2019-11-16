93d84633b9ff35d32bc20bfb625ace6ef1fcb50b
Patrick Shaw <mail@patrickshaw.me>
2019-11-16 23:39:44 +1100

    Added quixbugs-mergesort to benchmarker

---
+++ faultjs/fault-benchmarker/projects/quixbugs-mergesort/index.js
@@ -0,0 +1,57 @@
+export const mergesort = (arr) => {
+  const merge = (left, right) => {
+    const result = [];
+    let i = 0;
+    let j = 0;
+    while (i < left.length && j < right.length) {
+      if (left[i] <= right[j]) {
+        result.push(left[i]);
+        i++;
+      } else {
+        result.push(right[j]);
+        j++;
+      }
+    }
+    result.push(...left.slice(i), ...right.slice(j));
+    return result;
+  }
+
+  if (arr.length === 0) {
+    return arr;
+  } else {
+    const middle = Math.trunc(arr.length / 2);
+    const left = mergesort(arr.slice(0, middle));
+    const right = mergesort(arr.slice(middle));
+    return merge(left, right);
+  }
+}
+
+/*
+export const mergesort = (arr) => {
+  const merge = (left, right) => {
+    const result = [];
+    let i = 0;
+    let j = 0;
+    while (i < left.length && j < right.length) {
+      if (left[i] <= right[j]) {
+        result.push(left[i]);
+        i++;
+      } else {
+        result.push(right[j]);
+        j++;
+      }
+    }
+    result.push(...left.slice(i), ...right.slice(j));
+    return result;
+  }
+
+  if(arr.length <= 1) {
+    return arr;
+  } else {
+    const middle = Math.trunc(arr.length / 2);
+    const left = mergesort(arr.slice(0, middle));
+    const right = mergesort(arr.slice(middle));
+    return merge(left, right);
+  }
+}
+*/
