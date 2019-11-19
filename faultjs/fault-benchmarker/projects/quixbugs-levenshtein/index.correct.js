e86930c8e8893af28fc8268273445fefb4f7eebb
Patrick Shaw <mail@patrickshaw.me>
2019-11-19 23:02:33 +1100

    Added quixbugs-levenshtein (without test 4)

---
+++ faultjs/fault-benchmarker/projects/quixbugs-levenshtein/index.correct.js
@@ -0,0 +1,29 @@
+export const levenshtein = (source, target) => {
+  if(source.length === 0 || target.length === 0) {
+    return source.length || target.length;
+  } else if (source[0] === target[0]) {
+    return levenshtein(source.slice(1), target.slice(1));
+  } else {
+    return Math.min(
+      levenshtein(source, target.slice(1)),
+      levenshtein(source.slice(1), target.slice(1)),
+      levenshtein(source.slice(1), target),
+    )
+  }
+}
+
+/*
+export const levenshtein = (source, target) => {
+  if(source.length === 0 || target.length === 0) {
+    return source.length || target.length;
+  } else if (source[0] === target[0]) {
+    return levenshtein(source.slice(1), target.slice(1));
+  } else {
+    return 1 + Math.min(
+      levenshtein(source, target.slice(1)),
+      levenshtein(source.slice(1), target.slice(1)),
+      levenshtein(source.slice(1), target),
+    )
+  }
+}
+*/
