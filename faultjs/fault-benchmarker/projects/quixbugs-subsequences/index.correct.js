0f08f1fb39c2360df72dace7ebeb4f6c6397574e
Patrick Shaw <mail@patrickshaw.me>
2019-11-21 12:30:22 +1100

    Added quixbugs-subsequences to benchmarker

---
+++ faultjs/fault-benchmarker/projects/quixbugs-subsequences/index.correct.js
@@ -0,0 +1,33 @@
+export const subsequences = (a, b, k) => {
+  if (k === 0) {
+    return [[]];
+  }
+
+  const ret = [];
+  for( let i = a; i < b + 1 - k; i++) {
+    ret.push(
+      ...subsequences(i + 1, b, k - 1)
+        .map(rest => [i, ...rest])
+    )
+  }
+
+  return ret;
+}
+
+/*
+export const subsequences = (a, b, k) => {
+  if (k === 0) {
+    return [[]];
+  }
+
+  const ret = [];
+  for( let i = a; i < b + 1 - k; i++) {
+    ret.push(
+      ...subsequences(i + 1, b, k - 1)
+        .map(rest => [i, ...rest])
+    )
+  }
+
+  return ret;
+}
+*/
