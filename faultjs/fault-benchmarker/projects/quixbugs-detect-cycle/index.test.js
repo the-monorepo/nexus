92f3c2524da1459fbc20de6e4d6a452d97a3808b
Patrick Shaw <mail@patrickshaw.me>
2019-11-21 23:08:05 +1100

    Added quixbugs-detect-cycle

---
+++ faultjs/fault-benchmarker/projects/quixbugs-detect-cycle/index.test.js
@@ -0,0 +1,55 @@
+import { expect } from 'chai';
+import { detectCycle } from './index';
+class Node {
+  constructor(value, successor) {
+    this.value = value;
+    this.successor = successor;
+  }
+}
+
+describe('detectCycle', () => {
+  it('1', () => {
+    const node1 = new Node("1");
+    const node2 = new Node("2", node1);
+    const node3 = new Node("3", node2);
+    const node4 = new Node("4", node3);
+    const node5 = new Node("5", node4);
+    expect(detectCycle(node5)).to.equal(false);  
+  });
+
+  it('2', () => {
+    const node1 = new Node("1");
+    const node2 = new Node("2", node1);
+    const node3 = new Node("3", node2);
+    const node4 = new Node("4", node3);
+    const node5 = new Node("5", node4);
+    node1.successor = node5;
+    expect(detectCycle(node5)).to.equal(true);  
+  });
+
+  it('3', () => {
+    const node1 = new Node("1");
+    const node2 = new Node("2", node1);
+    const node3 = new Node("3", node2);
+    const node4 = new Node("4", node3);
+    const node5 = new Node("5", node4);
+    node1.successor = node2;
+    expect(detectCycle(node2)).to.equal(true);  
+  });
+
+  it('4', () => {
+    const node1 = new Node("1");
+    const node2 = new Node("2", node1);
+    const node3 = new Node("3", node2);
+    const node4 = new Node("4", node3);
+    const node5 = new Node("5", node4);
+    const node6 = new Node("6");
+    const node7 = new Node("7", node6);
+    expect(detectCycle(node7)).to.equal(false);  
+  });
+
+  it('5', () => {
+    const node = new Node("0");
+    expect(detectCycle(node)).to.equal(false);  
+  });
+});
