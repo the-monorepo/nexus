915d97e43c632bce74b68799996fe00a805e096a
Patrick Shaw <psha67@student.monash.edu>
2019-11-23 09:52:48 +1100

    Added quixbugs-minimum-spanning-tree to benchmarker

---
+++ faultjs/fault-benchmarker/projects/quixbugs-minimum-spanning-tree/index.correct.js
@@ -0,0 +1,65 @@
+export const minimumSpanningTree = (weightByEdge) => {
+  const groupByNode = new Map();
+  const mstEdges = new Set();
+
+  weightByEdge.sort((a, b) => a[2] - b[2])
+
+  for(const edge of weightByEdge) {
+    const vertexU = edge[0];
+    const vertexV = edge[1];
+
+    if(!groupByNode.has(vertexU)) {
+      groupByNode.set(vertexU, new Set([vertexU]));
+    }
+    if(!groupByNode.has(vertexV)) {
+      groupByNode.set(vertexV, new Set([vertexV]));
+    }
+
+    if(groupByNode.get(vertexU) !== groupByNode.get(vertexV)) {
+      mstEdges.add(edge);
+      const vertexUSpan = groupByNode.get(vertexU);
+      for(const vertex of groupByNode.get(vertexV)) {
+        vertexUSpan.add(vertex);
+      }
+      for(const node of groupByNode.get(vertexV)) {
+        groupByNode.set(node, groupByNode.get(vertexU));
+      }
+    }
+  }
+  
+  return mstEdges;
+}
+
+/*
+export const minimumSpanningTree = (weightByEdge) => {
+  const groupByNode = new Map();
+  const mstEdges = new Set();
+
+  weightByEdge.sort((a, b) => a[2] - b[2])
+
+  for(const edge of weightByEdge) {
+    const vertexU = edge[0];
+    const vertexV = edge[1];
+
+    if(!groupByNode.has(vertexU)) {
+      groupByNode.set(vertexU, new Set([vertexU]));
+    }
+    if(!groupByNode.has(vertexV)) {
+      groupByNode.set(vertexV, new Set([vertexV]));
+    }
+
+    if(groupByNode.get(vertexU) !== groupByNode.get(vertexV)) {
+      mstEdges.add(edge);
+      const vertexUSpan = groupByNode.get(vertexU);
+      for(const vertex of groupByNode.get(vertexV)) {
+        vertexUSpan.add(vertex);
+      }
+      for(const node of groupByNode.get(vertexV)) {
+        groupByNode.set(node, groupByNode.get(vertexU));
+      }
+    }
+  }
+  
+  return mstEdges;
+}
+*/
