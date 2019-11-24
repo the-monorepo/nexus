f6c9799903a863342cedfd5d3a2a203c957afe50
Patrick Shaw <mail@patrickshaw.me>
2019-11-24 11:08:37 +1100

    Added quixbugs-sqrt to benchmarker

---
+++ faultjs/fault-benchmarker/projects/quixbugs-sqrt/index.correct.js
@@ -0,0 +1,19 @@
+export const sqrt = (x, epsilon) => {
+  let approx = x / 2;
+  while (Math.abs(x - approx ** 2) > epsilon) {
+    approx = 0.5 * (approx + x / approx);
+  }
+
+  return approx;
+};
+
+/*
+export const sqrt = (x, epsilon) => {
+  let approx = x / 2;
+  while (Math.abs(x - approx ** 2) > epsilon) {
+    approx = 0.5 * (approx + x / approx);
+  }
+
+  return approx;
+};
+*/
