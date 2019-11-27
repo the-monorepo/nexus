6f841465649798c64a160ebdc2ebdff39bc7e47d
PatrickShaw <mail@patrickshaw.me>
2019-11-27 13:59:07 +1100

    Added boxplot to benchmark UI

--- faultjs/fault-benchmarker/webpack.config.js
+++ faultjs/fault-benchmarker/webpack.config.js
@@ -61,7 +61,7 @@ module.exports = {
       template: HtmlWebpackTemplate,
       appMountId: 'root',
       headHtmlSnippet: [
-        'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js',
+        'https://unpkg.com/chart.js@2.8.0/dist/Chart.bundle.js',
         'https://unpkg.com/chartjs-chart-box-and-violin-plot@2/build/Chart.BoxPlot.js',
       ].map(script => `<script src="${script}" type="text/javascript"></script>`).join(''),
       links: [
