e8a27a8eacd7463613cedbad7a38f1dfd05a22de
Patrick Shaw <mail@patrickshaw.me>
2019-12-30 06:01:03 +1100

    Fixed wrapper mutation implementations not actually mutating anything

---
+++ faultjs/fault-benchmarker/projects/quixbugs-gcd/index.correct.js
@@ -0,0 +1,26 @@
+export function gcd(a, b) {
+  if (b === 0) {
+    return a;
+  } else {
+    return gcd(b, a % b);
+  }
+}
+
+/*
+Input:
+a: A nonnegative int
+b: A nonnegative int
+
+
+Greatest Common Divisor
+
+Precondition:
+isinstance(a, int) and isinstance(b, int)
+
+Output:
+The greatest int that divides evenly into a and b
+
+Example:
+>>> gcd(35, 21)
+7
+*/
