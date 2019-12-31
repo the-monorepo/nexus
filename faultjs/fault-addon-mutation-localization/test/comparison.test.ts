457bd801c31294a83b90f39313e3abd5b5f69e37
Patrick Shaw <psha67@student.monash.edu>
2019-12-31 23:44:49 +1100

    Fixed comparison.test.ts

--- faultjs/fault-addon-mutation-localization/test/comparison.test.ts
+++ faultjs/fault-addon-mutation-localization/test/comparison.test.ts
@@ -1,10 +1,10 @@
 import {
   compareMutationEvaluations,
-  compareNodeEvaluations,
+  compareEvaluationHeaps,
   compareInstruction,
   MutationEvaluation,
-  NodeEvaluation,
-  createNodeEvaluation,
+  InstructionEvaluation,
+  createInstructionEvaluation,
   Instruction,
   getAstPath,
   DependencyInfo,
@@ -12,6 +12,7 @@ import {
   pathToPrimaryKey,
   createInstructionQueue,
   createInstructionBlocks,
+  compareInstructionEvaluations,
 } from '../src/index';
 import { parse } from '@babel/parser';
 import Heap from '@pshaw/binary-heap';
@@ -32,8 +33,8 @@ const arrayToMutationEvaluation = (arr: any[]): MutationEvaluation => {
   };
 };
 
-const arrayToNodeEvaluations = (arr: any[]): NodeEvaluation => {
-  const nodeEvaluation = createNodeEvaluation(0);
+const arrayToInstructionEvaluations = (arr: any[]): InstructionEvaluation => {
+  const nodeEvaluation = createInstructionEvaluation(0);
   nodeEvaluation.mutationEvaluations.push(arrayToMutationEvaluation(arr));
   return nodeEvaluation;
 }
@@ -58,36 +59,36 @@ describe('sorting', () => {
   describe(compareMutationEvaluations.name, () => {
     for (let i = 0; i < orderedEvaluations.length - 1; i++) {
       const lesser = arrayToMutationEvaluation(orderedEvaluations[i]);
-      const lesserNodeEvaluation = arrayToNodeEvaluations(orderedEvaluations[i]);
+      const lesserNodeEvaluation = arrayToInstructionEvaluations(orderedEvaluations[i]);
       it(`${arrToString(orderedEvaluations[i])} same as ${arrToString(
         orderedEvaluations[i],
       )}`, () => {
         expect(compareMutationEvaluations(lesser, lesser)).toBe(0);
         // TODO: Should have it's own test
-        expect(compareNodeEvaluations(lesserNodeEvaluation, lesserNodeEvaluation)).toBe(0);
+        expect(compareInstructionEvaluations(lesserNodeEvaluation, lesserNodeEvaluation)).toBe(0);
       });
       for (let j = i + 1; j < orderedEvaluations.length; j++) {
         const greater = arrayToMutationEvaluation(orderedEvaluations[j]);
-        const greaterNodeEvaluation = arrayToNodeEvaluations(orderedEvaluations[j]);
+        const greaterNodeEvaluation = arrayToInstructionEvaluations(orderedEvaluations[j]);
         it(`${arrToString(orderedEvaluations[i])} less than ${arrToString(
           orderedEvaluations[j],
         )}`, () => {
           expect(compareMutationEvaluations(lesser, greater)).toBeLessThan(0);
-          expect(compareNodeEvaluations(lesserNodeEvaluation, greaterNodeEvaluation)).toBeLessThan(0);
+          expect(compareInstructionEvaluations(lesserNodeEvaluation, greaterNodeEvaluation)).toBeLessThan(0);
         });
         it(`${arrToString(orderedEvaluations[j])} greater than ${arrToString(
           orderedEvaluations[i],
         )}`, () => {
           expect(compareMutationEvaluations(greater, lesser)).toBeGreaterThan(0);
-          expect(compareNodeEvaluations(greaterNodeEvaluation, lesserNodeEvaluation)).toBeGreaterThan(0);
+          expect(compareInstructionEvaluations(greaterNodeEvaluation, lesserNodeEvaluation)).toBeGreaterThan(0);
         });
       }
     }
   });
 
   it(compareInstruction.name, () => {
-    const nodeEvaluations: Map<string, NodeEvaluation> = new Map();
-    const instructionEvaluations: Map<Instruction<any>, Heap<MutationEvaluation>> = new Map();
+    const nodeEvaluations: Map<string, Heap<MutationEvaluation>> = new Map();
+    const instructionEvaluations: Map<Instruction<any>, InstructionEvaluation> = new Map();
 
     const compareFn = (a, b) => compareInstruction(nodeEvaluations, instructionEvaluations, a, b);
     
@@ -109,11 +110,11 @@ describe('sorting', () => {
     const dependenciesMap1 = new Map([[filePath, dependencies1]]);
     const dependenciesMap2 = new Map([[filePath, dependencies2]]);
 
-    const instruction1 = new Instruction(Symbol('1'), dependenciesMap1, [], []);
-    const instruction2 = new Instruction(Symbol('2'), dependenciesMap1, [], []);
-    const instruction3 = new Instruction(Symbol('3'), dependenciesMap1, [], []);
+    const instruction1 = new Instruction<any>(Symbol('1'), dependenciesMap1, [], []);
+    const instruction2 = new Instruction<any>(Symbol('2'), dependenciesMap1, [], []);
+    const instruction3 = new Instruction<any>(Symbol('3'), dependenciesMap1, [], []);
 
-    const instruction4 = new Instruction(Symbol('4'), dependenciesMap2, [], []);
+    const instruction4 = new Instruction<any>(Symbol('4'), dependenciesMap2, [], []);
 
     const instructions = [instruction1, instruction2, instruction3, instruction4];
 
@@ -122,11 +123,11 @@ describe('sorting', () => {
     const mE1 = arrayToMutationEvaluation([1, 0, 0, 0, 0]);
     const mE3 = arrayToMutationEvaluation([0, 0, 0, 0, 1]);
 
-    instructionEvaluations.get(instruction1)!.push(mE1);
-    instructionEvaluations.get(instruction3)!.push(mE3);
-    instructionEvaluations.get(instruction4)!.push(mE1);
+    instructionEvaluations.get(instruction1)!.mutationEvaluations.push(mE1);
+    instructionEvaluations.get(instruction3)!.mutationEvaluations.push(mE3);
+    instructionEvaluations.get(instruction4)!.mutationEvaluations.push(mE1);
 
-    nodeEvaluations.get(pathToPrimaryKey(filePath, path2))!.mutationEvaluations.push(mE3);
+    nodeEvaluations.get(pathToPrimaryKey(filePath, path2))!.push(mE3);
 
     expect(compareFn(instruction2, instruction1)).toBe(1);
     expect(compareFn(instruction2, instruction2)).toBe(0);
@@ -140,11 +141,11 @@ describe('sorting', () => {
       ...createInstructionBlocks(nodeEvaluations, instructionEvaluations, instructions.map(instruction => [instruction]))
     );
 
-    const popped = instructionQueue.pop();
+    const popped = instructionQueue.pop()!;
     expect(popped.peek().type).toBe(instruction3.type);
     expect(popped.length).toBe(1);
-    expect(instructionQueue.pop().peek().type).toBe(instruction2.type);
-    expect(instructionQueue.pop().peek().type).toBe(instruction4.type);
-    expect(instructionQueue.pop().peek().type).toBe(instruction1.type);
+    expect(instructionQueue.pop()!.peek().type).toBe(instruction2.type);
+    expect(instructionQueue.pop()!.peek().type).toBe(instruction4.type);
+    expect(instructionQueue.pop()!.peek().type).toBe(instruction1.type);
   });
 });
