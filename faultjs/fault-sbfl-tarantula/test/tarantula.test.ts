d8e138f8fe30ad32c6845232f4ebad90042f65a2
Patrick Shaw <mail@patrickshaw.me>
2020-01-07 16:43:00 +1100

    Fixed tarantula tests

--- faultjs/fault-sbfl-tarantula/test/tarantula.test.ts
+++ faultjs/fault-sbfl-tarantula/test/tarantula.test.ts
@@ -1,6 +1,6 @@
 import { tarantula } from '../src/index';
 describe('tarantula', () => {
   it('0 faults', () => {
-    expect(tarantula({ passed: 0, failed: 0 }, { passed: 10, failed: 111 })).toBe(0);
+    expect(tarantula({ passed: 0, failed: 0 }, { passed: 10, failed: 111 })).toBe(Number.NEGATIVE_INFINITY);
   });
 });
