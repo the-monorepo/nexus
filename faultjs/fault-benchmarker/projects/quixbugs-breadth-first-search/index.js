36d01ff07a4513a7f93607513bc9f2e188f2a215
PatrickShaw <mail@patrickshaw.me>
2020-01-17 01:43:46 +1100

    Fixed identifier being used before declaration

--- faultjs/fault-benchmarker/projects/quixbugs-breadth-first-search/index.js
+++ faultjs/fault-benchmarker/projects/quixbugs-breadth-first-search/index.js
@@ -7,47 +7,47 @@ export const breadthFirstSearch = (startNode, goalNode) => {
 
   while (true) {
     const node = queue.shift();
-    
+
     if (node === goalNode) {
       return true;
     } else {
-      for(const successor of node.successors) {
-        if(nodesSeen.has(successor)) {
+      for (const successor of node.successors) {
+        if (nodesSeen.has(successor)) {
           continue;
         }
         queue.push(successor);
-        nodesSeen.add(successor);
+        nodesSeen.has(successor);
       }
     }
   }
 
   return false;
-}
+};
 
 /*
-export const breadthFirstSearch = (startNode, goalNode) => {
-  const queue = [];
-  queue.push(startNode);
-
-  const nodesSeen = new Set();
-  nodesSeen.add(startNode);
-
-  while (queue.length > 0) {
-    const node = queue.shift();
-    
-    if (node === goalNode) {
-      return true;
-    } else {
-      for(const successor of node.successors) {
-        if(nodesSeen.has(successor)) {
-          continue;
-        }
-        queue.push(successor);
-        nodesSeen.add(successor);
-      }
-    }
-  }
-
-  return false;
-}
-*/
+   export const breadthFirstSearch = (startNode, goalNode) => {
+     const queue = [];
+     queue.push(startNode);
+   
+     const nodesSeen = new Set();
+     nodesSeen.add(startNode);
+   
+     while (queue.length > 0) {
+       const node = queue.shift();
+       
+       if (node === goalNode) {
+         return true;
+       } else {
+         for(const successor of node.successors) {
+           if(nodesSeen.has(successor)) {
+             continue;
+           }
+           queue.push(successor);
+           nodesSeen.add(successor);
+         }
+       }
+     }
+   
+     return false;
+   }
+   */
