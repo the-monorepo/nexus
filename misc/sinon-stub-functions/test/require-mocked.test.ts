87be7d0f81456b88bcf64b955c93ee408e389596
Patrick Shaw <mail@patrickshaw.me>
2020-01-18 17:07:16 +1100

    Fixed sinon-stub-functions test

--- misc/sinon-stub-functions/test/require-mocked.test.ts
+++ misc/sinon-stub-functions/test/require-mocked.test.ts
@@ -2,10 +2,10 @@ import rewiremock from 'rewiremock';
 import { stubFunctions } from '../src';
 describe('require', () => {
   it('exports mocked modules work', () => {
-    rewiremock.enable();
     rewiremock('./exports-module').with(
       stubFunctions(rewiremock.requireActual('./exports-module')),
     );
+    rewiremock.enable();
     const mockedModule = require('./exports-module');
     rewiremock.disable();
 
@@ -13,10 +13,10 @@ describe('require', () => {
   });
 
   it('mocked modules work', () => {
-    rewiremock.enable();
     rewiremock('./es6-module').with(
       stubFunctions(rewiremock.requireActual('./es6-module')),
     );
+    rewiremock.enable();
     const mockedModule = require('./es6-module');
     rewiremock.disable();
 
