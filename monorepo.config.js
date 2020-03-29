d9e11d8ef38ac9e45e00087f12208a28d4726db2
Patrick Shaw <mail@patrickshaw.me>
2020-03-30 01:45:21 +1100

    Added patrick-shaw project for mostly-personal or strongly-related-to-me projects

--- monorepo.config.js
+++ monorepo.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   buildProjects: ['build-packages'],
-  projectsDirs: ['misc', 'faultjs', 'semantic-documents'],
+  projectsDirs: ['misc', 'faultjs', 'semantic-documents', 'patrick-shaw'],
   serve: {
     servers: [
-      /*{
-        input: './faultjs/fault-benchmarker/src/frontend/index.tsx',
-      },*/
       {
-        input: './misc/my-resume/src/index.tsx',
+        input: './faultjs/fault-benchmarker/src/frontend/index.tsx',
+      },
+      {
+        input: './patrick-shaw/my-resume/src/index.tsx',
       },
       {
         input: './misc/page-breaker-chrome/src/index.tsx',
