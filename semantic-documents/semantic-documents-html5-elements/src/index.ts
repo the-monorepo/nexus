168d62c0e160ad68589569ef6882728cf812b488
Patrick Shaw <pshaw@atlassian.com>
2020-03-30 18:59:55 +1100

    Refactored html5-elements to have a more practical API

--- semantic-documents/semantic-documents-html5-elements/src/index.ts
+++ semantic-documents/semantic-documents-html5-elements/src/index.ts
@@ -1,92 +1,86 @@
 import { SemanticElement, StyleRoot } from '@semantic-documents/element';
 import * as types from '@semantic-documents/html5-symbols';
 
-export class H1Element extends SemanticElement {
-  constructor(styleRoot: StyleRoot) {
-    super(
-      styleRoot,
-      'h1',
-      types.HEADING_1,
-    );
+export const createElements = (styleRoot: StyleRoot) => ({
+  H1: class extends SemanticElement {
+    constructor() {
+      super(
+        styleRoot,
+        'h1',
+        types.HEADING_1,
+      );
+    }
+  },
+  H2: class extends SemanticElement {
+    constructor() {
+      super(
+        styleRoot,
+        'h2',
+        types.HEADING_2
+      )
+    }
+  },
+  H3: class extends SemanticElement {
+    constructor() {
+      super(
+        styleRoot,
+        'h3',
+        types.HEADING_3
+      )
+    }
+  },
+  H4: class extends SemanticElement {
+    constructor() {
+      super(
+        styleRoot,
+        'h4',
+        types.HEADING_4
+      )
+    }
+  },
+  H5: class extends SemanticElement {
+    constructor() {
+      super(
+        styleRoot,
+        'h5',
+        types.HEADING_5
+      );
+    }
+  },
+  H6: class extends SemanticElement {
+    constructor() {
+      super(
+        styleRoot,
+        'h6',
+        types.HEADING_6
+      )
+    }
+  },
+  Header: class extends SemanticElement {
+    constructor() {
+      super(
+        styleRoot,
+        'header',
+        types.HEADER,
+      )
+    }
+  },
+  Footer: class extends SemanticElement {
+    constructor() {
+      super(
+        styleRoot,
+        'footer',
+        types.FOOTER,
+      )
+    }
+  },
+  Section: class extends SemanticElement {
+    constructor() {
+      super(
+        styleRoot,
+        'section',
+        types.SECTION,
+      )
+    }
   }
-}
-
-export class H2Element extends SemanticElement {
-  constructor(styleRoot: StyleRoot) {
-    super(
-      styleRoot,
-      'h2',
-      types.HEADING_2
-    )
-  }
-}
-
-export class H3Element extends SemanticElement {
-  constructor(styleRoot: StyleRoot) {
-    super(
-      styleRoot,
-      'h3',
-      types.HEADING_3
-    )
-  }
-}
-
-export class H4Element extends SemanticElement {
-  constructor(styleRoot: StyleRoot) {
-    super(
-      styleRoot,
-      'h4',
-      types.HEADING_4
-    )
-  }
-}
-
-export class H5Element extends SemanticElement {
-  constructor(styleRoot: StyleRoot) {
-    super(
-      styleRoot,
-      'h5',
-      types.HEADING_5
-    );
-  }
-}
-
-export class H6Element extends SemanticElement {
-  constructor(styleRoot: StyleRoot) {
-    super(
-      styleRoot,
-      'h6',
-      types.HEADING_6
-    )
-  }
-}
-
-export class HeaderElement extends SemanticElement {
-  constructor(styleRoot: StyleRoot) {
-    super(
-      styleRoot,
-      'header',
-      types.HEADER,
-    )
-  }
-}
-
-export class FooterElement extends SemanticElement {
-  constructor(styleRoot: StyleRoot) {
-    super(
-      styleRoot,
-      'footer',
-      types.FOOTER,
-    )
-  }
-}
-
-export class SectionElement extends SemanticElement {
-  constructor(styleRoot: StyleRoot) {
-    super(
-      styleRoot,
-      'section',
-      types.SECTION,
-    )
-  }
-}
+})
