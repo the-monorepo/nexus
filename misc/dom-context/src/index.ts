97806555607b3e17e6e521faf93bac1d9408ceb1
Patrick Shaw <mail@patrickshaw.me>
2020-04-07 23:51:52 +1000

    More flexible decorator logic for dom-context

--- misc/dom-context/src/index.ts
+++ misc/dom-context/src/index.ts
@@ -1,14 +1,17 @@
-const PROVIDER_TYPE = Symbol('provider-type');
 export const RESPECT_BOUNDARIES_ATTRIBUTE = 'respect-boundaries';
 
-export const findParentProvider = <T>(node: Node, providerType: Symbol, respectBoundaries: boolean): ProviderElement<T> | null => {
+export const findParentProvider = (node: Node, providerSet: Set<any>, respectBoundaries: boolean): any | null => {
   let current: Node | null = node;
 
   while (current !== null) {
-    if (current[PROVIDER_TYPE] === providerType) {
-      break;
-    } else if (current instanceof ShadowRoot && !respectBoundaries) {
-      current = current.host;
+    if (providerSet.has(current)) {
+      return current;
+    } else if (current instanceof ShadowRoot) {
+      if(!respectBoundaries) {
+        current = current.host;
+      } else {
+        break;
+      }
     } else if (current instanceof Node) {
       current = current.parentNode;
     } else {
@@ -16,79 +19,105 @@ export const findParentProvider = <T>(node: Node, providerType: Symbol, respectB
     }
   }
 
-  return current as ProviderElement<T>;
+  return current;
 };
 
-abstract class ProviderElement<T> extends HTMLElement {
-  private _value: T | undefined = undefined;
-  public readonly consumers = new Set();
-  constructor(
-    private readonly contextSymbol: Symbol
-  ) {
-    super();
-    this.attachShadow({ mode: 'open' })
-      .appendChild(document.createElement('slot'));
-  }
+export const createContext = (contextName?: string) => {
+  // TODO: Could consider reworking this to use a data structure that would provider quicker lookups of which provider to use
+  const providerSet: Set<any> = new Set();
 
-  connectedCallback() {
-    if (this.isConnected) {
-      const parentProvider = findParentProvider(this.shadowRoot!, this.contextSymbol, this.respectBoundaries);
-      parentProvider
-    }
-  }
+  const PROVIDER_VALUE = Symbol(`${contextName}-provider-value`);
+  const PROVIDER = Symbol(`${contextName}-provider`);
 
-  get respectBoundaries() {
-    return this.hasAttribute(RESPECT_BOUNDARIES_ATTRIBUTE);
-  }
-
-  set value(value: T) {
-    this._value;
-  }
-}
-
-const createContextGetter = (clazz, target) => {
-  switch (target.kind) {
-    case 'method':
-      const originalDescriptor = Object.getOwnPropertyDescriptor(clazz.prototype, target.key);
-      return function getterDefaultContextConsumer() {
-        const defaultValue = originalDescriptor.get.call(this);
-        console.log('hmmm', defaultValue);
-        return defaultValue + '....';
-      };
-    case 'field':
-      if (target.initializer === undefined) {
-        return function fieldDefaultWithoutInitializerContextConsumer() {
-          return undefined;
-        };  
-      } else {
-        return function fieldDefaultWithInitializerContextConsumer() {
-          return target.initializer.call(this);
-        };  
-      }
-  }
-}
-
-export const createContext = () => {
-  const consumerDecorator = (target) => {
-    console.log(target);
+  const consumerDecorator = <T>(target) => {
+    const consumerSymbol = Symbol(`${contextName}-consumer-value`);
+    console.log('consumerStuff', target);
     return {
-      kind: "method",
-      key: target.key,
-      placement: "prototype",
-      descriptor: {
-        ...target.descriptor,
-        configurable: true,
+      ...target,
+      key: consumerSymbol,
+      extras: [{
+        kind: 'field',
+        key: PROVIDER,
+        placement: 'own',
+        descriptor: {
+          configurable: true,
+          writable: true,
+          enumerable: false,
+        },
+        initializer: () => null,
       },
+      {
+        kind: 'method',
+        key: target.key,
+        placement: 'prototype',
+        descriptor: {
+          get: function() {
+            return this[PROVIDER] !== null ? this[PROVIDER][PROVIDER_VALUE] : this[consumerSymbol];
+          }
+        }
+      }],
       finisher: (clazz) => {
-        Object.defineProperty(clazz.prototype, target.key, {
-          get: createContextGetter(clazz, target)
-        });
+        return class Consumer extends clazz { 
+          connectedCallback() {
+            if (this.isConnected) {
+              this[PROVIDER] = findParentProvider(this.parentNode, providerSet, false);
+            }
+            super.connectedCallback();
+          }
+
+          disconnectedCallback() {
+            this[PROVIDER] = null;
+            super.disconnectedCallback();
+          }
+        }
       }
     }
   };
 
-  const providerDecorator = (target, key, descriptor) => {
-    
+  const providerDecorator = (target) => {
+    const symbol = Symbol(`${contextName}-alias-provider-value`);
+    console.log('provider target', target);
+    return {
+      ...target,
+      key: symbol,
+      extras: [{
+        kind: 'method',
+        key: target.key,
+        placement: 'prototype',
+        descriptor: {
+          get: function() {
+            return this[symbol];
+          },
+          set: function(value) {
+            this[symbol] = value;
+          }
+        }
+      }, {
+        kind: 'method',
+        key: PROVIDER_VALUE,
+        placement: 'prototype',
+        descriptor: {
+          get: function() {
+            return this[target.key];
+          },
+          set: function(value) {
+            this[target.key] = value;
+          },
+        }
+      }],
+      finisher: (clazz) => {
+        return class Provider extends clazz {
+          connectedCallback() {
+            providerSet.add(this);
+            super.connectedCallback();
+          }
+          disconnectedCallback() {
+            providerSet.delete(this);
+            super.disconnectedCallback();
+          }
+        }
+      }
+    }
   };
 
   return {
