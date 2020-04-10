ab28ed110aee0f38d31141bfc9e329adbc5e2b77
Patrick Shaw <pshaw@atlassian.com>
2020-04-10 23:39:04 +1000

    Merged semantic-documents element and suffix-element

--- semantic-documents/semantic-documents-element/src/index.ts
+++ semantic-documents/semantic-documents-element/src/index.ts
@@ -1,18 +1,81 @@
-import { SuffixElement, StyleRoot } from '@semantic-documents/suffix-element';
+import createContext from 'dom-context';
 
-export { StyleRoot };
+import { pickClassName, createStyleRoot } from '@semantic-documents/classname-picker';
+import type { StyleRoot } from '@semantic-documents/classname-picker';
 
-export abstract class SemanticElement extends SuffixElement {
-  constructor(
-    styleRoot: StyleRoot,
-    elementName: string,
-    type?: Symbol,
-  ) {
-    super(
-      styleRoot,
-      elementName,
-      type,
-    );
+export type { StyleRoot };
+export { createStyleRoot };
+
+const elementHierachyContext = createContext('element-hierachy');
+export class OverrideHierachyElement extends HTMLElement {
+  @elementHierachyContext.provider private _hierachy: any[] = [];
+
+  set hierachy(value) {
+    this._hierachy = value;
+  }
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' })
+      .appendChild(document.createElement('slot'));
   }
 }
 
+abstract class SemanticElement extends HTMLElement {
+  private readonly semanticElement: HTMLElement;
+
+  @elementHierachyContext.provider
+  private hierachy: any[];
+  
+  private semanticType: any;
+
+  constructor(
+    private readonly styleRoot: StyleRoot,
+    styles: string,
+    elementName: string,
+    type: any,
+  ) {
+    super();
+    this.semanticType = type;
+
+    const stylesElement = document.createElement('style');
+    stylesElement.innerHTML = styles;
+
+    this.semanticElement = document.createElement(elementName);
+    this.semanticElement.appendChild(document.createElement('slot'));
+
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.appendChild(this.semanticElement);
+    shadowRoot.appendChild(stylesElement);
+
+    this.hierachy = [elementName];
+  }
+  
+  @elementHierachyContext.consumer
+  private onHierachyChange(hierachy = []) {
+    if (hierachy === undefined) {
+      return;
+    }
+    this.hierachy = [this.semanticType, ...hierachy];
+    this.update(this.hierachy); 
+  }
+  
+  connectedCallback() {
+    console.log('test');
+  }
+  
+  disconnectedCallback() {
+    console.log('disc')
+  }
+  
+  update(hierachy) {
+    const className = pickClassName(this.styleRoot, hierachy);
+    if (className !== undefined) {
+      this.semanticElement.setAttribute('class', className);
+    } else {
+      this.semanticElement.removeAttribute('class');
+    }
+  }
+}
+
+export default SemanticElement;
