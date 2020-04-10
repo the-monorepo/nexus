a2e4e085c0c9e5a5d6b9420284ba83e2c875b335
Patrick Shaw <mail@patrickshaw.me>
2020-04-11 02:52:46 +1000

    Removed old test code for dom babel plugin

--- misc/babel-plugin-transform-mobx-jsx/t.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const _template = mbx.elementBlueprint('<div></div>', (_rootNode) => {
-  const _div$ = _rootNode;
-  return [mbx.children(_div$, null)];
-});
-
-mbx.componentResult(_template, [TableHeader({})]);
