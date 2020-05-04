e76685245277319f6269d6e29e93451f1633f918
Patrick Shaw <mail@patrickshaw.me>
2020-05-05 01:25:16 +1000

    Added stylelint

--- build-packages/git-staged-stream/src/index.js
+++ build-packages/git-staged-stream/src/index.js
@@ -3,3 +3,4 @@ const createGitStatusFilterStream = require('git-status-filter-stream');
 const createGitStagedStream = () => createGitStatusFilterStream((status) => status[3] >= 2);
 
 module.exports = createGitStagedStream;
+ 
