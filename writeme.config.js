9d91116f4e007b82a35cf8b4f5c3b90075d565e7
Patrick Shaw <mail@patrickshaw.me>
2020-05-13 21:53:56 +1000

    Fixed writeme config

--- writeme.config.js
+++ writeme.config.js
@@ -1,3 +1,43 @@
 module.exports = {
+  projects: {
+    overrides: [
+      {
+        category: 'Cinder',
+        test: './cinder/*/',
+      },
+      {
+        category: 'Semantic documents',
+        test: './semantic-documents/*',
+      },
+      {
+        category: 'By Example',
+        test: './byexample/*',
+      },
+      {
+        category: 'Build tooling',
+        test: './{build-packages,misc,faultjs}/{build-util,gulp-*}',
+      },
+      {
+        category: 'Documentation',
+        test: './{build-packages,misc,faultjs}/{writeme-*,markdown-util}',
+      },
+      {
+        category: 'Logging',
+        test: './{build-packages,misc,faultjs}/{winston-formats,logger}',
+      },
+      {
+        category: 'ESLint',
+        test: './{build-packages,misc,faultjs}/tslint*',
+      },
+      {
+        category: 'TSLint',
+        test: './{build-packages,misc,faultjs}/eslint*',
+      },
+      {
+        category: 'FaultJS',
+        test: ['./{build-packages,misc,faultjs}/fault-*'],
+      },
+    ],
+  },
   isDevPackage: true,
 };
