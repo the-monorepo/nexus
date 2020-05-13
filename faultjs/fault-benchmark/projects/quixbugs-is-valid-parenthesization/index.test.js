0bd5400c57c1feda7a2f9e857c6b847df21c01cc
Patrick Shaw <mail@patrickshaw.me>
2020-05-13 21:36:04 +1000

    Moved defects out of monorepo :D :D

--- faultjs/fault-benchmark/projects/quixbugs-is-valid-parenthesization/index.test.js
+++
@@ -1,8 +0,0 @@
-import { isValidParenthesization } from './index';
-import expect from 'expect';
-
-describe('isValidParenthesization', () => {
-  it('1', () => expect(isValidParenthesization("((()()))()")).toBe(true));
-  it('2', () => expect(isValidParenthesization(")()(")).toBe(false));
-  it('3', () => expect(isValidParenthesization("((")).toBe(false));
-})
--- faultjs/fault-benchmark/projects/sinon-1968/test/es2015/a-module.mjs
+++
@@ -1,3 +0,0 @@
-export function anExport() {
-    return 42;
-}
