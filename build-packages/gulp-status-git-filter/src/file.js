8f7658f5a7ffc9cd7d02352ad094409eb0ce1c0d
Patrick Shaw <mail@patrickshaw.me>
2020-05-22 03:30:42 +1000

    Removed (almost) all build packages

--- build-packages/gulp-status-git-filter/src/file.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function changedFilePaths(filter) {
-  const fs = require('fs');
-  const git = require('isomorphic-git');
-
-  const statusMatrix = await git.statusMatrix({ fs, dir: '.' });
-
-  return statusMatrix.filter(filter).map(([filePath]) => filePath);
-};
