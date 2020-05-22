cf8af7a21e0679649bcbdb8c987deb4f8b1e3335
Patrick Shaw <mail@patrickshaw.me>
2020-05-23 01:43:46 +1000

    Added new package named resultful

--- misc/resultful/src/index.ts
+++ misc/resultful/src/index.ts
@@ -1,19 +1,26 @@
+import * as keys from './keys';
+import * as types from './types';
+export { types, keys };
+
 export type SuccessResult<T> = {
-  payload: T;
-  error: undefined;
-  exception: undefined;
+  type: typeof types.SUCCESS;
+  [keys.SUCCESS]: T;
+  [keys.ERROR]: undefined;
+  [keys.EXCEPTION]: undefined;
 };
 
 export type ErrorResult<E> = {
-  payload: undefined;
-  error: E;
-  exception: undefined;
+  type: typeof types.ERROR;
+  [keys.SUCCESS]: undefined;
+  [keys.ERROR]: E;
+  [keys.EXCEPTION]: undefined;
 };
 
 export type ExceptionResult<EX = any | Error> = {
-  payload: undefined;
-  error: undefined;
-  exception: EX;
+  type: typeof types.EXCEPTION;
+  [keys.SUCCESS]: undefined;
+  [keys.ERROR]: undefined;
+  [keys.EXCEPTION]: EX;
 };
 
 export type FailureResult<E, EX = any | Error> = ErrorResult<E> | ExceptionResult<EX>;
@@ -25,34 +32,128 @@ export type Result<T, E, EX = any | Error> =
 
 export const isException = <EX = any | Error>(
   result: Result<unknown, unknown, EX>,
-): result is ExceptionResult<EX> => result.exception !== undefined;
+): result is ExceptionResult<EX> => result.type === types.EXCEPTION;
 
 export const isError = <E>(result: Result<unknown, E>): result is ErrorResult<E> =>
-  result.error !== undefined;
+  result.type === types.ERROR;
 
-export const isSuccessful = <P>(result: Result<P, unknown>): result is SuccessResult<P> =>
-  result.payload !== undefined;
+export const isSuccess = <P>(result: Result<P, unknown>): result is SuccessResult<P> =>
+  result.type === types.SUCCESS;
 
 export const isFailure = <E, EX = any | Error>(
   result: Result<unknown, E, EX>,
-): result is FailureResult<E, EX> => result.payload === undefined;
+): result is FailureResult<E, EX> => {
+  switch (result.type) {
+    case types.ERROR:
+    case types.EXCEPTION: {
+      return true;
+    }
+    default:
+      return false;
+  }
+};
 
+/**
+ * Creates an "error" result - Return this if an error has ocurred
+ */
 export const error = <E>(error: E): ErrorResult<E> => ({
+  type: types.ERROR,
   error,
   payload: undefined,
   exception: undefined,
 });
 
 export const success = <T>(payload: T): SuccessResult<T> => ({
+  type: types.SUCCESS,
   payload,
   error: undefined,
   exception: undefined,
 });
 
-export const exception = <E extends Error = Error>(exception: E): ExceptionResult<E> => {
+/**
+ * Creates an "exception" result - Return this result if some unexpected,
+ * thought-to-be-impossible/didn't-think-about-it type erroneous behaviour has ocurred
+ */
+export const exception = <EX extends Error>(exception: EX): ExceptionResult<EX> => {
   return {
+    type: types.EXCEPTION,
     payload: undefined,
     error: undefined,
     exception,
   };
+};
+
+export type SimpleHandleCallback<T, R> = (value: T) => R;
+
+export type SuccessCallback<T, R> = SimpleHandleCallback<T, R>;
+export type FailureCallback<E, EX, R> = {
+  (error: undefined, exception: EX): R;
+  (error: E, exception: undefined): R;
+};
+export type ErrorCallback<E, R> = (error: E) => R;
+export type ExceptionCallback<EX, R> = (exception: EX) => R;
+
+export type SuccessOnlyHandleOptions<P, R> = {
+  payload: SuccessCallback<P, R>;
+};
+
+export type ErrorOnlyHandleOptions<E, R> = {
+  error: ErrorCallback<E, R>;
+};
+
+export type ExceptionOnlyHandleOptions<EX, R> = {
+  exception: ExceptionCallback<EX, R>;
+};
+
+export type FailureOnlyHandleOptions<E, EX, R> = {
+  failure: FailureCallback<E, EX, R>;
+};
+
+export type SuccessErrorExceptionHandleOptions<P, PR, E, ER, EX, EXR> = {
+  payload?: SuccessCallback<P, PR>;
+  error?: ErrorCallback<E, ER>;
+  exception?: ExceptionCallback<EX, EXR>;
+  failure?: never;
+};
+
+export type SuccessFailureHandleOptions<P, PR, E, EX, FR> = {
+  payload?: SuccessCallback<P, PR>;
+  error?: never;
+  exception?: never;
+  failure?: FailureCallback<E, EX, FR>;
+};
+
+export type HandleOptions<P, PR, E, ER, EX, EXR, FR> =
+  | SuccessErrorExceptionHandleOptions<P, PR, E, ER, EX, EXR>
+  | SuccessFailureHandleOptions<P, PR, E, EX, FR>;
+
+export const handle = <P, PR, E, ER, EX, EXR, FR>(
+  result: Result<P, E, EX>,
+  options: HandleOptions<P, PR, E, ER, EX, EXR, FR>,
+) => {
+  switch (result.type) {
+    case types.SUCCESS: {
+      if (options.payload !== undefined) {
+        return options.payload(result.payload);
+      }
+      break;
+    }
+    case types.ERROR: {
+      if (options.error !== undefined) {
+        return options.error(result.error);
+      } else if (options.failure !== undefined) {
+        return options.failure(result.error, undefined);
+      }
+      break;
+    }
+    case types.EXCEPTION: {
+      if (options.exception !== undefined) {
+        return options.exception(result.exception);
+      } else if (options.failure !== undefined) {
+        return options.failure(undefined, result.exception);
+      }
+    }
+  }
+
+  return result;
 };
