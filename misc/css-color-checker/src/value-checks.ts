054c67e5bde5027761677bd7137e02bbb053bd85
Patrick Shaw <mail@patrickshaw.me>
2020-05-27 03:43:30 +1000

    Removed try catches in css-color-checker
    Way overkill

--- misc/css-color-checker/src/value-checks.ts
+++ misc/css-color-checker/src/value-checks.ts
@@ -1,19 +1,11 @@
 export function isValidRgbVal(magnitudeString: string): boolean {
-  try {
-    const magnitude: number = Number.parseFloat(magnitudeString);
-    return magnitude >= 0 && magnitude <= 255;
-  } catch (err) {
-    return false;
-  }
+  const magnitude: number = Number.parseFloat(magnitudeString);
+  return magnitude >= 0 && magnitude <= 255;
 }
 
 export function isValidAlphaValue(magnitudeString: string): boolean {
-  try {
-    const magnitude: number = Number.parseFloat(magnitudeString);
-    return magnitude <= 1 && magnitude >= 0;
-  } catch (err) {
-    return false;
-  }
+  const magnitude: number = Number.parseFloat(magnitudeString);
+  return magnitude <= 1 && magnitude >= 0;
 }
 
 export function isValidPercentage(magnitude: number): boolean {
@@ -21,19 +13,11 @@ export function isValidPercentage(magnitude: number): boolean {
 }
 
 export function isValidPercentageString(magnitudeString: string): boolean {
-  try {
-    const magnitude: number = Number.parseFloat(magnitudeString);
-    return isValidPercentage(magnitude);
-  } catch (err) {
-    return false;
-  }
+  const magnitude: number = Number.parseFloat(magnitudeString);
+  return isValidPercentage(magnitude);
 }
 
 export function isValidHue(valueString: string): boolean {
-  try {
-    const value: number = Number.parseFloat(valueString);
-    return value >= 0 && value <= 360;
-  } catch (err) {
-    return false;
-  }
+  const value: number = Number.parseFloat(valueString);
+  return value >= 0 && value <= 360;
 }
