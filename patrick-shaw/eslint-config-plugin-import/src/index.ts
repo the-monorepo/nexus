429c630a09befbf35090cc7d3abea2a3417d0d96
Patrick Shaw <mail@patrickshaw.me>
2020-06-01 02:35:39 +1000

    Turned off ordered import rule
    Doesn't work for side effects

--- patrick-shaw/eslint-config-plugin-import/src/index.ts
+++ patrick-shaw/eslint-config-plugin-import/src/index.ts
@@ -1,16 +1,7 @@
 module.exports = {
   rules: {
     'import/default': 'off',
-    'import/order': [
-      'error',
-      {
-        'newlines-between': 'always-and-inside-groups',
-        alphabetize: {
-          order: 'asc',
-        },
-        groups: ['builtin', 'external', 'internal', ['parent', 'index', 'sibling']],
-      },
-    ],
+    'import/order': 'off',
     'import/no-named-as-default': 'error',
   },
 };
