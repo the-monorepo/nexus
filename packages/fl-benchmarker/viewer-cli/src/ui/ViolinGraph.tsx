219e28f2540003045644cd143c8df1c5e0b19377
Patrick Shaw <mail@patrickshaw.me>
2020-10-11 01:03:10 +1100

    Now supporting multiple violin graphs

--- packages/fl-benchmarker/viewer-cli/src/ui/ViolinGraph.tsx
+++ packages/fl-benchmarker/viewer-cli/src/ui/ViolinGraph.tsx
@@ -6,7 +6,19 @@ import { autorun } from 'mobx';
 import cx from 'classnames';
 
 import * as d3 from 'd3';
-import { scaleLinear, axisBottom, area, curveCatmullRom, extent, mean, axisLeft, select, nest } from 'd3';
+import {
+  scaleLinear,
+  axisBottom,
+  area,
+  curveCatmullRom,
+  extent,
+  mean,
+  axisLeft,
+  select,
+  group,
+  rollup,
+  scaleBand,
+} from 'd3';
 
 import * as previousStyles from './ViolinGraph.scss';
 import * as figureStyles from './figure.scss';
@@ -14,75 +26,111 @@ import styles from './ViolinGraphElement.scss';
 
 import { COLUMN, ROW, LayoutFlow } from './LayoutFlows';
 
-function kernelDensityEstimator(kernel, X) {
-  return (V) => {
-    return X.map((x) => {
-      return [x, mean(V, (v) => kernel(x - v))];
-    });
-  };
-};
 
-const data2 = [0, 0.1, 0.12, 0.15, 0.18, 0.5, 0.57, 0.9, 1];
-const data = [0, 0.1, 0.12, 0.15, 0.18, 0.5, 0.57, 0.9, 1];
+const data2 = [
+  { name: 'project-1', value: 0 },
+  { name: 'project-1', value: 0.1 },
+  { name: 'project-1', value: 0.12 },
+  { name: 'project-1', value: 0.15 },
+  { name: 'project-1', value: 0.18 },
+  { name: 'project-1', value: 0.5 },
+  { name: 'project-1', value: 0.5 },
+  { name: 'project-1', value: 0.57 },
+  { name: 'project-1', value: 0.9 },
+  { name: 'project-1', value: 1 },
+];
 
-const dataMap = {
-  'mbfl': data,
-  'otherstuff': data2,
-};
+const data1 = [
+  { name: 'project-1', value: 0 },
+  { name: 'project-1', value: 0.1 },
+  { name: 'project-1', value: 0.12 },
+  { name: 'project-1', value: 0.10 },
+  { name: 'project-1', value: 0 },
+  { name: 'project-1', value: 0.5 },
+  { name: 'project-1', value: 0.9 },
+  { name: 'project-1', value: 0.9 },
+  { name: 'project-1', value: 1 },
+];
+
+const dataMap = [
+  { name: 'title', data: data1 },
+  { name: 'otherstuff', data: data2 },
+  { name: 'erawr', data: [{ name: 'rawr', value: 0 }, { name: 'hmm', value: 0.4}, { name: 'rawr', value: 0.7} ] },
+];
+
 const width = 400;
 const height = 400;
 
-const categoryScale = scaleBand()
-  .range([0, width]);
+const techniqueScale = scaleBand()
+  .range([0, width])
+  .domain(dataMap.map(({ name }) => name))
+  .padding(0.05);
 
-const examScale = scaleLinear()
-  .domain([0, 1])
-  .range([0, height])
+const examScale = scaleLinear().domain([0, 1]).range([0, height]);
 
-const kernelEpanechnikov = (bandwidth) => {
-  return x => Math.abs(x /= bandwidth) <= 1 ? 0.75 * (1 - x * x) / bandwidth : 0;
+const kernelEpanechnikov = (bandwidth: number) => {
+  return (x: number) =>
+    Math.abs((x /= bandwidth)) <= 1 ? (0.75 * (1 - x * x)) / bandwidth : 0;
 };
 
-const kde = (kernel, thresholds, data) => {
-  return thresholds.map(t => [t, mean(data, d => kernel(t - d))]);
-}
+const kde = (kernel: (x: number) => number, thresholds: number[], data: number[]) => {
+  return thresholds.map((t) => [t, mean(data, (d) => kernel(t - d))]);
+};
 
-const examDensity = kde(kernelEpanechnikov(0.2), examScale.ticks(1000), data)
-
-const examDensityScale = scaleLinear()
-  .domain([0, d3.max(examDensity, d => d[1])])
-  .range([0, width])
-
-console.log(examDensity);
-
-const horizontalViolinArea = d3.area()
-  .y(d => examScale(d[0]))
-  .x0(d => (examDensityScale(d[1]) + width) / 2)
-  .x1(d => (-examDensityScale(d[1]) + width) / 2)
-  .curve(curveCatmullRom);
+const nameToAreaValues = new Map(
+  Object.values(dataMap)
+    .map(
+      ({ name, data }) => ([
+        name,
+        kde(
+          kernelEpanechnikov(0.2),
+          examScale.ticks(10),
+          data.map(({ value }) => value),
+        )
+      ])
+    )
+);
 
 class FaultJSViolinElement extends DomElement {
   [UPDATE]() {
     super[UPDATE]();
-    select(this.renderRoot.getElementById('violin-path'))
-      .datum(examDensity)
-      .attr('d', horizontalViolinArea);
-    select(this.renderRoot.getElementById('axis-left'))
-      .append('g')
-      .call(axisLeft(examScale))
-    select(this.renderRoot.getElementById('axis-bottom'))
-      .append('g')
-      .call(axisBottom(examDensityScale));
+    select(this.renderRoot.getElementById('axis-left')).call(axisLeft(examScale));
+    select(this.renderRoot.getElementById('axis-bottom')).call(
+      axisBottom(techniqueScale),
+    );
+
+    const svgSelection = select(this.renderRoot.getElementById('svg'));
+    for(const [name, areaValues] of nameToAreaValues) {
+      const bandwidth = techniqueScale.bandwidth();
+      const nameOffset = techniqueScale(name);
+
+      const examDensityScale = scaleLinear()
+        .domain([0, Math.max(...areaValues.map(d => d[1]))])
+        .range([0, 1]);
+
+      const violinArea = d3
+        .area()
+        .y((d) => examScale(d[0]))
+        .x0((d) => (examDensityScale(d[1]) * bandwidth + bandwidth) / 2 + nameOffset)
+        .x1((d) => (-examDensityScale(d[1]) * bandwidth + bandwidth) / 2 + nameOffset)
+        .curve(curveCatmullRom);
+
+      svgSelection
+        .append('path')
+        .attr('d', violinArea(areaValues))
+    }
   }
 
   render() {
     return (
       <>
-        <style>
-          {styles.toString()}
-        </style>
-        <svg class={styles.locals.svg} preserveAspectRatio="none" viewBox={`-25 -5 ${width + 25} ${height + 25}`}>
-          <path id="violin-path" />
+        <style>{styles.toString()}</style>
+        <svg
+          id="svg"
+          class={styles.locals.svg}
+          preserveAspectRatio="none"
+          viewBox={`-25 -5 ${width + 25} ${height + 25}`}
+        >
           <g id="axis-left" />
           <g id="axis-bottom" transform={`translate(0, ${height})`} />
         </svg>
@@ -102,14 +150,28 @@ export type SvgViolinProps = {
  * The violin is horizontal
  */
 const HorizontalSvgViolin = ({ class: className, id }: SvgViolinProps) => {
-  const d = horizontalViolinArea([[0, 1], [0.1, 20], [0.3, 10], [0.4, 15], [0.45, 5], [0.5, 1], [1, 30]]);
+  const d = horizontalViolinArea([
+    [0, 1],
+    [0.1, 20],
+    [0.3, 10],
+    [0.4, 15],
+    [0, 12],
+    [0.45, 5],
+    [0.5, 1],
+    [1, 30],
+  ]);
 
   const ar = axisBottom(x);
   console.log('???????', ar);
 
   return (
     <div class={previousStyles.violin}>
-      <svg preserveAspectRatio="none" viewBox='0 -1 1 2' class={cx(previousStyles.svg, previousStyles.horizontal, className)} id={id}>
+      <svg
+        preserveAspectRatio="none"
+        viewBox="0 -1 1 2"
+        class={cx(previousStyles.svg, previousStyles.horizontal, className)}
+        id={id}
+      >
         <path d={d} />
       </svg>
     </div>
@@ -144,7 +206,6 @@ export const ViolinGraph = ({ flow = 'row', class: className }: ViolinGraphProps
 };
 
 <>
-<div class={cx(figureStyles.discreteScale, figureStyles.vertical)}></div>
-      <div class={cx(figureStyles.discreteScale, figureStyles.horizontal)}></div>
-
-</>
+  <div class={cx(figureStyles.discreteScale, figureStyles.vertical)}></div>
+  <div class={cx(figureStyles.discreteScale, figureStyles.horizontal)}></div>
+</>;
