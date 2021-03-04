b4a143b1169a3b6f2d135f6eb3cd76045e75bae8
Patrick Shaw <mail@patrickshaw.me>
2021-03-04 22:54:32 +1100

    Fixed shell script for uploading nano

--- packages/patrick-shaw/particle-sensor-arduino/upload-to-arduino.sh
+++ packages/patrick-shaw/particle-sensor-arduino/upload-to-arduino.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-avrdude -p m328p -c arduino -P /dev/cu.usbserial-14110 -b 57600 -U flash:w:"target/avr-atmega328p/debug/particle-sensor-arduino.elf":e
+avrdude -p atmega328p -c arduino -P /dev/cu.usbserial-14210 -b 115200 -D -U flash:w:"target/avr-atmega328p/debug/particle-sensor-arduino.elf":e
