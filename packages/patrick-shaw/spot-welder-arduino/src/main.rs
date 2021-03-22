fa21217773352935697e6c2ca14a3d3bc0b03a5c
Patrick Shaw <mail@patrickshaw.me>
2021-03-23 05:07:51 +1100

    Moved pulse data to its own file

--- packages/patrick-shaw/spot-welder-arduino/src/main.rs
+++ packages/patrick-shaw/spot-welder-arduino/src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(abi_avr_interrupt)]
+mod pulse_data;
 use arduino_uno::{
     hal::port::portb::{PB2, PB5},
     pac::TC0,
@@ -15,66 +16,9 @@ use arduino_uno::{
     pwm::Timer2Pwm,
 };
 use avr_device::interrupt::Mutex;
-use core::{borrow::Borrow, cell::RefCell, ops::DerefMut};
+use pulse_data::{PulseMilliThresholds, PulseData, RedundentReadStatus};
+use core::{cell::RefCell, ops::DerefMut};
 use panic_halt as _;
-
-struct PulseData {
-    first_pulse_duration: u16,
-    pulse_gap_duration: u16,
-    second_pulse_duration: u16,
-}
-
-enum RedundentReadStatus<R> {
-    Success(R),
-    Corrupt,
-}
-impl PulseData {
-    fn read<F>(mut read_byte: F) -> RedundentReadStatus<PulseData>
-    where
-        F: FnMut() -> u8,
-    {
-        if let (
-            RedundentReadStatus::Success(first_pulse_duration),
-            RedundentReadStatus::Success(pulse_gap_duration),
-            RedundentReadStatus::Success(second_pulse_duration),
-        ) = (
-            redundent_read_u16(&mut read_byte),
-            redundent_read_u16(&mut read_byte),
-            redundent_read_u16(&mut read_byte),
-        ) {
-            RedundentReadStatus::Success(PulseData {
-                first_pulse_duration,
-                pulse_gap_duration,
-                second_pulse_duration,
-            })
-        } else {
-            RedundentReadStatus::Corrupt
-        }
-    }
-}
-
-struct PulseMilliThresholds {
-    start_millis: u16,
-    current_millis: u16,
-    first_pulse_end_threshold: u16,
-    second_pulse_start_threshold: u16,
-}
-
-impl From<PulseData> for PulseMilliThresholds {
-    fn from(data: PulseData) -> Self {
-        let second_pulse_start_threshold = data.second_pulse_duration;
-        let first_pulse_end_threshold = second_pulse_start_threshold + data.pulse_gap_duration;
-        let start_millis = first_pulse_end_threshold + data.first_pulse_duration;
-        let current_millis = start_millis;
-
-        PulseMilliThresholds {
-            start_millis,
-            second_pulse_start_threshold,
-            first_pulse_end_threshold,
-            current_millis,
-        }
-    }
-}
 
 struct SpotWelderIO {
     low_sound: PD3<Pwm<Timer2Pwm>>,
@@ -175,28 +119,6 @@ impl SpotWelderManager {
 
 static SPOT_WELDER_MANAGER_MUTEX: Mutex<RefCell<SpotWelderManager>> =
     Mutex::new(RefCell::new(SpotWelderManager::new()));
-
-fn read_u16<F>(mut read_byte: F) -> u16
-where
-    F: FnMut() -> u8,
-{
-    let small_byte = read_byte() as u16;
-    let large_byte = (read_byte() as u16) << 8;
-    return large_byte | small_byte;
-}
-
-fn redundent_read_u16<F>(mut read_byte: F) -> RedundentReadStatus<u16>
-where
-    F: FnMut() -> u8,
-{
-    let value1 = read_u16(&mut read_byte);
-    let value2 = read_u16(&mut read_byte);
-    if value1 == value2 {
-        RedundentReadStatus::Success(value1)
-    } else {
-        RedundentReadStatus::Corrupt
-    }
-}
 
 #[arduino_uno::entry]
 fn main() -> ! {
