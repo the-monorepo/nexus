ce0f32ea31effdad9b4919dc3f08dc2263e9c92e
Patrick Shaw <mail@patrickshaw.me>
2021-04-14 01:35:46 +1000

    Added reduce

--- packages/pipelines/core-2/src/index.ts
+++ packages/pipelines/core-2/src/index.ts
@@ -19,15 +19,45 @@ export async function* of<T extends any[]>(values: T[]) {
   yield* values;
 }
 
+export const reduce = async <I>(iterable: AsyncIterable<I>, reducer: (current: I, incomingValue: I) => Promise<I> | I, initial?: I): Promise<I> => {
+  const iterator = iterable[Symbol.asyncIterator]();
+
+  let current = initial ?? await (async () => {
+    const result = await iterator.next();
+    if (result.done) {
+      throw new Error('Reduce of empty array with no initial value');
+    } else {
+      return result.value;
+    }
+  })();
+
+  let i = await iterator.next();
+  do {
+    current = await reducer(current, i.value);
+  } while(i.done)
+
+  return current;
+};
+
+export const every = async <T>(iterable: AsyncIterable<T>, condition: (value: T) => boolean | Promise<boolean>): Promise<boolean> => {
+  for await (const i of iterable) {
+    if (!await condition(i)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 export async function* concat<T>(...iterables: AsyncIterable<T>[]) {
   for (const iterable of iterables) {
     yield* iterable;
   }
 }
 
-export async function* filter<T>(iterable: AsyncIterable<T>, filter: (item: T) => boolean) {
+export async function* filter<T>(iterable: AsyncIterable<T>, filter: (item: T) => boolean | Promise<boolean>) {
   for await (const value of iterable) {
-    if (filter(value)) {
+    if (await filter(value)) {
       yield value;
     }
   }
