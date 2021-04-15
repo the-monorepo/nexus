256a14f19f6cd0972c2ed05108b9a11600c58844
Patrick Shaw <mail@patrickshaw.me>
2021-04-16 00:25:29 +1000

    WIP Broadcaster impl

---
+++ packages/pipelines/core-2/test/broadcaster.test.ts
@@ -0,0 +1,32 @@
+import { broadcaster } from '../src/index';
+
+async function* createTestIterable() {
+  let i = 0;
+  while(true) {
+    yield i;
+    i++;
+  }
+}
+
+it(broadcaster.name, async () => {
+  const iterator = createTestIterable();
+
+  const broadcastIterable = broadcaster(iterator);
+
+  const a = broadcastIterable[Symbol.asyncIterator]();
+
+  console.log(0)
+  const result0 = await a.next();
+  expect(result0.value).toBe(0);
+
+  console.log(1)
+  const b = broadcastIterable[Symbol.asyncIterator]();
+
+  console.log(2)
+  const [result1a, result2a] = await Promise.all([a.next(), a.next()]);
+  expect([result1a.value, result2a.value]).toEqual([1, 2]);
+
+  console.log(3)
+  const [result1b, result2b] = await Promise.all([b.next(), b.next()]);
+  expect([result1b.value, result2b]).toEqual([1, 2]);
+});
---
+++ packages/pipelines/core-2/test/emitter.test.ts
@@ -0,0 +1,17 @@
+import { emitter } from '../src/index';
+
+it(emitter.name, async () => {
+  const myEmitter = emitter<number>();
+
+  myEmitter.push(0);
+
+  const promise = Promise.all([myEmitter.next(), myEmitter.next(), myEmitter.next()]);
+
+  myEmitter.push(1);
+  myEmitter.push(2);
+  myEmitter.push(3);
+
+  await expect(promise).resolves.toEqual(
+    [1, 2, 3].map((value) => ({ value, done: false })),
+  );
+});
