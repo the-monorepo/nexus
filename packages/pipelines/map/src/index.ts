0dedaff389dcecdb2425598f984977f3dd1baf75
Patrick Shaw <mail@patrickshaw.me>
2021-04-16 17:38:00 +1000

    Map and delay now preserve return values (WIP)

--- packages/pipelines/map/src/index.ts
+++ packages/pipelines/map/src/index.ts
@@ -1,7 +1,24 @@
-async function* map<I, O>(iterable: AsyncIterable<I>, mapFn: (i: I) => O) {
-  for await (const i of iterable) {
-    yield await mapFn(i);
+class Mapper<I, O> implements AsyncIterableIterator<O> {
+  constructor(private readonly iterator: AsyncIterator<I>, private readonly mapFn: (i: I) => O) {
+
+  }
+
+  async next() {
+    const i = await this.iterator.next();
+    if(i.done) {
+      return i;
+    }
+
+    return { value: this.mapFn(i.value), done: false };
+  }
+
+  [Symbol.asyncIterator](): AsyncIterableIterator<O> {
+    return this;
   }
 }
+
+const map = <I, O>(iterable: AsyncIterable<I>, mapFn: (i: I) => O) => {
+  return new Mapper(iterable[Symbol.asyncIterator](), mapFn);
+};
 
 export default map;
