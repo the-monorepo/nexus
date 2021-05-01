c9aa72cfdc61f029cdcc394ddeb5b254f8f7f83e
Patrick Shaw <mail@patrickshaw.me>
2021-05-01 19:28:22 +1000

    WIP dependencies now in import map

--- generate-import-map.ts
+++ generate-import-map.ts
@@ -1,6 +1,8 @@
 import { globToRegExp } from "https://deno.land/std/path/glob.ts";
 import { walk } from 'https://deno.land/std/fs/walk.ts';
+import { exists } from 'https://deno.land/std@0.95.0/fs/exists.ts';
 import { join } from 'https://deno.land/std/path/mod.ts';
+
 const dirnameURL = new URL('.', import.meta.url);
 
 const { workspaces } = JSON.parse(await Deno.readTextFile(join(dirnameURL.pathname, 'package.json')));
@@ -13,10 +15,51 @@ const iterable = walk('.', {
 const imports: Record<string, string> = {
 };
 
+const addPackageJsonPaths = (packageJson: Record<string, any>, basePath: string) => {
+  imports[`${packageJson.name}/`] = join(basePath, '');
+  if (packageJson.exports !== undefined) {
+    for (const [path, mapping] of Object.entries<any>(packageJson.exports)) {
+      const importPath = join(packageJson.name, path);
+
+      if (typeof mapping === 'string') {
+        imports[importPath] = join(basePath, mapping);
+      } else if (mapping.import !== undefined) {
+        imports[importPath] = join(basePath, mapping.import);
+      } else if (mapping.default !== undefined) {
+        imports[importPath] = join(basePath, mapping.default);
+      }
+    }
+  } else if (packageJson.module !== undefined) {
+    imports[packageJson.name] = join(basePath, packageJson.module);
+  }
+}
+
 for await (const i of iterable) {
+  console.log(i.path)
   const packageJson = JSON.parse(await Deno.readTextFile(join(dirnameURL.pathname, i.path, 'package.json')))
-  imports[`${packageJson.name}/`] = `./${i.path}/src/`;
-  imports[`${packageJson.name}`] = `./${i.path}/src/index.ts`;
+  addPackageJsonPaths(packageJson, i.path);
+  for (const [packageName, version] of Object.entries(packageJson.dependencies ?? [])) {
+    if (/^workspace/.test(version as string)) {
+      continue
+    }
+
+    const modulePath = `./node_modules/${packageName}/`;
+    const modulePackageJsonPath = join(modulePath, 'package.json');
+
+    const nestedModulePath = join(i.path, modulePath);
+    const nestedModulePackageJsonPath = join(nestedModulePath, 'package.json');
+
+    console.log(modulePath);
+    imports[`${packageName}/`] = modulePath;
+
+    if (await exists(modulePackageJsonPath)) {
+      const dependencyPackageJson = JSON.parse(await Deno.readTextFile(modulePackageJsonPath));
+      addPackageJsonPaths(dependencyPackageJson, modulePath);
+    } else if (await exists(nestedModulePackageJsonPath)) {
+      const dependencyPackageJson = JSON.parse(await Deno.readTextFile(nestedModulePackageJsonPath));
+      addPackageJsonPaths(dependencyPackageJson, nestedModulePath);
+    }
+  }
 }
 
 await Deno.writeTextFile('local.import-map.json', JSON.stringify({ imports }, undefined, 2));
