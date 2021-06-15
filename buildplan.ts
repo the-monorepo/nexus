4dcfeabf726cd4e9fdc23c3359977b6fdaa21a3e
Patrick Shaw <mail@patrickshaw.me>
2021-06-15 12:28:30 +1000

    Ran format

--- buildplan.ts
+++ buildplan.ts
@@ -12,18 +12,30 @@ task('transpile', require.resolve('./buildplan/transpile.ts'));
 
 task('writeme', 'Generates README doco', require.resolve('./buildplan/writeme.ts'));
 
-task('watch', 'Like build but continuously watches for changes', require.resolve('./buildplan/watch.ts'));
+task(
+  'watch',
+  'Like build but continuously watches for changes',
+  require.resolve('./buildplan/watch.ts'),
+);
 
 // task("default", 'Alias for build', build);
 
-task('format', 'Formats all your source code files', require.resolve('./buildplan/format.ts'));
+task(
+  'format',
+  'Formats all your source code files',
+  require.resolve('./buildplan/format.ts'),
+);
 task(
   'formatStaged',
   "Formats all your source code that's currently staging in Git",
   require.resolve('./buildplan/formatStaged.ts'),
 );
 
-task('check-types', 'Run TypeScript type checking', require.resolve('./buildplan/checkTypes.ts'));
+task(
+  'check-types',
+  'Run TypeScript type checking',
+  require.resolve('./buildplan/checkTypes.ts'),
+);
 
 task(
   'check-types-staged',
@@ -31,9 +43,17 @@ task(
   require.resolve('./buildplan/checkTypesStaged.ts'),
 );
 
-task('test', 'Runs unit tests (without building)', require.resolve('./buildplan/test.ts'));
+task(
+  'test',
+  'Runs unit tests (without building)',
+  require.resolve('./buildplan/test.ts'),
+);
 
-task('webpack', 'Bundke all Webpack-bundled packages', require.resolve('./buildplan/webpack.ts'));
+task(
+  'webpack',
+  'Bundke all Webpack-bundled packages',
+  require.resolve('./buildplan/webpack.ts'),
+);
 
 task('serve', 'Serves Webpack bundled packages', require.resolve('./buildplan/serve.ts'));
 run();
