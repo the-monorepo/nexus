5e7b9f7a115c43f07bb75a6f5b8cefaeb924d046
Patrick Shaw <mail@patrickshaw.me>
2021-06-18 03:56:23 +1000

    More accurate generate-import-map.ts

--- generate-import-map.ts
+++ generate-import-map.ts
@@ -1,7 +1,7 @@
 import { globToRegExp } from "https://deno.land/std/path/glob.ts";
 import { walk } from 'https://deno.land/std/fs/walk.ts';
 import { exists } from 'https://deno.land/std@0.95.0/fs/exists.ts';
-import { join } from 'https://deno.land/std/path/mod.ts';
+import { join, normalize } from 'https://deno.land/std/path/mod.ts';
 
 const dirnameURL = new URL('.', import.meta.url);
 
@@ -16,28 +16,41 @@ const imports: Record<string, string> = {
 };
 
 const addPackageJsonPaths = (packageJson: Record<string, any>, basePath: string) => {
-  imports[`${packageJson.name}/`] = join(basePath, '');
+  imports[`${packageJson.name}/`] = './' + join(basePath, '/');
   if (packageJson.exports !== undefined) {
-    for (const [path, mapping] of Object.entries<any>(packageJson.exports)) {
-      const importPath = join(packageJson.name, path);
+    const isMainExportsOnly = Object.values(packageJson.exports).every(val => typeof val === 'string');
+    if (isMainExportsOnly) {
+      if (packageJson.exports.import !== undefined) {
+        imports[packageJson.name] = './' + join(basePath, packageJson.exports.import);
+      } else if (packageJson.exports.default !== undefined) {
+        imports[packageJson.name] = './' + join(basePath, packageJson.exports.default);
+      }
+    } else {
+      for (const [path, mapping] of Object.entries<any>(packageJson.exports)) {
+        const importPath = join(packageJson.name, path);
 
-      if (typeof mapping === 'string') {
-        imports[importPath] = join(basePath, mapping);
-      } else if (mapping.import !== undefined) {
-        imports[importPath] = join(basePath, mapping.import);
-      } else if (mapping.default !== undefined) {
-        imports[importPath] = join(basePath, mapping.default);
+        if (typeof mapping === 'string') {
+          imports[importPath] = './' + join(basePath, mapping);
+        } else if (mapping.import !== undefined) {
+          imports[importPath] = './' + join(basePath, mapping.import);
+        } else if (mapping.default !== undefined) {
+          imports[importPath] = './' + join(basePath, mapping.default);
+        }
       }
     }
   } else if (packageJson.module !== undefined) {
-    imports[packageJson.name] = join(basePath, packageJson.module);
+    imports[packageJson.name] = './' + join(basePath, packageJson.module);
+  } else if (packageJson.main !== undefined) {
+    imports[packageJson.name] = './' + join(basePath, packageJson.main);
   }
 }
 
 for await (const i of iterable) {
-  console.log(i.path)
+  const relativePath = './' + i.path;
+  console.log(relativePath);
+
   const packageJson = JSON.parse(await Deno.readTextFile(join(dirnameURL.pathname, i.path, 'package.json')))
-  addPackageJsonPaths(packageJson, i.path);
+  addPackageJsonPaths(packageJson, relativePath);
   for (const [packageName, version] of Object.entries(packageJson.dependencies ?? [])) {
     if (/^workspace/.test(version as string)) {
       continue
@@ -46,8 +59,8 @@ for await (const i of iterable) {
     const modulePath = `./node_modules/${packageName}/`;
     const modulePackageJsonPath = join(modulePath, 'package.json');
 
-    const nestedModulePath = join(i.path, modulePath);
-    const nestedModulePackageJsonPath = join(nestedModulePath, 'package.json');
+    const nestedModulePath = './' + join(i.path, modulePath);
+    const nestedModulePackageJsonPath = './' + join(nestedModulePath, 'package.json');
 
     console.log(modulePath);
     imports[`${packageName}/`] = modulePath;
