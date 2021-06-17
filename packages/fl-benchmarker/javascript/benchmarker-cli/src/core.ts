db6319f54738d65753daa62857a7ca7189d6b2b3
Patrick Shaw <mail@patrickshaw.me>
2021-06-18 03:58:04 +1000

    Fixed source-map-register/register.js

--- packages/fl-benchmarker/javascript/benchmarker-cli/src/core.ts
+++ packages/fl-benchmarker/javascript/benchmarker-cli/src/core.ts
@@ -1,4 +1,4 @@
-import 'source-map-support/register';
+import 'source-map-support/register.js';
 
 import { resolve, normalize } from 'path';
 
--- packages/monorepo/javascript/buildplan/src/index.ts
+++ packages/monorepo/javascript/buildplan/src/index.ts
@@ -1,4 +1,4 @@
-import 'source-map-support/register';
+import 'source-map-support/register.js';
 
 import { run } from '@buildplan/core';
 
