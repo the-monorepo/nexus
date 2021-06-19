5c3d08cf4e8c97a22acd17d19da3b2f02a0731b7
Patrick Shaw <mail@patrickshaw.me>
2021-06-19 17:56:41 +1000

    generate-import-map now uses node

--- generate-import-map.ts
+++ generate-import-map.ts
@@ -1,78 +1,219 @@
-import { globToRegExp } from "https://deno.land/std/path/glob.ts";
-import { walk } from 'https://deno.land/std/fs/walk.ts';
-import { exists } from 'https://deno.land/std@0.95.0/fs/exists.ts';
-import { join, normalize } from 'https://deno.land/std/path/mod.ts';
-
-const dirnameURL = new URL('.', import.meta.url);
-
-const { workspaces } = JSON.parse(await Deno.readTextFile(join(dirnameURL.pathname, 'package.json')));
-const iterable = walk('.', {
-  match: workspaces.map(globToRegExp),
-  includeFiles: false,
-  includeDirs: true,
+import { readFile, writeFile, stat } from 'fs/promises';
+import { dirname, join } from 'path';
+import { exit } from 'process';
+process.exit = (...args) => {
+  console.log('hm');
+  exit(...args);
+}
+process.on('beforeExit', (code) => {
+  console.log('Process beforeExit event with code: ', code);
 });
 
-const imports: Record<string, string> = {
+process.on('exit', (code) => {
+  console.log('Process exit event with code: ', code);
+});
+process.on('uncaughtException', (err, origin) => {
+  console.log(
+    err,
+    origin
+  );
+});
+
+process.on('warning', (warning) => {
+  console.warn(warning.name);    // Print the warning name
+  console.warn(warning.message); // Print the warning message
+  console.warn(warning.stack);   // Print the stack trace
+});
+
+const imports: Record<string, string> = {};
+const scopes: Record<string, Record<string, string>> = {};
+
+const exportsToScopeEntries = (
+  packageName: string,
+  packageDir: string,
+  exportValue: string | Record<string, string>,
+): [string, string][] => {
+  if (typeof exportValue === 'string') {
+    return [[packageName, join(packageDir, exportValue)]];
+  } else {
+    return Object.entries(exportValue).map(([relativeEntrypoint, relativeMapping]) => {
+      const packageEntrypoint = join(packageName, relativeEntrypoint);
+
+      const mapping = join(packageDir, relativeMapping);
+
+      return [packageEntrypoint, mapping];
+    });
+  }
 };
 
-const addPackageJsonPaths = (packageJson: Record<string, any>, basePath: string) => {
-  imports[`${packageJson.name}/`] = './' + join(basePath, '/');
-  if (packageJson.exports !== undefined) {
-    const isMainExportsOnly = Object.values(packageJson.exports).every(val => typeof val === 'string');
-    if (isMainExportsOnly) {
-      if (packageJson.exports.import !== undefined) {
-        imports[packageJson.name] = './' + join(basePath, packageJson.exports.import);
-      } else if (packageJson.exports.default !== undefined) {
-        imports[packageJson.name] = './' + join(basePath, packageJson.exports.default);
-      }
-    } else {
-      for (const [path, mapping] of Object.entries<any>(packageJson.exports)) {
-        const importPath = join(packageJson.name, path);
+const addImportsFromDependencies = async (
+  dependencies: Record<string, string> | undefined = {},
+  cwd: string,
+) => {
+  for (const packageName of Object.keys(dependencies)) {
+    try {
+      await addImportsFromPackageName(packageName, cwd);
+    } catch(err) {
+      console.error(err);
+    }
+  }
+  /*await Promise.allSettled(
+    Object.keys(dependencies).map((packageName) =>
+      addImportsFromPackageName(packageName, cwd),
+    ),
+  );*/
+};
 
-        if (typeof mapping === 'string') {
-          imports[importPath] = './' + join(basePath, mapping);
-        } else if (mapping.import !== undefined) {
-          imports[importPath] = './' + join(basePath, mapping.import);
-        } else if (mapping.default !== undefined) {
-          imports[importPath] = './' + join(basePath, mapping.default);
+const jsonCache = new Map();
+const readJson = (aPath: string) => {
+  if (jsonCache.has(aPath)) {
+    return jsonCache.get(aPath)!;
+  }
+
+  const parsed = (async () => {
+    const text = await readFile(aPath, 'utf8');
+
+    return JSON.parse(text);
+  })();
+
+  jsonCache.set(aPath, parsed);
+
+  return parsed;
+};
+
+const importCache = new Map();
+const addImportsFromPackageName = (packageName: string, cwd: string) => {
+  const key = packageName + '_' + cwd;
+  if (packageName === '@fl-benchmarker/viewer-cli') {
+    debugger;
+  }
+  if (importCache.has(key)) {
+    console.log('reusing', packageName, cwd, importCache.size);
+    const value = importCache.get(key)!;
+    console.log(value);
+    return;
+  }
+  console.log('caching', packageName, cwd, importCache.size);
+
+  const promise = (async () => {
+    try {
+      const aPath = await (() => {
+        try {
+          Object.keys(require.cache).forEach(function(key) {
+            delete require.cache[key];
+          });
+          return require.resolve(packageName, { paths: [cwd] });
+        } catch (err) {
+          if (err.code === 'ERR_PACKAGE_PATH_NOT_EXPORTED') {
+            return err.message.replace(/^.* in /, '');
+          }
+          throw err;
         }
+      })();
+      const packageJsonPath = await findAssociatedPackageJsonForPath(aPath);
+
+      const json = await readJson(packageJsonPath);
+
+      const packageDir = dirname(packageJsonPath);
+
+      const mainEntrypoints = ((): [string, string][] => {
+        if (json.exports !== undefined) {
+          if (json.exports.import !== undefined) {
+            return exportsToScopeEntries(packageName, packageDir, json.exports.import);
+          } else if (json.exports.default !== undefined) {
+            return exportsToScopeEntries(packageName, packageDir, json.exports.default);
+          }
+          return [];
+        } else {
+          const main = json.module ?? json.main;
+          const mappings: [string, string][] = (main === undefined ? [] : [[packageName, join(packageDir, main)]]);
+          return [
+            // E.g. @x/y
+            [packageName + '/', dirname(packageJsonPath)],
+            ...mappings,
+          ]
+        }
+      })();
+
+      await addImportsFromDependencies(json.dependencies, packageDir);
+
+      scopes[cwd] = {
+        ...scopes[cwd],
+        ...Object.fromEntries(mainEntrypoints),
+      };
+    } catch (err) {
+      if (err.code === 'MODULE_NOT_FOUND') {
+        return;
       }
+      console.error(err, packageName, [cwd]);
+      throw err;
     }
-  } else if (packageJson.module !== undefined) {
-    imports[packageJson.name] = './' + join(basePath, packageJson.module);
-  } else if (packageJson.main !== undefined) {
-    imports[packageJson.name] = './' + join(basePath, packageJson.main);
+  })();
+
+  importCache.set(key, promise);
+
+  return promise;
+};
+
+const packageJsonCache = new Map();
+const findAssociatedPackageJsonForPath = (filePath: string) => {
+  let current = dirname(filePath);
+  let resolve;
+  let reject;
+  const promise = new Promise<string>((resolveInput, rejectInput) => {
+    resolve = resolveInput;
+    reject = rejectInput;
+  });
+  (async () => {
+    try {
+      // TODO: Account for getting to the root path
+      while (true) {
+        if (packageJsonCache.has(current)) {
+          const value = await packageJsonCache.get(current)!;
+          resolve(value);
+          return value;
+        }
+        packageJsonCache.set(current, promise);
+        try {
+          const packageJsonPath = join(current, 'package.json');
+          const stats = await stat(packageJsonPath);
+          if (stats.isFile()) {
+            resolve(packageJsonPath);
+            return packageJsonPath;
+          }
+        } catch (err) {
+          if (err.code !== 'ENOTDIR' && err.code !== 'ENOENT' && err.code !== 'EISDIR') {
+            console.error(err);
+            throw err;
+          }
+        }
+        current = dirname(current);
+      }
+    } catch(err) {
+      reject(err);
+      throw err;
+    }
+  })();
+
+  return promise;
+};
+
+const main = async () => {
+  try {
+    const rootPackageJson = JSON.parse(
+      await readFile(join(__dirname, 'package.json'), 'utf8'),
+    );
+    await addImportsFromDependencies(rootPackageJson.dependencies, __dirname);
+    console.log({ scopes });
+
+    await writeFile(
+      'local.import-map.json',
+      JSON.stringify({ imports, scopes }, undefined, 2),
+      'utf8',
+    );
+  } catch (err) {
+    console.log(err);
   }
-}
+};
 
-for await (const i of iterable) {
-  const relativePath = './' + i.path;
-  console.log(relativePath);
-
-  const packageJson = JSON.parse(await Deno.readTextFile(join(dirnameURL.pathname, i.path, 'package.json')))
-  addPackageJsonPaths(packageJson, relativePath);
-  for (const [packageName, version] of Object.entries(packageJson.dependencies ?? [])) {
-    if (/^workspace/.test(version as string)) {
-      continue
-    }
-
-    const modulePath = `./node_modules/${packageName}/`;
-    const modulePackageJsonPath = join(modulePath, 'package.json');
-
-    const nestedModulePath = './' + join(i.path, modulePath);
-    const nestedModulePackageJsonPath = './' + join(nestedModulePath, 'package.json');
-
-    console.log(modulePath);
-    imports[`${packageName}/`] = modulePath;
-
-    if (await exists(modulePackageJsonPath)) {
-      const dependencyPackageJson = JSON.parse(await Deno.readTextFile(modulePackageJsonPath));
-      addPackageJsonPaths(dependencyPackageJson, modulePath);
-    } else if (await exists(nestedModulePackageJsonPath)) {
-      const dependencyPackageJson = JSON.parse(await Deno.readTextFile(nestedModulePackageJsonPath));
-      addPackageJsonPaths(dependencyPackageJson, nestedModulePath);
-    }
-  }
-}
-
-await Deno.writeTextFile('local.import-map.json', JSON.stringify({ imports }, undefined, 2));
+main();
