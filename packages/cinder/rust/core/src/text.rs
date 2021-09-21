b02f67c3ba424847fd45d86f837c4d17968fbda8
Patrick Shaw <pshaw@atlassian.com>
2021-09-21 23:01:31 +1000

    Renamed TextXYZ to TextDomXYZ

--- packages/cinder/rust/core/src/text.rs
+++ packages/cinder/rust/core/src/text.rs
@@ -1,23 +1,23 @@
 use crate::text::*;
 
-struct TextBlueprint {
+struct TextDomBlueprint {
 
 }
 
-impl<'a> Blueprint<&'a str, &'a str, TextUpdater<'a>, TextMounter<'a>> for TextBlueprint {
-    fn create(&mut self, input: &'a str) -> TextMounter<'a> {
-        TextMounter {
+impl<'a> Blueprint<&'a str, &'a str, TextDomUpdater<'a>, TextDomMounter<'a>> for TextDomBlueprint {
+    fn create(&mut self, input: &'a str) -> TextDomMounter<'a> {
+      TextDomMounter {
           initial_text: input,
         }
     }
 }
 
-struct TextUpdater<'a> {
+struct TextDomUpdater<'a> {
   text_node: web_sys::Text,
   text: Box<&'a str>,
 }
 
-impl<'a> Updater<&'a str> for TextUpdater<'a> {
+impl<'a> Updater<&'a str> for TextDomUpdater<'a> {
   fn update(&mut self, input: &'a str) {
     if *self.text == input {
       return;
@@ -31,18 +31,18 @@ impl<'a> Updater<&'a str> for TextUpdater<'a> {
   }
 }
 
-struct TextMounter<'a> {
+struct TextDomMounter<'a> {
   initial_text: &'a str,
 }
 
 
-impl<'a> Mounter<&'a str, TextUpdater<'a>> for TextMounter<'a> {
-  fn mount(self, container: &mut web_sys::Node, before: &web_sys::Node) -> TextUpdater<'a> {
+impl<'a> Mounter<&'a str, TextDomUpdater<'a>> for TextDomMounter<'a> {
+  fn mount(self, container: &mut web_sys::Node, before: &web_sys::Node) -> TextDomUpdater<'a> {
     let document = web_sys::window().unwrap().document().unwrap();
     let text_node = document.create_text_node(self.initial_text);
     container.insert_before(&text_node, Some(before)).unwrap();
 
-    TextUpdater {
+    TextDomUpdater {
       text_node,
       text: Box::new(self.initial_text),
     }
