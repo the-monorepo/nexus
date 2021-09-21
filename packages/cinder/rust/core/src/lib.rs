0fdcfc42eb025ac32e808c48b6bd03530089dfc4
Patrick Shaw <pshaw@atlassian.com>
2021-09-21 20:26:25 +1000

    Added TextMounter and TextUpdater + Refactoring

--- packages/cinder/rust/core/src/lib.rs
+++ packages/cinder/rust/core/src/lib.rs
@@ -1,15 +1,67 @@
+use wasm_bindgen::prelude::*;
+use web_sys::Text;
+
 trait Updater<I> {
-  fn update(&self, input: I);
-  fn unmount(&self);
+  fn update(&mut self, input: I);
+  fn unmount(&mut self);
 }
 
-trait Mounter<I, UO, U : Updater<UO>> {
-  fn mount(&self) -> U;
+trait Mounter<UO, U : Updater<UO>> {
+  fn mount(self, container: &mut web_sys::Node, before: &web_sys::Node) -> U;
 }
 
 
-trait Blueprint<I, UO, U : Updater<UO>, M : Mounter<I, UO, U>> {
-  fn create(&self) -> M;
+trait Blueprint<I, UO, U : Updater<UO>, M : Mounter<UO, U>> {
+  fn create(&mut self, input: I) -> M;
+}
+
+struct TextBlueprint {
+
+}
+
+impl<'a> Blueprint<&'a str, &'a str, TextUpdater<'a>, TextMounter<'a>> for TextBlueprint {
+    fn create(&mut self, input: &'a str) -> TextMounter<'a> {
+        TextMounter {
+          initial_text: input,
+        }
+    }
+}
+
+struct TextUpdater<'a> {
+  text_node: web_sys::Text,
+  text: Box<&'a str>,
+}
+
+impl<'a> Updater<&'a str> for TextUpdater<'a> {
+  fn update(&mut self, input: &'a str) {
+    if *self.text == input {
+      return;
+    }
+
+    *self.text = input;
+  }
+
+  fn unmount(&mut self) {
+    todo!()
+  }
+}
+
+struct TextMounter<'a> {
+  initial_text: &'a str,
+}
+
+
+impl<'a> Mounter<&'a str, TextUpdater<'a>> for TextMounter<'a> {
+  fn mount(self, container: &mut web_sys::Node, before: &web_sys::Node) -> TextUpdater<'a> {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let text_node = document.create_text_node(self.initial_text);
+    container.insert_before(&text_node, Some(before)).unwrap();
+
+    TextUpdater {
+      text_node,
+      text: Box::new(self.initial_text),
+    }
+  }
 }
 
 trait Renderer {
@@ -27,7 +79,6 @@ impl Renderer for DomRenderer {
 }
 
 pub fn create_dom_renderer(container: web_sys::Node) -> DomRenderer {
-  return DomRenderer { container };
   /*
     // Use `web_sys`'s global `window` function to get a handle on the global
     // window object.
@@ -41,4 +92,5 @@ pub fn create_dom_renderer(container: web_sys::Node) -> DomRenderer {
 
     body.append_child(&val).unwrap();
   */
+  return DomRenderer { container };
 }
