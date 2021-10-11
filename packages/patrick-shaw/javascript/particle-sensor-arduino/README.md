d370d8101bd5a42244d707c4f5f8eb3c72627c16
Patrick Shaw <mail@patrickshaw.me>
2021-10-12 00:49:29 +1100

    Moved out personal projects from the-monorepo

--- packages/patrick-shaw/javascript/particle-sensor-arduino/README.md
+++
@@ -1,6 +0,0 @@
-# Particle Sensor Arduino
-
-Arduino logic for Arduino part side of the particle sensor app
-
----
-This documentation was generated using [writeme](https://www.npmjs.com/package/@writeme/core)
--- packages/patrick-shaw/rust/spot-welder-arduino/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cargo build && avr-objcopy -O ihex ./target/avr-atmega328p/debug/spot-welder-arduino.elf ./release.hex
-arduino-cli upload --input-file ./release.hex -p $(find /dev -maxdepth 1 -name "cu.usbserial*" ) --fqbn arduino:avr:uno
