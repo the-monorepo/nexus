3b90e4b83cd3c81462d73a83529d7b3587b5dedf
Patrick Shaw <pshaw@atlassian.com>
2022-02-05 16:33:16 +1100

    Better help messages for scriptplan cli

--- packages/scriptplan/rust/cli/src/main.rs
+++ packages/scriptplan/rust/cli/src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App};
+use clap::App;
 
 use std::collections::VecDeque;
 
@@ -15,28 +15,28 @@ use scriptplan_bash::yaml_rust::YamlLoader;
 use scriptplan_bash::YamlScriptParser;
 use std::convert::TryFrom;
 
+fn new_cli_app<'a>() -> App<'a> {
+    App::new("Scriptplan CLI")
+        .setting(clap::AppSettings::TrailingVarArg)
+        .arg(
+            clap::Arg::new("script-file")
+                .short('s')
+                .long("script-file")
+                .takes_value(true)
+                .default_value("./scripts.yaml"),
+        )
+}
+
 #[tokio::main]
 async fn main() {
-    let scriptplan_app = App::new("Scriptplan CLI")
-    .setting(clap::AppSettings::TrailingVarArg)
-    .arg(
-        clap::Arg::new("script-file")
-            .short('s')
-            .long("script-file")
-            .takes_value(true)
-            .default_value("./scripts.yaml"),
-    )
-    .arg(
-      clap::Arg::new("other")
-        .multiple_values(true)
-        .hide(true)
-    );
-
-    let initial_matches = scriptplan_app.get_matches();
+    let initial_matches = new_cli_app()
+        .setting(clap::AppSettings::DisableHelpSubcommand)
+        .setting(clap::AppSettings::DisableVersionFlag)
+        .setting(clap::AppSettings::AllowExternalSubcommands)
+        .arg(clap::Arg::new("TASK_COMMANDS").multiple_values(true))
+        .get_matches();
 
     let script_file = initial_matches.value_of("script-file").unwrap();
-
-    let other_args = initial_matches.values_of_os("other").unwrap();
 
     let path = Path::new(script_file);
     let s = fs::read_to_string(path).unwrap();
@@ -48,27 +48,28 @@ async fn main() {
 
     let scriptplan = YamlScriptParser::try_from(&map).unwrap();
 
-    let app = scriptplan
-        .tasks
-        .keys()
-        .fold(App::new("Scriptplan Tasks")
-        .setting(clap::AppSettings::NoBinaryName), |temp_app, task| {
+    let app = scriptplan.tasks.keys().fold(
+        new_cli_app()
+          .setting(clap::AppSettings::SubcommandRequiredElseHelp),
+        |temp_app, task| {
             temp_app.subcommand(
                 App::new(task.to_string())
                     .setting(clap::AppSettings::TrailingVarArg)
-                    .arg(clap::Arg::new("other").multiple_values(true).hide(true)),
+                    .setting(clap::AppSettings::DisableHelpSubcommand)
+                    .setting(clap::AppSettings::DisableVersionFlag)
+                    .arg(clap::Arg::new("EXTRA_ARGUMENTS").multiple_values(true)),
             )
-        });
+        },
+    );
 
-    let app_matches = app.get_matches_from(other_args);
+    // TODO: Would be nice if we didn't get_matches twice
+    let app_matches = app.get_matches();
 
     if let Some(name) = app_matches.subcommand_name() {
         let root_task = app_matches.subcommand_matches(name).unwrap();
         let user_vars_iter: VecDeque<_> = (|| {
-            if let Some(values) = root_task.values_of("other") {
-                return values
-                    .map(|x| Arc::new(x.to_string()))
-                    .collect();
+            if let Some(values) = root_task.values_of("EXTRA_ARGUMENTS") {
+                return values.map(|x| Arc::new(x.to_string())).collect();
             } else {
                 return VecDeque::new().into_iter().collect();
             }
