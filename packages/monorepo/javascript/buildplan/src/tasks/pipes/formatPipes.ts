2e747924ae1c6b7a198ad2b8ff50b0473cf04e95
Patrick Shaw <mail@patrickshaw.me>
2022-04-16 06:58:12 +1000

    Removed use of gulp-eslint and gulp-prettier

--- packages/monorepo/javascript/buildplan/src/tasks/pipes/formatPipes.ts
+++ packages/monorepo/javascript/buildplan/src/tasks/pipes/formatPipes.ts
@@ -3,24 +3,48 @@ import { simplePipeLogger } from '../utils/simplePipeLogger';
 
 import logger from '../utils/logger';
 
-import prettier from 'gulp-prettier';
-
-import eslint from 'gulp-eslint';
+import * as eslint from 'eslint';
+import prettier from 'prettier';
+import * as through2 from 'through2';
 
 const prettierPipes = async (stream) => {
   const l = logger.child(chalk.magentaBright('prettier'));
-  return stream.pipe(simplePipeLogger(l)).pipe(prettier());
+
+  const prettierOptions = await prettier.resolveConfig();
+
+  return stream.pipe(simplePipeLogger(l)).pipe(
+    through2.obj(async (file, enc, callback) => {
+      const formattedText = prettier.format(file.contents.toString(), prettierOptions);
+
+      file.contents = Buffer.from(formattedText, 'utf8');
+
+      callback(undefined, file);
+    }),
+  );
 };
 
 const lintPipes = async (stream, lintOptions) => {
   const l = logger.child(chalk.magentaBright('eslint'));
-  return (
-    stream
-      .pipe(simplePipeLogger(l))
-      .pipe(eslint(lintOptions))
-      .pipe(eslint.format('unix'))
-      // TODO: Need to halt build process/throw error
-      .pipe(eslint.failAfterError())
+
+  const instance = new eslint.ESLint(lintOptions);
+
+  return stream.pipe(simplePipeLogger(l)).pipe(
+    through2.obj(async (file, enc, callback) => {
+      const results = await instance.lintText(file.contents.toString(), {
+        filePath: file.path,
+      });
+
+      if (results.length > 0) {
+        if (results.length > 1) {
+          l.warn(`Received ${results.length} ESLint results for a single file`);
+        }
+
+        if (results[0].output !== undefined) {
+          file.contents = Buffer.from(results[0].output, 'utf8');
+        }
+      }
+      callback(undefined, file);
+    }),
   );
 };
 
