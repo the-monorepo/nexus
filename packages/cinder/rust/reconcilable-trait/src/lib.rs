489332736d001ab80f10bd4e71c12cb3e84aae49
Patrick Shaw <mail@patrickshaw.me>
2022-04-28 20:30:11 +1000

    Added an Unchanged struct to reconcilable-trait for when you have no extra information to add after a reconciliation failure

--- packages/cinder/rust/reconcilable-trait/src/lib.rs
+++ packages/cinder/rust/reconcilable-trait/src/lib.rs
@@ -1,6 +1,11 @@
 pub trait Reconcilable {
     type Value;
-    type Unreconciled;
+    type Unreconciled; // = Unchanged<Self, Self::Value>;
     type Reconciled;
     fn reconcile(self, new_value: Self::Value) -> Result<Self::Reconciled, Self::Unreconciled>;
+}
+
+pub struct Unchanged<Source, Value> {
+    pub source: Source,
+    pub value: Value,
 }
