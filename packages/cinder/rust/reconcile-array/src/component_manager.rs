3140c04acbb1fb83e68dcccb4848955dce0cc9d5
Patrick Shaw <mail@patrickshaw.me>
2022-10-27 20:01:00 -1000

    reconcile-array tests now compile

--- packages/cinder/rust/reconcile-array/src/component_manager.rs
+++ packages/cinder/rust/reconcile-array/src/component_manager.rs
@@ -5,40 +5,32 @@ use reconcilable_trait::Reconcilable;
  * Handles the repopulation as well as tracking of whether to pop/shift off more component values
  */
 #[derive(Debug)]
-pub struct ComponentManager<
-    ManagerGeneric,
-    HeadTailGeneric
-> {
-    pub iterator: ManagerGeneric,
+pub struct ComponentManager<IteratorManagerGeneric, HeadTailGeneric> {
+    pub iterator: IteratorManagerGeneric,
     pub ends: HeadTailGeneric,
 }
 
-impl<
-    IteratorGeneric,
-    BeforeIteratorManagerGeneric,
-    EndIdentifier : Wrapper,
-    EndsGeneric,
-> Repopulatable<
-    EndIdentifier
-> for
-    ComponentManager<BeforeIteratorManagerGeneric, EndsGeneric> where
-    IteratorGeneric : DoubleEndedIterator,
-    BeforeIteratorManagerGeneric : Repopulatable<
+impl<IteratorGeneric, BeforeIteratorManagerGeneric, EndIdentifier: Wrapper, EndsGeneric>
+    Repopulatable<EndIdentifier> for ComponentManager<BeforeIteratorManagerGeneric, EndsGeneric>
+where
+    IteratorGeneric: DoubleEndedIterator,
+    BeforeIteratorManagerGeneric: Repopulatable<
         EndIdentifier,
         OkGeneric = iterator_manager::IteratorResult<IteratorGeneric, IteratorGeneric::Item>,
-        FailureGeneric = iterator_manager::IteratorManager<IteratorGeneric, Nothing>
+        FailureGeneric = iterator_manager::IteratorManager<IteratorGeneric, Nothing>,
     >,
-    EndsGeneric: MergeTrait<EndIdentifier::Wrapped<IteratorGeneric::Item>>
+    EndsGeneric: MergeTrait<EndIdentifier::Wrapped<IteratorGeneric::Item>>,
 {
-    type OkGeneric = ComponentManager<IteratorManager<IteratorGeneric, Allow>, EndsGeneric::MergedObject>;
+    type OkGeneric =
+        ComponentManager<IteratorManager<IteratorGeneric, Allow>, EndsGeneric::MergedObject>;
     type FailureGeneric = ComponentManager<IteratorManager<IteratorGeneric, Nothing>, EndsGeneric>;
     fn repopulate(
         self,
-        identifier: EndIdentifier
+        identifier: EndIdentifier,
     ) -> Result<
-    ComponentManager<IteratorManager<IteratorGeneric, Allow>, EndsGeneric::MergedObject>,
-    ComponentManager<IteratorManager<IteratorGeneric, Nothing>, EndsGeneric>
-> {
+        ComponentManager<IteratorManager<IteratorGeneric, Allow>, EndsGeneric::MergedObject>,
+        ComponentManager<IteratorManager<IteratorGeneric, Nothing>, EndsGeneric>,
+    > {
         match self.iterator.repopulate(identifier) {
             Ok(result) => Ok(ComponentManager {
                 iterator: result.manager,
@@ -52,16 +44,14 @@ impl<
     }
 }
 
-impl<IteratorGeneric : DoubleEndedIterator>
-    ComponentManager<IteratorManager<IteratorGeneric, Allow>, HeadTail<Nothing, Nothing>>
+impl<IteratorGeneric: DoubleEndedIterator> From<IteratorGeneric>
+    for ComponentManager<IteratorManager<IteratorGeneric, Allow>, HeadTail<Nothing, Nothing>>
 {
-    pub fn new(
-        iterator: IteratorGeneric,
-    ) -> ComponentManager<IteratorManager<IteratorGeneric, Allow>, HeadTail<Nothing, Nothing>> {
-        return ComponentManager {
+    fn from(iterator: IteratorGeneric) -> Self {
+        ComponentManager {
             iterator: IteratorManager::new(iterator),
             ends: HeadTail::nothing(),
-        };
+        }
     }
 }
 
@@ -70,24 +60,19 @@ enum ManagerReconcileFailureState<E, U> {
     Unreconciled(U),
 }
 
-impl<
-    Value,
-    ManagerGeneric,
-    EndsGeneric
-> Reconcilable<Value> for ComponentManager<ManagerGeneric, EndsGeneric> where
-    EndsGeneric : Reconcilable<Value>
+impl<Value, ManagerGeneric, EndsGeneric> Reconcilable<Value>
+    for ComponentManager<ManagerGeneric, EndsGeneric>
+where
+    EndsGeneric: Reconcilable<Value>,
 {
     type Unreconciled = ComponentManager<ManagerGeneric, EndsGeneric::Unreconciled>;
-    type Reconciled =  ComponentManager<ManagerGeneric, EndsGeneric::Reconciled>;
+    type Reconciled = ComponentManager<ManagerGeneric, EndsGeneric::Reconciled>;
 
-    fn reconcile(self, value: Value) -> Result<
-        Self::Reconciled,
-        Self::Unreconciled
-    > {
+    fn reconcile(self, value: Value) -> Result<Self::Reconciled, Self::Unreconciled> {
         let result = self.ends.reconcile(value);
-        
+
         match result {
-            Ok(reonciled) => {
+            Ok(reconciled) => {
                 todo!();
             }
             Err(unreconciled) => {
@@ -100,29 +85,45 @@ impl<
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::collections::VecDeque;
     use reconcilable_trait::mocks::AlwaysReconcileValue;
-    
+    use std::collections::VecDeque;
+
     #[test]
     fn todo_reconcile_h() {
-        let list = VecDeque::from([AlwaysReconcileValue::<u32>::new()]);
-        let manager = ComponentManager::new(list.into_iter());
+        let list = VecDeque::from([AlwaysReconcileValue::<i32>::new()]);
+        let manager = ComponentManager::from(list.into_iter());
 
-        assert_eq!(manager.repopulate(HEAD).unwrap().reconcile(Head(4)).unwrap(), 4);
+        let (_, Head(val)) = manager
+            .repopulate(HEAD)
+            .unwrap()
+            .reconcile(Head(4))
+            .unwrap()
+            .ends
+            .split();
+
+        assert_eq!(val, 4);
     }
 
     #[test]
     fn todo_reconcile_t() {
-        let list = VecDeque::from([AlwaysReconcileValue::<u32>::new()]);
-        let manager = ComponentManager::new(list.into_iter());
+        let list = VecDeque::from([AlwaysReconcileValue::<i32>::new()]);
+        let manager = ComponentManager::from(list.into_iter());
 
-        assert_eq!(manager.repopulate(TAIL).unwrap().reconcile(Tail(4)).unwrap(), 4);
+        let (_, Head(val)) = manager
+            .repopulate(HEAD)
+            .unwrap()
+            .reconcile(Head(4))
+            .unwrap()
+            .ends
+            .split();
+
+        assert_eq!(val, 4);
     }
 
     #[test]
     fn repopulate_success() {
         let list = VecDeque::from([1, 2, 3]);
-        let manager = ComponentManager::new(list.into_iter());
+        let manager = ComponentManager::from(list.into_iter());
 
         let manager = manager.repopulate(TAIL).unwrap();
         assert_eq!(manager.ends.tail, 3);
@@ -130,17 +131,19 @@ mod tests {
         let manager = manager.repopulate(HEAD).unwrap();
         assert_eq!(manager.ends.head, 1);
     }
-    
+
     #[test]
     fn repopulate_t_error_when_empty() {
-        let manager = ComponentManager::new(VecDeque::<AlwaysReconcileValue::<u32>>::from([]).into_iter());
+        let manager =
+            ComponentManager::from(VecDeque::<AlwaysReconcileValue<i32>>::from([]).into_iter());
 
         manager.repopulate(TAIL).unwrap_err();
     }
 
     #[test]
     fn repopulate_h_error_when_empty() {
-        let manager = ComponentManager::new(VecDeque::<AlwaysReconcileValue::<u32>>::from([]).into_iter());
+        let manager =
+            ComponentManager::from(VecDeque::<AlwaysReconcileValue<i32>>::from([]).into_iter());
 
         manager.repopulate(HEAD).unwrap_err();
     }
